a7aeb147532772f28ce9222b254a3239ce8ab0ba
Johniel <johniel.s.m@gmail.com>
2016-11-07 01:09:04 +0900

    arc063 CD

---
+++ codeforces/377div2/A/main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+typedef long long int lli;
+typedef unsigned long long ull;
+typedef complex<double> point;
+
+template<typename P, typename Q>
+ostream& operator << (ostream& os, pair<P, Q> p)
+{
+  os << "(" << p.first << "," << p.second << ")";
+  return os;
+}
+
+int main(int argc, char *argv[])
+{
+  std::ios::sync_with_stdio(false);
+  int k, r;
+  while (cin >> k >> r) {
+    for (int i = 1; i < 1000; ++i) {
+      int mod = (k * i) % 10;
+      if (mod == r || mod == 0) {
+        cout << i << endl;
+        break;
+      }
+    }
+  }
+  return 0;
+}
