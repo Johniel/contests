066bef699bd0e2c2181019cf8b9befdf99f85f34
___Johniel <johniel.s.m@gmail.com>
2017-05-15 14:55:53 +0900

    Create main.cpp

---
+++ codeforces/411div2/A/main.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+typedef long long int lli;
+typedef unsigned long long ull;
+typedef complex<double> point;
+
+template<typename P, typename Q>
+ostream& operator << (ostream& os, pair<P, Q> p)
+{
+  os << "(" << p.first << "," << p.second << ")";
+  return os;
+}
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  lli a, b;
+  while (cin >> a >> b) {
+    if (a == b) cout << a << endl;
+    else cout << 2 << endl;
+  }
+  
+  return 0;
+}
