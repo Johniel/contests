29a91c1033f8e275e6b68e6d87b825f8e97510b9
Johniel <johniel.s.m@gmail.com>
2017-08-20 20:44:07 +0900

    429div2 ABC

---
+++ codeforces/429div2/A/main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+typedef long long int lli;
+typedef unsigned long long ull;
+typedef complex<double> point;
+
+template<typename P, typename Q>
+ostream& operator << (ostream& os, pair<P, Q> p)
+{
+  os << "(" << p.first << "," << p.second << ")";
+  return os;
+}
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n, k;
+  while (cin >> n >> k) {
+    string s;
+    cin >> s;
+    map<char, int> m;
+    int mx = 0;
+    each (c, s) mx = max(mx, ++m[c]);
+    cout << (mx <= k ? "YES" : "NO") << endl;
+  }
+  
+  return 0;
+}
