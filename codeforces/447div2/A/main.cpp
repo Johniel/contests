d20e4db7a2c7c0e3fd72a56991422451c01cda0f
Johniel <johniel.s.m@gmail.com>
2017-11-20 22:43:51 +0900

    447div2AB

---
+++ codeforces/447div2/A/main.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+typedef long long int lli;
+typedef unsigned long long ull;
+typedef complex<double> point;
+
+template<typename P, typename Q>
+ostream& operator << (ostream& os, pair<P, Q> p)
+{
+  os << "(" << p.first << "," << p.second << ")";
+  return os;
+}
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  string s;
+  while (cin >> s) {
+    int cnt = 0;
+    for (int i = 0; i < s.size(); ++i) {
+      for (int j = i + 1; j < s.size(); ++j) {
+        for (int k = j + 1; k < s.size(); ++k) {
+          cnt += (s[i] == 'Q' && s[j] == 'A' && s[k] == 'Q');
+        }
+      }
+    }
+    cout << cnt << endl;
+  }
+  return 0;
+}
