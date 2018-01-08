a9be0fa70c0025dc32d9606219b973c220f22a6c
Johniel <johniel.s.m@gmail.com>
2018-01-09 03:49:25 +0900

    cf hello2018

---
+++ codeforces/hello2018/C/main.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+typedef long long int lli;
+typedef unsigned long long ull;
+typedef complex<double> point;
+
+template<typename P, typename Q>
+ostream& operator << (ostream& os, pair<P, Q> p)
+{
+  os << "(" << p.first << "," << p.second << ")";
+  return os;
+}
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  lli n;
+  lli L;
+  while (cin >> n >> L) {
+    const lli N = 35;
+    lli cost[N];
+    fill(cost, cost + N, 1LL << 60);
+    for (int i = 0; i < n; ++i) {
+      cin >> cost[i];
+    }
+
+    for (int i = 0; i + 1 < N; ++i) {
+      cost[i + 1] = min(cost[i + 1], cost[i] * 2);
+    }
+    
+    lli v[N];
+    for (int i = 0; i < N; ++i) {
+      v[i] = 1LL << i;
+    }
+
+    lli mn = 1LL << 60;
+    for (int i = n - 1; 0 <= i; --i) {
+      lli sum = 0;
+      lli x = L;
+      for (int j = i; 0 <= j; --j) {
+        lli a = x / v[j];
+        if (x % v[j]) ++a;
+        mn = min(mn, sum + cost[j] * a);
+
+        sum += cost[j] * (x / v[j]);
+        x %= v[j];
+      }
+    }
+    cout << mn << endl;
+  }
+  
+  return 0;
+}
---
+++ codeforces/hello2018/D/main.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+typedef long long int lli;
+typedef unsigned long long ull;
+typedef complex<double> point;
+
+template<typename P, typename Q>
+ostream& operator << (ostream& os, pair<P, Q> p)
+{
+  os << "(" << p.first << "," << p.second << ")";
+  return os;
+}
+
+struct S {
+  int first, second, idx;
+};
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n, T;
+  while (cin >> n >> T) {
+    vector<S> a(n);
+    each (i, a) cin >> i.first >> i.second;
+    for (int i = 0; i < n; ++i) {
+      a[i].idx = i;
+    }
+    sort(a.begin(), a.end(), [] (auto x, auto y) { return x.second < y.second; });
+    auto f = [&] (int m) {
+      lli sum = 0;
+      int cap = m;
+      each (i, a) {
+        if (cap == 0) break;
+        if (m <= i.first) {
+          --cap;
+          sum += i.second;
+        }
+      }
+      return cap == 0 && sum <= T;
+    };
+    int small = 0;
+    int large = a.size() + 1;
+    while (small + 1 < large) {
+      int mid = (small + large) / 2;
+      if (f(mid)) small = mid;
+      else large = mid;
+    }
+    cout << small << endl;
+    vector<int> v;
+    for (int i = 0; i < a.size() && v.size() < small; ++i) {
+      if (small <= a[i].first) {
+        v.push_back(a[i].idx);
+      }
+    }
+    cout << v.size() << endl;
+    each (i, v) cout << i+1 << ' ';
+    cout << endl;
+  }
+  
+  return 0;
+}
