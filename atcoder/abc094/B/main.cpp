5300a0798aacbe9bfe64f63850f370d773ade2ad
Johniel <motohazu@mercari.com>
2018-10-28 18:07:26 +0900

    ABC094AB

---
+++ atcoder/abc094/B/main.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+typedef long long int lli;
+typedef unsigned long long ull;
+typedef complex<double> point;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; each (i, v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { each (i, v) is >> i; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n, m, x;
+  while (cin >> n >> m >> x) {
+    vector<int> a(m);
+    cin >> a;
+    sort(a.begin(), a.end());
+    each (i, a) --i;
+
+    int z = 0;
+    for (int i = x - 1; i <= n; ++i) {
+      if (binary_search(a.begin(), a.end(), i)) {
+        ++z;
+      }
+    }
+
+    int y = 0;
+    for (int i = x - 1; 0 <= i; --i) {
+      if (binary_search(a.begin(), a.end(), i)) {
+        ++y;
+      }      
+    }
+    
+    cout << min(z, y) << endl;
+  }
+  
+  return 0;
+}
