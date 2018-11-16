54bb28c115b1a4dbab1f39797509e66fe55a24f6
Johniel <motohazu@mercari.com>
2018-11-16 11:04:00 +0900

    ABC065AB

---
+++ atcoder/abc065/B/main.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+typedef long long int lli;
+typedef unsigned long long ull;
+typedef complex<double> point;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; each (i, v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { each (i, v) is >> i; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n;
+  while (cin >> n) {
+    vector<int> a(n);
+    cin >> a;
+    each (i, a) --i;
+    int curr = 0;
+    map<int, int> cost;
+    cost[curr] = 0;
+    while (curr != 1) {
+      int next = a[curr];
+      if (cost.count(next)) break;
+      cost[next] = cost[curr] + 1;
+      curr = next;
+    }
+    cout << (cost.count(1) ? cost[1] : -1) << endl;
+  }
+
+  return 0;
+}
