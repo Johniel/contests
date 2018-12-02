98453945b0c31b619d3a373caa80840172adcaec
Johniel <johniel.s.m@gmail.com>
2018-12-02 22:04:30 +0900

    ABC114ABCD

---
+++ atcoder/abc114/B/main.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+typedef long long int lli;
+typedef unsigned long long ull;
+typedef complex<double> point;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; each (i, v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { each (i, v) is >> i; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  string s;
+  while (cin >> s) {
+    vector<int> v;
+    each (c, s) v.push_back(c - '0');
+    int mn = 1 << 29;
+    for (int i = 0; i + 2 < v.size(); ++i) {
+      setmin(mn, abs(753 - (v[i] * 100 + v[i + 1] * 10 + v[i + 2])));
+    }
+    cout << mn << endl;
+  }
+  
+  return 0;
+}
