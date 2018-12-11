ae0999d256e695bbd55892d24378cace5886db62
Johniel <motohazu@mercari.com>
2018-12-11 12:23:51 +0900

    ARC058C

---
+++ atcoder/arc058/C/main.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+typedef long long int lli;
+typedef unsigned long long ull;
+typedef complex<double> point;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; each (i, v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { each (i, v) is >> i; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+bool fn(int n, vector<int> v)
+{
+  while (n) {
+    if (binary_search(v.begin(), v.end(), n % 10)) {
+      return false;
+    }
+    n /= 10;
+  }
+  return true;
+}
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n, k;
+  while (cin >> n >> k) {
+    vector<int> d(k);
+    cin >> d;
+    while (!fn(n, d)) ++n;
+    cout << n << endl;
+  }
+  
+  return 0;
+}
