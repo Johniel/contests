fb5cff50b633cb0e9d126aa4c72732ec02446f4d
Johniel <johniel.s.m@gmail.com>
2019-01-28 01:02:23 +0900

    NIKKEI ABC

---
+++ atcoder/nikkei/2019/qual/C/main.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+typedef long long int lli;
+typedef unsigned long long ull;
+typedef complex<double> point;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; each (i, v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { each (i, v) is >> i; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n;
+  while (cin >> n) {
+    vector<pair<lli, lli>> v(n);
+    cin >> v;
+    sort(v.begin(), v.end(), [] (auto i, auto j) { return i.first + i.second > j.first + j.second; });
+    lli sum = 0;
+    for (int i = 0; i < v.size(); ++i) {
+      if (i % 2) sum -= v[i].second;
+      else sum += v[i].first;
+    }
+    cout << sum << endl;
+  }
+
+  return 0;
+}
