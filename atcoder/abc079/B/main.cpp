f65a460b8b77bae719e59594d95e3c412b7e4fcd
Johniel <motohazu@mercari.com>
2019-05-17 19:42:35 +0900

    abc079ABCD

---
+++ atcoder/abc079/B/main.cpp
@@ -0,0 +1,40 @@
+// atcoder/abc079/B/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+typedef long long int lli;
+typedef unsigned long long ull;
+typedef complex<double> point;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; each (i, v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { each (i, v) is >> i; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  lli dp[100];
+  dp[0] = 2;
+  dp[1] = 1;
+  for (int i = 2; i < 100; ++i) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+
+  int n;
+  while (cin >> n) cout << dp[n] << endl;
+
+  return 0;
+}
