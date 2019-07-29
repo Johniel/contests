a2b61553783c57a93ab293d517bc95a18dceddf5
Johniel <motohazu@mercari.com>
2019-07-29 21:38:14 +0900

    abc078ABCD

---
+++ atcoder/abc078/D/main.cpp
@@ -0,0 +1,41 @@
+// atcoder/abc078/D/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+typedef long long int lli;
+typedef unsigned long long ull;
+typedef complex<double> point;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; each (i, v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { each (i, v) is >> i; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  lli n, z, w;
+  while (cin >> n >> z >> w) {
+    vector<lli> a(n);
+    cin >> a;
+    if (n == 1) cout << abs(a.back() - w) << endl;
+    else {
+      // sort(a.begin(), a.end());
+      cout << max(abs(a[n - 1] - a[n - 2]), abs(a[n - 1] - w)) << endl;
+    }
+  }
+
+  return 0;
+}
