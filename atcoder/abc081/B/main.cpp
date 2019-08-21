06626caba3a4ba0ad474c8fa19263d5975609bf6
Johniel <motohazu@mercari.com>
2019-08-21 17:33:53 +0900

    abc81,83,86B

---
+++ atcoder/abc081/B/main.cpp
@@ -0,0 +1,46 @@
+// atcoder/abc081/B/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+typedef long long int lli;
+typedef unsigned long long ull;
+typedef complex<double> point;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; each (i, v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { each (i, v) is >> i; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n;
+  while (cin >> n) {
+    vector<int> a(n);
+    cin >> a;
+    int mn = 1 << 29;
+    each (i, a) {
+      int x = (i ^ 1) ? 0 : (1 << 29);
+      for (int j = 0; j < 30; ++j) {
+        if (i & (1 << j)) break;
+        x = j + 1;
+      }
+      setmin(mn, x);
+    }
+    cout << mn << endl;
+  }
+
+  return 0;
+}
---
+++ atcoder/abc086/B/main.cpp
@@ -0,0 +1,42 @@
+// atcoder/abc086/B/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+typedef long long int lli;
+typedef unsigned long long ull;
+typedef complex<double> point;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; each (i, v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { each (i, v) is >> i; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  string s, t;
+  while (cin >> s >> t) {
+    s += t;
+    int n;
+    sscanf(s.c_str(), "%d", &n);
+    bool f = false;
+    for (int i = 0; i * i <= n; ++i) {
+      f = f || (i * i == n);
+    }
+    cout << (f ? "Yes" : "No") << endl;
+  }
+
+  return 0;
+}
