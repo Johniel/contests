484e9ec7ffe77cafa10de8fb549d5b511e60a168
Johniel <johniel.s.m@gmail.com>
2019-12-19 02:47:54 +0900

    abc028ABC

---
+++ atcoder/abc028/C/main.cpp
@@ -0,0 +1,43 @@
+// atcoder/abc028/C/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+typedef long long int lli;
+typedef unsigned long long ull;
+typedef complex<double> point;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; each (i, v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { each (i, v) is >> i; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  vector<int> v(5);
+  while (cin >> v) {
+    set<int> s;
+    sort(v.begin(), v.end());
+    do {
+      s.insert(v[0] + v[1] + v[2]);
+    } while (next_permutation(v.begin(), v.end()));
+    auto itr = s.rbegin();
+    ++itr;
+    ++itr;
+    cout << *itr << endl;
+  }
+
+  return 0;
+}
