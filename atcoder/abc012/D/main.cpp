6792c8a10ea0c92c24a28826a173ef01477d280a
Johniel <johniel.s.m@gmail.com>
2019-12-31 16:00:54 +0900

    abc012

---
+++ atcoder/abc012/D/main.cpp
@@ -0,0 +1,64 @@
+// atcoder/abc012/D/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+typedef long long int lli;
+typedef unsigned long long ull;
+typedef complex<double> point;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; each (i, v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { each (i, v) is >> i; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n, m;
+  while (cin >> n >> m) {
+    const int N = 300 + 5;
+    int g[N][N];
+    fill(&g[0][0], &g[N - 1][N - 1] + 1, 1 << 29);
+    for (int i = 0; i < m; ++i) {
+      int a, b, t;
+      cin >> a >> b >> t;
+      --a;
+      --b;
+      g[a][b] = g[b][a] = t;
+    }
+    for (int i = 0; i < n; ++i) {
+      g[i][i] = 0;
+    }
+    for (int k = 0; k < N; ++k) {
+      for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+          setmin(g[i][j], g[i][k] + g[k][j]);
+        }
+      }
+    }
+
+    int mn = 1 << 29;
+    for (int i = 0; i < n; ++i) {
+      int mx = 0;
+      for (int j = 0; j < n; ++j) {
+        setmax(mx, g[i][j]);
+      }
+      setmin(mn, mx);
+    }
+    cout << mn << endl;
+  }
+
+  return 0;
+}
