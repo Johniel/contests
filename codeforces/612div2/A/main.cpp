3b947fd331bc138c20ed6e9283588da44e59b3c8
Johniel <johniel.s.m@gmail.com>
2020-01-06 03:30:14 +0900

    612div2ABCD

---
+++ codeforces/612div2/A/main.cpp
@@ -0,0 +1,51 @@
+// codeforces/612div2/A/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+typedef long long int lli;
+typedef unsigned long long ull;
+typedef complex<double> point;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; each (i, v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { each (i, v) is >> i; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  int tc;
+  while (cin >> tc) {
+    while (tc--) {
+      int n;
+      string s;
+      cin >> n >> s;
+      reverse(s.begin(), s.end());
+      int mx = 0;
+      int cnt = 0;
+      each (c, s) {
+        if (c == 'P') {
+          ++cnt;
+        } else {
+          setmax(mx, cnt);
+          cnt = 0;
+        }
+      }
+      cout << mx << endl;
+    }
+  }
+
+  return 0;
+}
