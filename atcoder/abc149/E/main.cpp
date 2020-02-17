34c57f80ceb2c33a3239b10dbf3c884fbdf553a0
Johniel <motohazu@mercari.com>
2020-02-17 20:03:11 +0900

    abc149E

---
+++ atcoder/abc149/E/main.cpp
@@ -0,0 +1,75 @@
+// atcoder/abc149/E/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+template<typename P, typename Q> inline pair<Q, P> reverse(pair<P, Q> p) { return make_pair(p.second, p.first); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr lli mod = 1e9 + 7;
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n;
+  lli m;
+  while (cin >> n >> m) {
+    vector<lli> a(n);
+    cin >> a;
+    sort(a.begin(), a.end());
+
+    const int N = 1e5 + 5;
+    lli acc[N];
+    fill(acc, acc + N, 0);
+    for (int i = 0; i < a.size(); ++i) {
+      acc[i + 1] = acc[i] + a[i];
+    }
+
+    auto fn = [&] (lli x) {
+      lli sum = 0;
+      lli z = 0;
+      each (i, a) {
+        auto itr = lower_bound(a.begin(), a.end(), x - i);
+        lli j = itr - a.begin();
+        sum += n - j;
+        z += (acc[n] - acc[j]) + ((n - j) * i);
+      }
+      return make_pair(sum, z);
+    };
+
+    lli small = 0;
+    lli large = 2 * 1e5 + 5;
+    while (small + 1 < large) {
+      lli mid = (small + large) / 2;
+      if (fn(mid).first < m) large = mid;
+      else small = mid;
+    }
+
+    auto p = fn(small);
+    cout << p.second - (p.first - m) * small << endl;
+  }
+
+  return 0;
+}
