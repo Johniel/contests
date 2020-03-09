171ab115c61e45a1a7be0d38df9f9839d2fb954c
Johniel <motohazu@mercari.com>
2020-03-09 19:46:40 +0900

    arc040C

---
+++ atcoder/arc040/C/main.cpp
@@ -0,0 +1,67 @@
+// atcoder/arc040/C/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+int di[] = {0, 1, -1, 0, 1, -1, 1, -1};
+int dj[] = {1, 0, 0, -1, 1, -1, -1, 1};
+constexpr lli mod = 1e9 + 7;
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n;
+  while (cin >> n) {
+    char g[n][n];
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < n; ++j) {
+        cin >> g[i][j];
+      }
+    }
+
+    int cnt = 0;
+    for (int i = 0; i < n; ++i) {
+      bool f = false;
+      for (int j = n - 1; 0 <= j; --j) {
+        if (f) g[i][j] = 'o';
+        if (g[i][j] == '.') {
+          g[i][j] = 'o';
+          f = true;
+          ++cnt;
+          for (int k = j; k < n; ++k) {
+            if (i + 1 < n) {
+              g[i + 1][k] = 'o';
+            }
+          }
+        }
+      }
+    }
+    cout << cnt << endl;
+  }
+
+  return 0;
+}
