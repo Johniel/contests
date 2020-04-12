6bafa05bfbcfb7bfab0b416bc47abb38ada16c12
Johniel <johniel.s.m@gmail.com>
2020-04-12 20:13:32 +0900

    arc023A

---
+++ atcoder/arc023/A/main.cpp
@@ -0,0 +1,51 @@
+// atcoder/arc023/A/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+
+int main(int argc, char *argv[])
+{
+  std::ios_base::sync_with_stdio(0);
+  std::cin.tie(0);
+  std::cout.setf(std::ios_base::fixed);
+  std::cout.precision(15);
+
+  int y, m, d;
+  while (cin >> y >> m >> d) {
+    auto fn = [] (int y, int m, int d) {
+      return 365 * y + y / 4 - y / 100 + y / 400 + 306 * (m + 1) / 10 + d - 429;
+    };
+    if (m == 1) { m = 13; --y; }
+    if (m == 2) { m = 14; --y; }
+
+    cout << fn(2014, 5, 17) - fn(y, m, d) << endl;
+  }
+
+  return 0;
+}
