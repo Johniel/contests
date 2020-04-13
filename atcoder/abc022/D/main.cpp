ec7746c5a0de545fdad049a6a6e85e1641094858
Johniel <motohazu@mercari.com>
2020-04-13 20:18:42 +0900

    abc022D

--- atcoder/abc022/D/main.cpp
+++ atcoder/abc022/D/main.cpp
@@ -9,129 +9,139 @@
 
 using namespace std;
 
-typedef long long int lli;
-typedef unsigned long long ull;
-typedef complex<double> point;
-
 template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
 template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
-template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; each (i, v) os << i << ","; os << ")"; return os; }
-template<typename T> istream& operator >> (istream& is, vector<T>& v) { each (i, v) is >> i; return is; }
-
-template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "("; each (i, s) os << i << ","; os << ")"; return os; }
-template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "("; each (i, m) os << i << ","; os << ")"; return os; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
 
 template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
 template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
-template<typename P, typename Q> inline pair<Q, P> swap(pair<P, Q> p) { return make_pair(p.second, p.first); }
 
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
 constexpr lli mod = 1e9 + 7;
 
-struct Edge {
-  int src, dst;
-  Edge() { src = dst = -1; }
-  Edge(int s, int d) : src(s), dst(d) {}
-  Edge rev(void) const { return Edge(dst, src); }
+namespace std{
+  bool operator < (point a, point b){
+    if(a.real() != b.real())return a.real() < b.real();
+    return a.imag() < b.imag();
+  }
 };
 
-inline bool operator < (const Edge& a, const Edge& b)
+bool cmp_real(const point &a, const point &b)
 {
-  if (a.src != b.src) return a.src < b.src;
-  return a.dst < b.dst;
+  if(a.real() != b.real())return a.real() < b.real();
+  return a.imag() < b.imag();
 }
 
-inline bool operator == (const Edge& a, const Edge& b)
+point normal(point v)
 {
-  return a.dst == b.dst && a.src == b.src;
+  return v * point(0, -1);
 }
 
-inline bool operator != (const Edge& a, const Edge& b)
+double dot(point a, point b)
 {
-  return !(a == b);
+  return (a.real() * b.real() + a.imag() * b.imag());
 }
 
-inline ostream& operator << (ostream& os, Edge e)
+double cross(point a, point b)
 {
-  os << make_pair(e.src, e.dst);
-  return os;
+  return (a.real() * b.imag() - a.imag() * b.real());
 }
 
-const int N = 100 + 10;
-vector<Edge> g[N];
-
-// do not use parallel edge
-map<Edge, lli> cap;
-
-void add_edge(int src, int dst, lli c)
+namespace CCW{
+  enum{ RIGHT = 1, LEFT = -1, FRONT = -2, BACK = +2, OTHER = 0 };
+};
+int ccw(point a, point b, point c)
 {
-  Edge a(src, dst);
-  Edge b = a.rev();
-  g[a.src].push_back(a);
-  g[b.src].push_back(b);
-  cap[a] += c;
-  return ;
+  b -= a;
+  c -= a;
+  if( cross(b, c) < 0 )return CCW::RIGHT;
+  if( cross(b, c) > 0 )return CCW::LEFT;
+  if( dot(b, c) < 0 )return CCW::BACK;
+  if( norm(b) < norm(c) )return CCW::FRONT;
+  return CCW::OTHER;
 }
 
-lli max_flow(int src, int snk)
+//uva 10065
+vector<point> andrew(vector<point> v)
 {
-  lli sum = 0;
-  while (true) {
-    Edge path[N] = {Edge(), };
-    set<int> vis({src});
-    queue<int> q;
-    for (q.push(src); q.size(); q.pop()) {
-      each (e, g[q.front()]) {
-        if (!vis.count(e.dst) && cap[e]) {
-          q.push(e.dst);
-          vis.insert(e.dst);
-          path[e.dst] = e;
-        }
+  if(v.size() < 3)return vector<point>();
+  sort(v.begin(), v.end(), cmp_real);
+  vector<point> r[2];
+  for(int i=0; i<2; ++i){
+    for(int j=0; j<v.size(); ++j){
+      r[i].push_back( v[j] );
+      while(2 < r[i].size()){
+        vector<point>::iterator itr = r[i].end();
+        if( ccw( *(itr-3), *(itr-2), *(itr-1) ) == CCW::RIGHT )break;
+        else r[i].erase( itr-2 );
       }
     }
-    if (path[snk] == Edge()) {
-      break;
+    for(int j=0; j<v.size(); ++j){
+      // v[j].imag() *= -1;
+      v[j] = point(v[j].real(), v[j].imag() * -1);
     }
-    lli mn = 1LL << 60;
-    for (Edge e = path[snk]; e != Edge(); e = path[e.src]) {
-      mn = min(mn, cap[e]);
-    }
-    for (Edge e = path[snk]; e != Edge(); e = path[e.src]) {
-      cap[e] -= mn;
-      cap[e.rev()] += mn;
-    }
-    sum += mn;
   }
-
-  return sum;
+  for(int i=r[1].size()-2; 0<i; --i){
+    // r[1][i].imag() *= -1;
+    r[1][i] = point(r[1][i].real(), r[1][i].imag() * -1);
+    r[0].push_back( r[1][i] );
+  }
+  return r[0];
 }
-
-auto min_cut = max_flow;
 
 int main(int argc, char *argv[])
 {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
+  std::ios_base::sync_with_stdio(0);
+  std::cin.tie(0);
+  std::cout.setf(std::ios_base::fixed);
+  std::cout.precision(15);
 
-  int n, m, e;
-  while (cin >> n >> m >> e) {
-    fill(g, g + N, vector<Edge>());
-    cap.clear();
-
-    vector<int> girl(m);
-    cin >> girl;
-
-    for (int i = 0; i < e; ++i) {
-      int a, b;
-      cin >> a >> b;
-      add_edge(a, b, 1);
-      add_edge(b, a, 1);
+  int n;
+  while (cin >> n) {
+    vec<point> a, b;
+    for (int i = 0; i < n; ++i) {
+      double x, y;
+      cin >> x >> y;
+      a.push_back(point(x, y));
+    }
+    for (int i = 0; i < n; ++i) {
+      double x, y;
+      cin >> x >> y;
+      b.push_back(point(x, y));
     }
 
-    const int snk = N - 1;
-    each (i, girl) {
-      add_edge(i, snk, 1);
+    a = andrew(a);
+    b = andrew(b);
+
+    auto fn = [] (point a, point b) {
+      double r = a.real() - b.real();
+      double i = a.imag() - b.imag();
+      return sqrt(r * r + i * i);
+    };
+
+    double x = 0;
+    for (int i = 0; i < a.size(); ++i) {
+      int j = (i + 1) % a.size();
+      x += fn(a[i], a[j]);
     }
-    cout << min_cut(0, snk) << endl;
+
+    double y = 0;
+    for (int i = 0; i < b.size(); ++i) {
+      int j = (i + 1) % b.size();
+      y += fn(b[i], b[j]);
+    }
+
+    cout << y / x << endl;
   }
 
   return 0;
