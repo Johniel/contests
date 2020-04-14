20dd440294429f70869b99327e7feb411232e22f
Johniel <motohazu@mercari.com>
2020-04-14 19:57:31 +0900

    agc037A

---
+++ atcoder/agc037/A/main.cpp
@@ -0,0 +1,71 @@
+// atcoder/agc037/A/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+
+int main(int argc, char *argv[])
+{
+  std::ios_base::sync_with_stdio(0);
+  std::cin.tie(0);
+  std::cout.setf(std::ios_base::fixed);
+  std::cout.precision(15);
+
+  str s;
+  while (cin >> s) {
+    const int N = 2 * 1e5 + 5;
+    static int dp[N][3];
+    fill(&dp[0][0], &dp[N - 1][3 - 1] + 1, -(1 << 29));
+    dp[0][0] = 0;
+    for (int i = 0; i < s.size(); ++i) {
+      for (int j = 0; j <= 2; ++j) {
+        if (dp[i][j] < 0) continue;
+        str prev;
+        for (int k = 0; k < j; ++k) {
+          if (0 <= i - k - 1) {
+            prev += s[i - k - 1];
+          } else {
+            prev = "";
+            break;
+          }
+        }
+        reverse(prev.begin(), prev.end());
+        str t;
+        for (int k = 0; k < 2 && i + k < s.size(); ++k) {
+          t += s[i + k];
+          if (prev != t) {
+            setmax(dp[i + k + 1][t.size()], dp[i][j] + 1);
+          }
+        }
+      }
+    }
+    cout << max(dp[s.size()][1], dp[s.size()][2]) << endl;
+  }
+
+  return 0;
+}
