0d29839a8b7c74fd709795720ca5dc288c7ba161
Johniel <johniel.s.m@gmail.com>
2020-05-17 23:48:10 +0900

    abc168ABCDE

---
+++ atcoder/abc168/E/main.cpp
@@ -0,0 +1,213 @@
+// atcoder/abc168/E/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+
+const int N = 2 * 1e5 + 5;
+namespace math {
+  lli fact[N], finv[N], inv[N];
+
+  void init()
+  {
+    fact[0] = fact[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < N; i++){
+      fact[i] = fact[i - 1] * i % mod;
+      inv[i] = mod - inv[mod % i] * (mod / i) % mod;
+      finv[i] = finv[i - 1] * inv[i] % mod;
+    }
+  }
+
+  lli mod_comb(int n, int k)
+  {
+    assert(n < N && k < N);
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fact[n] * (finv[k] * finv[n - k] % mod) % mod;
+  }
+
+  lli extgcd(lli a, lli b, lli& x, lli& y)
+  {
+    lli g = a;
+    x = 1;
+    y = 0;
+    if (b != 0) {
+      g = extgcd(b, a % b, y, x);
+      y -= (a / b) * x;
+    }
+    return g;
+  }
+
+  lli mod_inverse(lli a, lli m)
+  {
+    lli x, y;
+    extgcd(a, m, x, y);
+    return (m + x % m) % m;
+  }
+  // nHm=n+m-1Cm
+
+  lli mod_pow(lli n, int p)
+  {
+    if (p == 0) return 1;
+    if (p == 1) return n;
+    lli m = mod_pow(n, p / 2);
+    m *= m;
+    m %= mod;
+    if (p % 2) m = (m * n) % mod;
+    return m;
+  }
+};
+
+// from: https://qiita.com/drken/items/3b4fdf0a78e7a138cd9a
+template<int MOD> struct Fp {
+  long long val;
+  constexpr Fp(long long v = 0) noexcept : val(v % MOD) {
+    if (val < 0) val += MOD;
+  }
+  constexpr int getmod() { return MOD; }
+  constexpr Fp operator - () const noexcept {
+    return val ? MOD - val : 0;
+  }
+  constexpr Fp operator + (const Fp& r) const noexcept { return Fp(*this) += r; }
+  constexpr Fp operator - (const Fp& r) const noexcept { return Fp(*this) -= r; }
+  constexpr Fp operator * (const Fp& r) const noexcept { return Fp(*this) *= r; }
+  constexpr Fp operator / (const Fp& r) const noexcept { return Fp(*this) /= r; }
+  constexpr Fp& operator += (const Fp& r) noexcept {
+    val += r.val;
+    if (val >= MOD) val -= MOD;
+    return *this;
+  }
+  constexpr Fp& operator -= (const Fp& r) noexcept {
+    val -= r.val;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr Fp& operator *= (const Fp& r) noexcept {
+    val = val * r.val % MOD;
+    return *this;
+  }
+  constexpr Fp& operator /= (const Fp& r) noexcept {
+    long long a = r.val, b = MOD, u = 1, v = 0;
+    while (b) {
+      long long t = a / b;
+      a -= t * b; swap(a, b);
+      u -= t * v; swap(u, v);
+    }
+    val = val * u % MOD;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr bool operator == (const Fp& r) const noexcept {
+    return this->val == r.val;
+  }
+  constexpr bool operator != (const Fp& r) const noexcept {
+    return this->val != r.val;
+  }
+  friend constexpr ostream& operator << (ostream &os, const Fp<MOD>& x) noexcept {
+    return os << x.val;
+  }
+  friend constexpr Fp<MOD> modpow(const Fp<MOD> &a, long long n) noexcept {
+    if (n == 0) return 1;
+    auto t = modpow(a, n / 2);
+    t = t * t;
+    if (n & 1) t = t * a;
+    return t;
+  }
+};
+using mint = Fp<mod>;
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.setf(ios_base::fixed);
+  cout.precision(15);
+
+  math::init();
+
+  int n;
+  while (cin >> n) {
+    vec<pair<lli, lli>> v(n);
+    cin >> v;
+
+    int x0 = 0;
+    int y0 = 0;
+    int xy0 = 0;
+    {
+      vec<pair<lli, lli>> u;
+      each (i, v) {
+        if (i.first == 0 && i.second == 0) ++xy0;
+        else if (i.first == 0) ++x0;
+        else if (i.second == 0) ++y0;
+        else {
+          lli g = __gcd(i.first, i.second);
+          i.first /= g;
+          i.second /= g;
+          u.push_back(i);
+        }
+      }
+      v = u;
+    }
+
+    map<pair<lli, lli>, lli> m;
+    each (i, v) ++m[i];
+
+    vec<pair<lli, lli>> u;
+    each (i, m) u.push_back(i.first);
+
+    set<pair<lli, lli>> vis;
+    mint x = 1;
+    for (int i = 0; i < u.size(); ++i) {
+      auto a = u[i];
+      auto b = make_pair(a.second, -a.first);
+      auto c = make_pair(-a.second, a.first);
+      auto d = make_pair(-a.first, -a.second);
+
+      if (vis.count(a) || vis.count(b) || vis.count(c) || vis.count(d)) continue;
+      vis.insert(a);
+      vis.insert(b);
+      vis.insert(c);
+      vis.insert(d);
+
+      mint p = math::mod_pow(2, m[a] + m[d]);
+      mint q = math::mod_pow(2, m[b] + m[c]) - 1;
+      x *= p + q;
+    }
+
+    mint p = math::mod_pow(2, x0);
+    mint q = math::mod_pow(2, y0) - 1;
+    x *= p + q;
+
+    x -= 1;
+    cout << x + xy0 << endl;
+  }
+
+  return 0;
+}
