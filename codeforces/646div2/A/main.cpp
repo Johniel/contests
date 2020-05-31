533a6046e1fa3ccf369db500b275fe18c49dbc06
Johniel <johniel.s.m@gmail.com>
2020-06-01 02:47:17 +0900

    646div2ABC

---
+++ codeforces/646div2/A/main.cpp
@@ -0,0 +1,69 @@
+// codeforces/646div2/A/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.setf(ios_base::fixed);
+  cout.precision(15);
+
+  int _;
+  cin >> _;
+
+  int n, x;
+  while (cin >> n >> x) {
+    vec<int> a(n);
+    cin >> a;
+    each (i, a) i %= 2;
+
+    int o = count(a.begin(), a.end(), 1);
+    int e = count(a.begin(), a.end(), 0);
+
+    if (!o) {
+      cout << "No" << endl;
+      continue;
+    }
+
+    --o;
+    --x;
+
+    int mn = min(x - x % 2, o - o % 2);
+    x -= mn;
+    mn = min(x, e);
+    x -= mn;
+
+    if (x) cout << "No" << endl;
+    else cout << "Yes" << endl;
+  }
+
+  return 0;
+}
---
+++ codeforces/646div2/B/main.cpp
@@ -0,0 +1,79 @@
+// codeforces/646div2/B/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+
+template<typename T>
+struct PrefixSum {
+  vector<T> sum;
+  PrefixSum(vector<T> v) {
+    sum.push_back(0);
+    for (int i = 0; i < v.size(); ++i) {
+      sum.push_back(sum.back() + v[i]);
+    }
+  }
+  T operator () (size_t begin, size_t end) const {
+    assert(begin <= end);
+    return sum[end] - sum[begin];
+  }
+};
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.setf(ios_base::fixed);
+  cout.precision(15);
+
+  int _;
+  cin >> _;
+
+  str s;
+  while (cin >> s) {
+    vec<int> _1;
+    vec<int> _0;
+
+    each (c, s) {
+      _0.push_back(c == '0');
+      _1.push_back(c == '1');
+    }
+
+    PrefixSum<int> z(_0);
+    PrefixSum<int> o(_1);
+
+    int mn = 1 << 29;
+    for (int i = 0; i < s.size(); ++i) {
+      setmin(mn, z(0, i) + o(i, s.size()));
+      setmin(mn, o(0, i) + z(i, s.size()));
+    }
+    cout << mn << endl;
+  }
+
+  return 0;
+}
