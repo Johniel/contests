5652aa2925eb701db47b9257975a6954da9e6f67
Yuya Watanabe <yuya.watanabe@legalforce.co.jp>
2020-06-24 19:13:11 +0900

    B

---
+++ atcoder/nikkei2019-2-qual/B/main.cpp
@@ -0,0 +1,71 @@
+// atcoder/nikkei2019-2-qual/B/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 998244353;
+
+namespace math {
+  lli mod_pow(lli n, int p)
+  {
+    if (p == 0) return 1;
+    if (p == 1) return n;
+    lli m = mod_pow(n, p / 2);
+    m *= m;
+    m %= mod;
+    if (p % 2) m = (m * n) % mod;
+    return m;
+  }
+};
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.setf(ios_base::fixed);
+  cout.precision(15);
+
+  int n;
+  while (cin >> n) {
+    vec<int> v(n);
+    cin >> v;
+
+    map<int, int> m;
+    each (i, v) ++m[i];
+
+    lli x = (v[0] == 0 && m[0] == 1);
+
+    sort(v.begin(), v.end());
+    for (int i = 1; i <= v.back(); ++i) {
+      if (m[i] == 0) x = 0;
+      (x *= math::mod_pow(m[i - 1], m[i])) %= mod;
+    }
+    cout << x << endl;
+  }
+
+  return 0;
+}
