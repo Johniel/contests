18f30678deacf2298e4cddaa9811be038de5bae5
Yuya Watanabe <yuya.watanabe@legalforce.co.jp>
2020-07-14 19:44:32 +0900

    C

---
+++ atcoder/aising2019/C/main.cpp
@@ -0,0 +1,81 @@
+// atcoder/aising2019/C/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.setf(ios_base::fixed);
+  cout.precision(15);
+
+  int h, w;
+  while (cin >> h >> w) {
+    const int H = 400 + 5;
+    const int W = 400 + 5;
+    static char g[H][W];
+    for (int i = 0; i < h; ++i) {
+      for (int j = 0; j < w; ++j) {
+        cin >> g[i][j];
+      }
+    }
+
+    static bool vis[H][W];
+    fill(&vis[0][0], &vis[H - 1][W - 1] + 1, false);
+
+    lli x = 0;
+    map<char, lli> m;
+    function<void(int, int)> rec = [&] (int i, int j) {
+      vis[i][j] = true;
+      ++m[g[i][j]];
+      for (int d = 0; d < 4; ++d) {
+        int ni = i + di[d];
+        int nj = j + dj[d];
+        unless (0 <= ni && ni < h) continue;
+        unless (0 <= nj && nj < w) continue;
+        if (g[i][j] != g[ni][nj] && !vis[ni][nj]) {
+          rec(ni, nj);
+        }
+      }
+    };
+    for (int i = 0; i < h; ++i) {
+      for (int j = 0; j < w; ++j) {
+        if (!vis[i][j]) {
+          m.clear();
+          rec(i, j);
+          x += m['.'] * m['#'];
+        }
+      }
+    }
+    cout << x << endl;
+  }
+
+  return 0;
+}
