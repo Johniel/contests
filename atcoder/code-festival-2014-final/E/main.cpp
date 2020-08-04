d70eb236f9b62b42f8922c832e6546bec78e6563
Yuya Watanabe <yuya.watanabe@legalforce.co.jp>
2020-08-04 19:22:53 +0900

    E

---
+++ atcoder/code-festival-2014-final/E/main.cpp
@@ -0,0 +1,66 @@
+// atcoder/code-festival-2014-final/E/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.setf(ios_base::fixed);
+  cout.precision(15);
+
+  int n;
+  while (cin >> n) {
+    vec<int> a(n);
+    cin >> a;
+
+    const int N = 3000 + 3;
+    const int UP = 0;
+    const int DOWN = 1;
+    const int OTHER = 2;
+    static int dp[N][N][3];
+    fill(&dp[0][0][0], &dp[N - 1][N - 1][3 - 1] + 1, 0);
+    for (int i = 0; i < a.size(); ++i) {
+      for (int j = 0; j < a.size(); ++j) {
+        setmax(dp[i + 1][i][DOWN], dp[i][j][OTHER] + 1);
+        setmax(dp[i + 1][i][UP], dp[i][j][OTHER] + 1);
+        if (a[i] > a[j]) setmax(dp[i + 1][i][DOWN], dp[i][j][UP] + 1);
+        if (a[i] < a[j]) setmax(dp[i + 1][i][UP], dp[i][j][DOWN] + 1);
+        setmax(dp[i + 1][j][DOWN], dp[i][j][DOWN]);
+        setmax(dp[i + 1][j][UP], dp[i][j][UP]);
+      }
+    }
+    int mx = *max_element(&dp[0][0][0], &dp[N - 1][N - 1][3 - 1] + 1);
+    if (mx < 3) mx = 0;
+    cout << mx << endl;
+  }
+
+  return 0;
+}
