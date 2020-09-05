f64dc22415e7d81f64097cf77e70113d30297c07
Johniel <johniel.s.m@gmail.com>
2020-09-06 04:33:58 +0900

    ABCDE

---
+++ codeforces/667div3/E/main.cpp
@@ -0,0 +1,72 @@
+// codeforces/667div3/E/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.setf(ios_base::fixed);
+  cout.precision(15);
+
+  int _;
+  cin >> _;
+
+  int n, k;
+  while (cin >> n >> k) {
+    vec<int> x(n), y(n);
+    cin >> x >> y;
+
+    sort(x.begin(), x.end());
+
+    int mx1 = 0;
+    int mx2 = 0;
+    deque<int> a, b;
+    each (i, x) {
+      b.push_back(i);
+      vec<int> v;
+      while (b.size() && b.front() + k < b.back()) {
+        v.push_back(b.front());
+        b.pop_front();
+      }
+      each (j, v) {
+        a.push_back(j);
+        while (a.size() && a.front() + k < a.back()) {
+          a.pop_front();
+        }
+        setmax<int>(mx2, a.size());
+      }
+      setmax<int>(mx1, mx2 + b.size());
+    }
+    cout << mx1 << endl;
+  }
+
+  return 0;
+}
