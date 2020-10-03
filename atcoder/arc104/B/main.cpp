36dba7e8fc6db7228a79e4704306427034388d90
Johniel <johniel.s.m@gmail.com>
2020-10-04 00:37:06 +0900

    AB

---
+++ atcoder/arc104/B/main.cpp
@@ -0,0 +1,81 @@
+// atcoder/arc104/B/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+
+template<typename T>
+struct PrefixSum {
+  vector<T> sum;
+  PrefixSum(vector<T> v) {
+    sum.push_back(0);
+    for (int i = 0; i < v.size(); ++i) {
+      sum.push_back(sum.back() + v[i]);
+    }
+  }
+  T operator () (size_t begin, size_t end) const {
+    assert(begin <= end);
+    return sum[end] - sum[begin];
+  }
+};
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.setf(ios_base::fixed);
+  cout.precision(15);
+
+  int n;
+  str s;
+  while (cin >> n >> s) {
+    vec<int> a(n, 0), c(n, 0), t(n, 0), g(n, 0);
+    for (int i = 0; i < n; ++i) {
+      char x = s[i];
+      if (x == 'A') a[i] = 1;
+      if (x == 'C') c[i] = 1;
+      if (x == 'T') t[i] = 1;
+      if (x == 'G') g[i] = 1;
+    }
+
+    PrefixSum<int> A(a);
+    PrefixSum<int> C(c);
+    PrefixSum<int> T(t);
+    PrefixSum<int> G(g);
+
+    int y = 0;
+    for (int i = 0; i < n; ++i) {
+      for (int j = i+1; j <= n; ++j) {
+        y += (A(i, j) == T(i, j)) && (C(i, j) == G(i, j));
+      }
+    }
+    cout << y << endl;
+  }
+
+  return 0;
+}
