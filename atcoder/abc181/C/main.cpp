f7aee5b5ac758604dedf0dc3873b4c463e794305
Johniel <johniel.s.m@gmail.com>
2020-11-02 01:24:52 +0900

    ABCDEF

---
+++ atcoder/abc181/C/main.cpp
@@ -0,0 +1,84 @@
+// atcoder/abc181/C/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+
+double dot(point a, point b)
+{
+  return (a.real() * b.real() + a.imag() * b.imag());
+}
+
+double cross(point a, point b)
+{
+  return (a.real() * b.imag() - a.imag() * b.real());
+}
+
+namespace CCW{
+  enum{ RIGHT = 1, LEFT = -1, FRONT = -2, BACK = +2, OTHER = 0 };
+};
+int ccw(point a, point b, point c)
+{
+  b -= a;
+  c -= a;
+  if( cross(b, c) < 0 )return CCW::RIGHT;
+  if( cross(b, c) > 0 )return CCW::LEFT;
+  if( dot(b, c) < 0 )return CCW::BACK;
+  if( norm(b) < norm(c) )return CCW::FRONT;
+  return CCW::OTHER;
+}
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.setf(ios_base::fixed);
+  cout.precision(15);
+
+  int n;
+  while (cin >> n) {
+    vec<pair<int, int>> v(n);
+    cin >> v;
+
+    bool f = false;
+    for (int i = 0; i < v.size(); ++i) {
+      for (int j = 0; j < v.size(); ++j) {
+        for (int k = 0; k < v.size(); ++k) {
+          if (i == j || i == k || j == k) continue;
+          point a(v[i].first, v[i].second);
+          point b(v[j].first, v[j].second);
+          point c(v[k].first, v[k].second);
+          f = f || ccw(a, b, c) == CCW::FRONT;
+        }
+      }
+    }
+    cout << (f ? "Yes" : "No") << endl;
+  }
+
+  return 0;
+}
