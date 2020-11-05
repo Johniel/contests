3e3dcd768c1d23f59e83ff006a4f920ae3df9538
Johniel <johniel.s.m@gmail.com>
2020-11-05 19:18:40 +0900

    B

---
+++ atcoder/arc026/B/main.cpp
@@ -0,0 +1,59 @@
+// atcoder/arc026/B/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.setf(ios_base::fixed);
+  cout.precision(15);
+
+  lli n;
+  while (cin >> n) {
+    set<lli> vis;
+    for (lli i = 1; i * i <= n; ++i) {
+      if (n % i == 0) {
+        if (i != n) vis.insert(i);
+        if (n / i != n) vis.insert(n / i);
+      }
+    }
+    lli sum = 0;
+    each (i, vis) {
+      sum += i;
+      setmin(sum, n + 1);
+    }
+    if (sum == n) cout << "Perfect" << endl;
+    if (sum < n) cout << "Deficient" << endl;
+    if (sum > n) cout << "Abundant" << endl;
+  }
+
+  return 0;
+}
