d10279c8d0bdcec4fd1212ec1d9762edd1843bad
Johniel <johniel.s.m@gmail.com>
2020-11-19 19:51:07 +0900

    E

---
+++ atcoder/s8pc-2/E/main.cpp
@@ -0,0 +1,79 @@
+// atcoder/s8pc-2/E/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.setf(ios_base::fixed);
+  cout.precision(15);
+
+  str s;
+  while (cin >> s) {
+    vec<int> v;
+    for (int i = 0; i < s.size(); ++i) {
+      v.push_back(i);
+    }
+    sort(v.begin(), v.end(), [&] (int a, int b) {
+      for (int i = 0; i + a < s.size() && i + b < s.size(); ++i) {
+        if (s[i + a] != s[i + b]) return s[i + a] < s[i + b];
+      }
+      return a < b;
+    });
+
+    lli x = 0;
+    for (int i = 1; i <= s.size(); ++i) {
+      x += (s.size() - i + 1) * i;
+    }
+
+    // each (i, v) {
+    //   const char* c = s.c_str();
+    //   cout << str(c+i) << endl;
+    // }
+    // cout << x << endl;
+
+    for (int i = 0; i + 1 < v.size(); ++i) {
+      const char* c = s.c_str();
+      // cout << str(c+v[i]) << "," << str(c+v[i + 1]) << endl;
+
+      lli cnt = 0;
+      for (int j = 0; v[i] + j < s.size() && v[i + 1] + j < s.size(); ++j) {
+        if (s[v[i] + j] != s[v[i + 1] + j]) break;
+        ++cnt;
+      }
+      // cout << "diff: " << cnt << endl;
+      x -= cnt * (cnt + 1) / 2;
+    }
+    cout << x << endl;
+  }
+
+  return 0;
+}
