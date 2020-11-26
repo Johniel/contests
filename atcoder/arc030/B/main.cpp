01d26caa9fa54a3b497e77f153350a022427fb65
Johniel <johniel.s.m@gmail.com>
2020-11-26 19:20:28 +0900

    B

---
+++ atcoder/arc030/B/main.cpp
@@ -0,0 +1,77 @@
+// atcoder/arc030/B/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+
+const int N = 100 + 3;
+vec<int> g[N];
+vec<int> h;
+pair<int, int> rec(int curr, int prev)
+{
+  int cnt = h[curr];
+  int sum = 0;
+  each (next, g[curr]) {
+    if (next == prev) continue;
+    auto p = rec(next, curr);
+    if (p.first) {
+      cnt += p.first;
+      sum += 2 + p.second;
+    }
+  }
+  return make_pair(cnt, sum);
+}
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.setf(ios_base::fixed);
+  cout.precision(15);
+
+  int n, x;
+  while (cin >> n >> x) {
+    --x;
+    h.resize(n);
+    cin >> h;
+    fill(g, g + N, vec<int>());
+    for (int i = 0; i < n - 1; ++i) {
+      int a, b;
+      cin >> a >> b;
+      --a;
+      --b;
+      g[a].push_back(b);
+      g[b].push_back(a);
+    }
+    auto p = rec(x, -1);
+    if (p.first) cout << p.second << endl;
+    else cout << 0 << endl;
+  }
+
+  return 0;
+}
