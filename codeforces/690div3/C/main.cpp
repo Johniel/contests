dd3ed851ac6a835a34725be8faf47c81ebd2119d
Johniel <johniel.s.m@gmail.com>
2020-12-16 01:40:51 +0900

    ABCDE

---
+++ codeforces/690div3/C/main.cpp
@@ -0,0 +1,66 @@
+// codeforces/690div3/C/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.setf(ios_base::fixed);
+  cout.precision(15);
+
+  int _;
+  cin >> _;
+
+  const lli inf = 1LL << 60;
+  const int N = 70;
+  lli mn[N];
+  fill(mn, mn + N, inf);
+  str s = "0123456789";
+  sort(s.begin(), s.end());
+  do {
+    if (s.front() == '0') continue;
+    lli x = 0;
+    lli y = 0;
+    for (int i = 0; i < s.size(); ++i) {
+      x = x * 10 + (s[i] - '0');
+      y += s[i] - '0';
+      setmin(mn[y], x);
+    }
+  } while (next_permutation(s.begin(), s.end()));
+  replace(mn, mn + N, inf, -(1LL));
+
+  int x;
+  while (cin >> x) {
+    cout << mn[x] << endl;
+  }
+
+  return 0;
+}
