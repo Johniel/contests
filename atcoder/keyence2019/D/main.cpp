8bfbb395f25ac97620a230462f7cb920bc9bb668
Johniel <johniel.s.m@gmail.com>
2021-01-28 20:07:47 +0900

    D

---
+++ atcoder/keyence2019/D/main.cpp
@@ -0,0 +1,89 @@
+// atcoder/keyence2019/D/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.setf(ios_base::fixed);
+  cout.precision(15);
+
+
+  int h, w;
+  while (cin >> h >> w) {
+    vec<int> a(h);
+    vec<int> b(w);
+    cin >> a >> b;
+
+    each (i, a) --i;
+    each (i, b) --i;
+
+    map<int, int> x, y;
+    for (int i = 0; i < a.size(); ++i) {
+      x[a[i]] = i;
+    }
+    for (int i = 0; i < b.size(); ++i) {
+      y[b[i]] = i;
+    }
+
+    vec<int> v;
+    for (int i = 0; i < h * w; ++i) {
+      v.push_back(i);
+    }
+    reverse(v.begin(), v.end());
+
+    const int N = 1000 + 3;
+    static int g[N][N];
+
+    lli z = (x.size() == a.size()) && (y.size() == b.size());
+    int p = 0;
+    int q = 0;
+    int w = 0;
+    each (k, v) {
+      if (x.count(k) && y.count(k)) {
+        ++p;
+        ++q;
+      } else if (x.count(k)) {
+        ++p;
+        (z *= q) %= mod;
+      } else if (y.count(k)) {
+        ++q;
+        (z *= p) %= mod;
+      } else {
+        (z *= (p * q) - w) %= mod;
+      }
+      ++w;
+    }
+    cout << z << endl;
+  }
+
+  return 0;
+}
