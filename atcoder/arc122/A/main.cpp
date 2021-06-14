5e96b0f1d1c0cb5e9a5e0bfe85767f8a660dd6ba
Johniel <johniel.s.m@gmail.com>
2021-06-14 20:14:11 +0900

    A

---
+++ atcoder/arc122/A/main.cpp
@@ -0,0 +1,138 @@
+// atcoder/arc122/A/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+
+// from: https://qiita.com/drken/items/3b4fdf0a78e7a138cd9a
+template<int MOD> struct Fp {
+  long long val;
+  constexpr Fp(long long v = 0) noexcept : val(v % MOD) {
+    if (val < 0) val += MOD;
+  }
+  constexpr int getmod() { return MOD; }
+  constexpr Fp operator - () const noexcept {
+    return val ? MOD - val : 0;
+  }
+  constexpr Fp operator + (const Fp& r) const noexcept { return Fp(*this) += r; }
+  constexpr Fp operator - (const Fp& r) const noexcept { return Fp(*this) -= r; }
+  constexpr Fp operator * (const Fp& r) const noexcept { return Fp(*this) *= r; }
+  constexpr Fp operator / (const Fp& r) const noexcept { return Fp(*this) /= r; }
+  constexpr Fp& operator += (const Fp& r) noexcept {
+    val += r.val;
+    if (val >= MOD) val -= MOD;
+    return *this;
+  }
+  constexpr Fp& operator -= (const Fp& r) noexcept {
+    val -= r.val;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr Fp& operator *= (const Fp& r) noexcept {
+    val = val * r.val % MOD;
+    return *this;
+  }
+  constexpr Fp& operator /= (const Fp& r) noexcept {
+    long long a = r.val, b = MOD, u = 1, v = 0;
+    while (b) {
+      long long t = a / b;
+      a -= t * b; swap(a, b);
+      u -= t * v; swap(u, v);
+    }
+    val = val * u % MOD;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr bool operator == (const Fp& r) const noexcept {
+    return this->val == r.val;
+  }
+  constexpr bool operator != (const Fp& r) const noexcept {
+    return this->val != r.val;
+  }
+  friend constexpr ostream& operator << (ostream &os, const Fp<MOD>& x) noexcept {
+    return os << x.val;
+  }
+  friend constexpr Fp<MOD> modpow(const Fp<MOD> &a, long long n) noexcept {
+    if (n == 0) return 1;
+    auto t = modpow(a, n / 2);
+    t = t * t;
+    if (n & 1) t = t * a;
+    return t;
+  }
+};
+using mint = Fp<mod>;
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.setf(ios_base::fixed);
+  cout.precision(15);
+
+  int n;
+  while (cin >> n) {
+    vec<lli> _a(n);
+    cin >> _a;
+    vec<mint> a;
+    each (i, _a) a.push_back(mint(i));
+    const int N = 1e5 + 3;
+    static mint dp[N][2];
+    fill(&dp[0][0], &dp[N - 1][2 - 1] + 1, mint(0));
+    const int pos = 0;
+    const int neg = 1;
+    dp[0][pos] = mint(1);
+    for (int i = 0; i < a.size(); ++i) {
+      dp[i + 1][pos] += dp[i][pos];
+      dp[i + 1][neg] += dp[i][pos];
+      dp[i + 1][pos] += dp[i][neg];
+    }
+    mint x = 0;
+    x += (dp[a.size() - 1][pos] + dp[a.size() - 1][neg]) * a[0];
+    for (int i = 1; i < a.size(); ++i) {
+      // +a, +b, -c, +d
+      // +a, -b, +c, +d
+      // +a, +b, +c, -d
+      // +a, -b, +c, -d
+      {
+        mint pref = dp[i][neg];
+        int rem = a.size() - 1 - i;
+        mint suff = rem ? dp[rem - 1][pos] + dp[rem - 1][neg] : mint(1);
+        x -= a[i] * (pref * suff);
+      }
+      {
+        mint pref = dp[i][pos];
+        int rem = a.size() - 1 - i;
+        mint suff = dp[rem][pos] + dp[rem][neg];
+        x += a[i] * (pref * suff);
+      }
+    }
+    cout << x << endl;
+  }
+
+  return 0;
+}
