0c07bb81e857282513f6ccfb5342db20ea7aaf1a
Johniel <johniel.s.m@gmail.com>
2021-06-14 20:35:18 +0900

    B

---
+++ atcoder/arc122/B/main.cpp
@@ -0,0 +1,71 @@
+// atcoder/arc122/B/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+
+double trinaly_search(double small, double large, function<double(double)>fn)
+{
+  for (int i = 0; i < 100; ++i) {
+    double left = (large - small) / 3 + small;
+    double right = (large - small) * 2 / 3 + small;
+    if (fn(right) > fn(left)) large = right;
+    else small = left;
+  }
+
+  return small;
+}
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.setf(ios_base::fixed);
+  cout.precision(15);
+
+  int n;
+  while (cin >> n) {
+    vec<double> a(n);
+    cin >> a;
+
+    function<double(double)> fn = [&] (double x) {
+      double sum = 0;
+      each (i, a) {
+        sum += x + i - min(i, 2.0 * x);
+        // cout << x + i - min(i, 2.0 * x) << endl;
+      }
+      return sum / a.size();
+    };
+    double x = trinaly_search(0, 1e10, fn);
+    // cout << endl;
+    cout << fn(x) << endl;
+    // break;
+  }
+
+  return 0;
+}
