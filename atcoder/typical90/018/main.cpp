6a11c6b72447bf39f8fe4942485a30407963b795
Johniel <johniel.s.m@gmail.com>
2021-10-19 18:37:37 +0900

    018

---
+++ atcoder/typical90/018/main.cpp
@@ -0,0 +1,65 @@
+// atcoder/typical90/018/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.setf(ios_base::fixed);
+  cout.precision(15);
+
+  double T, L, x, y, q;
+  while (cin >> T >> L >> x >> y >> q) {
+    while (q--) {
+      double e;
+      cin >> e;
+      double th = (2.0 * M_PI) / T * e;
+
+      double a, b, c;
+      a = 0;
+      b = 0;
+      c = -L/2.0;
+
+      double tmp_b = b * cos(-th) - c * sin(-th);
+      double tmp_c = b * sin(-th) + c * cos(-th);
+
+      b = tmp_b;
+      c = tmp_c;
+
+      c += L/2.0;
+
+      double w = hypot(a - x, b - y);
+      cout << 360.0 * atan(abs(c) / w) / (2.0 * M_PI) << endl;
+    }
+  }
+
+  return 0;
+}
