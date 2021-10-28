2070a0d54c5e60ae5c701809ade5b3712955255c
Johniel <johniel.s.m@gmail.com>
2021-10-28 18:30:34 +0900

    063

---
+++ atcoder/typical90/063/main.cpp
@@ -0,0 +1,67 @@
+// atcoder/typical90/063/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.setf(ios_base::fixed);
+  cout.precision(15);
+
+  int h, w;
+  while (cin >> h >> w) {
+    int g[h][w];
+    for (int i = 0; i < h; ++i) {
+      for (int j = 0; j < w; ++j) {
+        cin >> g[i][j];
+      }
+    }
+    map<vec<int>, int> m;
+    int mx = 0;
+    for (int j = 0; j < w; ++j) {
+      for (int bit = 1; bit < (1 << h); ++bit) {
+        vec<int> v;
+        for (int i = 0; i < h; ++i) {
+          if (bit & (1 << i)) v.push_back(g[i][j]);
+          else v.push_back(-1);
+        }
+        auto s = set<int>(v.begin(), v.end());
+        s.erase(-1);
+        if (s.size() == 1) {
+          setmax(mx, m[v] += __builtin_popcount(bit));
+        }
+      }
+    }
+    cout << mx << endl;
+  }
+
+  return 0;
+}
