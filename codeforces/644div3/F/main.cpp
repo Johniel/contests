4d1fed066c17fe049f37a6672bcf402e67a6c404
Johniel <johniel.s.m@gmail.com>
2021-11-15 20:47:38 +0900

    F

---
+++ codeforces/644div3/F/main.cpp
@@ -0,0 +1,75 @@
+// codeforces/644div3/F/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.setf(ios_base::fixed);
+  cout.precision(15);
+
+  int _;
+  cin >> _;
+
+  int n, m;
+  while (cin >> n >> m) {
+    vec<str> v(n);
+    cin >> v;
+
+    try {
+      each (i, v) {
+        for (int j = 0; j < i.size(); ++j) {
+          for (char c = 'a'; c <= 'z'; ++c) {
+            str s = i;
+            s[j] = c;
+            bool f = true;
+            each (k, v) {
+              int d = 0;
+              for (int p = 0; p < s.size(); ++p) {
+                d += (s[p] != k[p]);
+              }
+              f = f && (d <= 1);
+            }
+            if (f) {
+              cout << s << endl;
+              throw "";
+            }
+          }
+        }
+      }
+      cout << -1 << endl;
+    }
+    catch (const char* e) {
+    }
+  }
+
+  return 0;
+}
