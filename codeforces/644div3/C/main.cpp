5b4b319364ba3c3097bab03acef82ea8a8080d28
Johniel <johniel.s.m@gmail.com>
2021-11-15 21:13:03 +0900

    C

---
+++ codeforces/644div3/C/main.cpp
@@ -0,0 +1,72 @@
+// codeforces/644div3/C/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.setf(ios_base::fixed);
+  cout.precision(15);
+
+  int _;
+  cin >> _;
+
+  int n;
+  while (cin >> n) {
+    vec<int> a(n);
+    cin >> a;
+    map<int, int> m;
+    each (i, a) ++m[i % 2];
+
+    if (m[0] % 2 == 0 && m[1] % 2 == 0) {
+      cout << "Yes" << endl;
+      continue;
+    }
+    if (m[0] % 2 == 1 && m[1] % 2 == 1) {
+      bool f = false;
+      for (int i = 0; i < a.size(); ++i) {
+        for (int j = i + 1; j < a.size(); ++j) {
+          if (abs(a[i] - a[j]) == 1) {
+            f = true;
+            i = j = (1 << 29);
+          }
+        }
+      }
+      if (f) {
+        cout << "Yes" << endl;
+        continue;
+      }
+    }
+    cout << "No" << endl;
+  }
+
+  return 0;
+}
