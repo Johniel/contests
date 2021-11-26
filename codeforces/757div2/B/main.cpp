e1b8faf85bec4ac8b973745cb9ef5ca85945c5df
Johniel <johniel.s.m@gmail.com>
2021-11-26 22:30:48 +0900

    AB

---
+++ codeforces/757div2/B/main.cpp
@@ -0,0 +1,74 @@
+// codeforces/757div2/B/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.setf(ios_base::fixed);
+  cout.precision(15);
+
+  int _;
+  cin >> _;
+
+  int n;
+  while (cin >> n) {
+    vec<lli> a(n);
+    cin >> a;
+
+    vec<lli> v;
+    for (int i = 1; i <= n; ++i) {
+      v.push_back(-i);
+      v.push_back(+i);
+    }
+    sort(v.begin(), v.end(), [] (int a, int b) { return abs(a) > abs(b); });
+
+    vec<pair<int, int>> b;
+    for (int i = 0; i < a.size(); ++i) {
+      b.push_back(make_pair(a[i], i));
+    }
+    sort(b.begin(), b.end());
+    reverse(b.begin(), b.end());
+
+    lli z = 0;
+    vec<int> u(n+1, -999);
+    u[0] = 0;
+    for (int i = 0; i < n; ++i) {
+      u[b[i].second + 1] = v.back();
+      z += 2 * abs(v.back()) * b[i].first;
+      v.pop_back();
+    }
+    cout << z << endl;
+    each (i, u) cout << i << ' '; cout << endl;
+  }
+
+  return 0;
+}
