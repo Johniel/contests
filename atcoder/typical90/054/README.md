4d6cb07e3ad30380dcfae3b4268c01f9740baf5c
Johniel <johniel.s.m@gmail.com>
2021-12-01 01:15:02 +0900

    054

---
+++ atcoder/typical90/054/README.md
@@ -0,0 +1,2 @@
+# 競プロ典型 90 問 054 - Takahashi Number（★6）
+何も考えずBFS。
---
+++ atcoder/typical90/054/main.cpp
@@ -0,0 +1,80 @@
+// atcoder/typical90/054/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.setf(ios_base::fixed);
+  cout.precision(15);
+
+  int n, m;
+  while (cin >> n >> m) {
+    vec<int> g[n];
+    vec<int> v[m];
+    for (int i = 0; i < m; ++i) {
+      int k;
+      cin >> k;
+      v[i].resize(k);
+      cin >> v[i];
+      each (j, v[i]) {
+        --j;
+        g[j].push_back(i);
+      }
+    }
+    const int inf = 1 << 29;
+    vec<int> t(n, inf);
+    t[0] = 0;
+    queue<int> q;
+    set<int> vis1;
+    set<int> vis2;
+    vis2.insert(0);
+    q.push(0);
+    while (q.size()) {
+      int curr = q.front();
+      q.pop();
+      each (next, g[curr]) {
+        if (vis1.count(next)) continue;
+        vis1.insert(next);
+        each (k, v[next]) {
+          if (vis2.count(k)) continue;
+          vis2.insert(k);
+          setmin(t[k], t[curr] + 1);
+          q.push(k);
+        }
+      }
+    }
+    each (i, t) if (i == inf) i = -1;
+    each (i, t) cout << i << endl;
+  }
+
+  return 0;
+}
