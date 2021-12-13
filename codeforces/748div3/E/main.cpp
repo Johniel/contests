4d0235cdedf22dfa2c21460e83741315e239f550
Johniel <johniel.s.m@gmail.com>
2021-12-13 19:44:38 +0900

    E

---
+++ codeforces/748div3/E/main.cpp
@@ -0,0 +1,96 @@
+// codeforces/748div3/E/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <iostream>
+#include <cassert>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <vector>
+#include <complex>
+#include <array>
+#include <queue>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+
+const int N = 4 * 1e5 + 3;
+vec<int> g[N];
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.setf(ios_base::fixed);
+  cout.precision(15);
+
+  int _;
+  cin >> _;
+
+  int n, k;
+  while (cin >> n >> k) {
+    fill(g, g + n, vec<int>());
+    for (int i = 0; i < n - 1; ++i) {
+      int a, b;
+      cin >> a >> b;
+      --a;
+      --b;
+      g[a].push_back(b);
+      g[b].push_back(a);
+    }
+    static int z[N];
+    static int deg[N];
+    fill(deg, deg + n, 0);
+    fill(z, z + n, 0);
+    for (int i = 0; i < n; ++i) {
+      deg[i] = g[i].size();
+    }
+    queue<int> q;
+    for (int i = 0; i < n; ++i) {
+      if (deg[i] == 1) {
+        q.push(i);
+        deg[i] = 0;
+      }
+    }
+    while (q.size()) {
+      int curr = q.front();
+      q.pop();
+      each (next, g[curr]) {
+        if (1 < deg[next]) {
+          setmax(z[next], z[curr] + 1);
+          if (--deg[next] == 1) q.push(next);
+        }
+      }
+    }
+    int cnt = 0;
+    for (int i = 0; i < n; ++i) {
+      cnt += (k <= z[i]);
+    }
+    cout << cnt << endl;
+  }
+
+  return 0;
+}
