b846ab6aa3711ee1245ecc4df5f7bc9c210c4dea
Johniel <johniel.s.m@gmail.com>
2022-04-20 01:34:31 +0900

    ABC

---
+++ codeforces/783div2/B/main.cpp
@@ -0,0 +1,66 @@
+// codeforces/783div2/B/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+// #define endl "\n"
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+// constexpr lli mod = 998244353;
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.setf(ios_base::fixed);
+  cout.precision(15);
+
+  int _;
+  cin >> _;
+
+  int n, m;
+  while (cin >> n >> m) {
+    vec<lli> a(n);
+    cin >> a;
+    sort(a.begin(), a.end());
+    vec<lli> b;
+    lli next = 0;
+    for (int i = 0; i < a.size(); ++i) {
+      b.push_back(next);
+      const int j = (i + 1) % a.size();
+      next += max(a[i], a[j]) + 1;
+      if (m < b.back()) break;
+    }
+    bool f = (b.back() < m) && (b.back() + max(a.front(), a.back()) + 1 <= m);
+    for (int i = 0; i + 1 < b.size(); ++i) {
+      f = f && (b[i] < b[i + 1]);
+    }
+    // cout << b << endl;
+    cout << (f ? "YES" : "NO") << endl;
+  }
+
+  return 0;
+}
