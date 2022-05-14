d0a91d2b9edce2d9f44abb3c258faf9cf89c228d
Johniel <johniel.s.m@gmail.com>
2022-05-14 23:03:56 +0900

    F

---
+++ atcoder/abc251/F/main.cpp
@@ -0,0 +1,84 @@
+// atcoder/abc251/F/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+// #define endl "\n"
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+// constexpr lli mod = 998244353;
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.setf(ios_base::fixed);
+  cout.precision(15);
+
+  int n, m;
+  while (cin >> n >> m) {
+    vec<int> g[n];
+    for (int i = 0; i < m; ++i) {
+      int a, b;
+      cin >> a >> b;
+      --a;
+      --b;
+      g[a].push_back(b);
+      g[b].push_back(a);
+    }
+    queue<int> q;
+    vec<pair<int, int>> a;
+    set<int> vis;
+    vis.insert(0);
+    for (q.push(0); q.size(); q.pop()) {
+      int curr = q.front();
+      each (next, g[curr]) {
+        unless (vis.count(next)) {
+          vis.insert(next);
+          q.push(next);
+          a.push_back({curr, next});
+        }
+      }
+    }
+    vec<pair<int, int>> b;
+    vis.clear();
+    function<void(int)> rec = [&] (int curr) {
+      vis.insert(curr);
+      each (next, g[curr]) {
+        unless (vis.count(next)) {
+          b.push_back({curr, next});
+          rec(next);
+        }
+      }
+      return ;
+    };
+    rec(0);
+    each (i, b) cout << i.first + 1 << ' ' << i.second + 1 << endl;
+    each (i, a) cout << i.first + 1 << ' ' << i.second + 1 << endl;
+  }
+
+  return 0;
+}
