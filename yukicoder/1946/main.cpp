6a89e44285857f08952b332c980310796fcc5b6f
Johniel <johniel.s.m@gmail.com>
2022-05-20 21:53:10 +0900

    F

---
+++ yukicoder/1946/main.cpp
@@ -0,0 +1,79 @@
+// yukicoder/1946/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+// #define endl "\n"
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+// constexpr lli mod = 998244353;
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.setf(ios_base::fixed);
+  cout.precision(15);
+
+  int n, m;
+  while (cin >> n >> m) {
+    vec<int> a(m);
+    cin >> a;
+
+    const int N = 2 * 1e5 + 3;
+    static bool ex[N];
+    fill(ex, ex + N, true);
+    each (i, a) ex[i] = false;
+
+    static bool vis[N];
+    fill(vis, vis + N, true);
+    for (lli i = n; 0 < i; --i) {
+      for (lli j = 1; j * j <= i; ++j) {
+        if (i % j == 0) {
+          lli k = i / j;
+          vis[j] = !vis[j];
+          if (j != k) vis[k] = !vis[k];
+        }
+      }
+    }
+    int z = 0;
+    for (lli i = n; 0 < i; --i) {
+      if (ex[i] != vis[i]) {
+        ++z;
+        for (lli j = 1; j * j <= i; ++j) {
+          if (i % j == 0) {
+            lli k = i / j;
+            vis[j] = !vis[j];
+            if (j != k) vis[k] = !vis[k];
+          }
+        }
+      }
+    }
+    cout << z << endl;
+  }
+
+  return 0;
+}
