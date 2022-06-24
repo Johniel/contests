8e781df4637b41bfd7be5c36d861ea53542d2336
Johniel <johniel.s.m@gmail.com>
2022-06-25 02:33:57 +0900

    F

---
+++ atcoder/abc256/F/main.cpp
@@ -0,0 +1,169 @@
+// atcoder/abc256/F/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+// #define endl "\n"
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using point = complex<double>;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 998244353;
+
+template<typename T>
+struct SegTree {
+  int n;
+  int origin_size;
+  vector<T> v;
+  using F = function<T(T, T)>;
+  F fn;
+  T e;
+  SegTree(int n_, T e_, F fn_) {
+    origin_size = n_;
+    e = e_;
+    fn = fn_;
+    n = 1;
+    while (n < n_) n *= 2;
+    v.resize(2 * n - 1, e);
+  }
+
+  void update(size_t k, T a) {
+    k += n - 1;
+    v[k] = a;
+    while (k > 0) {
+      k = (k - 1) / 2;
+      v[k] = fn(v[k * 2 + 1], v[k * 2 + 2]);
+    }
+    return ;
+  }
+
+  T operator [] (size_t idx) const {
+    return v.at(idx + n - 1);
+  }
+
+  inline T operator () () const {
+    return query(0, origin_size, 0, 0, n);
+  }
+
+  inline T operator () (size_t a) const {
+    return query(a, a+1, 0, 0, n);
+  }
+
+  inline T operator () (size_t a, size_t b) const {
+    return query(a, b, 0, 0, n);
+  }
+
+  inline T query(size_t a, size_t b) const {
+    assert(a < b);
+    assert(b <= origin_size);
+    return query(a, b, 0, 0, n);
+  }
+
+  T query(size_t a, size_t b, size_t k, size_t l, size_t r) const {
+    if (r <= a || b <= l) return e;
+    if (a <= l && r <= b) return v.at(k);
+
+    T vl = query(a, b, k * 2 + 1, l, (l + r) / 2);
+    T vr = query(a, b, k * 2 + 2, (l + r) / 2, r);
+
+    return fn(vl, vr);
+  }
+};
+
+namespace math {
+  lli extgcd(lli a, lli b, lli& x, lli& y)
+  {
+    lli g = a;
+    x = 1;
+    y = 0;
+    if (b != 0) {
+      g = extgcd(b, a % b, y, x);
+      y -= (a / b) * x;
+    }
+    return g;
+  }
+
+  lli mod_inverse(lli a, lli m)
+  {
+    lli x, y;
+    extgcd(a, m, x, y);
+    return (m + x % m) % m;
+  }
+};
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.setf(ios_base::fixed);
+  cout.precision(15);
+
+  int n, q;
+  while (cin >> n >> q) {
+    vec<lli> a(n);
+    cin >> a;
+    a.insert(a.begin(), 0);
+
+    SegTree<lli> x(a.size(), 0, [] (lli a, lli b) { return (a + b) % mod; });
+    SegTree<lli> y(a.size(), 0, [] (lli a, lli b) { return (a + b) % mod; });
+    SegTree<lli> z(a.size(), 0, [] (lli a, lli b) { return (a + b) % mod; });
+
+    auto update = [&] (lli j, lli aj) {
+      x.update(j, j * j % mod * aj % mod);
+      y.update(j, j * aj % mod);
+      z.update(j, aj % mod);
+      return ;
+    };
+
+    for (int j = 1; j < a.size(); ++j) {
+      update(j, a[j]);
+    }
+
+    while (q--) {
+      int op;
+      cin >> op;
+
+      if (op == 1) {
+        lli j, w;
+        cin >> j >> w;
+        w %= mod;
+        a[j] = w;
+        update(j, a[j]);
+      }
+      if (op == 2) {
+        lli i;
+        cin >> i;
+        const lli d2 = math::mod_inverse(2, mod);
+        lli w = 0;
+        w += d2 * x(0, i + 1) % mod;
+        w -= (2 * i + 3) * y(0, i + 1) % mod * d2 % mod;
+        w += (i + 1) * (i + 2) % mod * d2 % mod * z(0, i + 1) % mod;
+        cout << (w + mod) % mod << endl;
+      }
+    }
+  }
+
+  return 0;
+}
