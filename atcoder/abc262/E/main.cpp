620b1052026532342d97596587816385b8294812
Johniel <johniel.s.m@gmail.com>
2022-08-01 00:01:42 +0900

    ABCDE

---
+++ atcoder/abc262/E/main.cpp
@@ -0,0 +1,107 @@
+// atcoder/abc262/E/main.cpp
+// author: @___Johniel
+// github: https://github.com/johniel/
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+// #define endl "\n"
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 998244353;
+
+const int N = 2 * 1e5 + 3;
+namespace math {
+  lli fact[N], finv[N], inv[N];
+
+  void init()
+  {
+    fact[0] = fact[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < N; i++){
+      fact[i] = fact[i - 1] * i % mod;
+      inv[i] = mod - inv[mod % i] * (mod / i) % mod;
+      finv[i] = finv[i - 1] * inv[i] % mod;
+    }
+  }
+
+  lli mod_comb(int n, int k)
+  {
+    assert(n < N && k < N);
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fact[n] * (finv[k] * finv[n - k] % mod) % mod;
+  }
+
+  inline lli nCk(int n, int k)
+  {
+    return mod_comb(n, k);
+  }
+
+  // n個の箱にm個の何かを分配する場合の数
+  inline lli nHm(int n, int m)
+  {
+    return nCk(n + m - 1, m);
+  }
+};
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.setf(ios_base::fixed);
+  cout.precision(15);
+
+  math::init();
+
+  int n, m, k;
+  while (cin >> n >> m >> k) {
+    static int deg[N];
+    fill(deg, deg + N, 0);
+    for (int i = 0; i < m; ++i) {
+      int a, b;
+      cin >> a >> b;
+      --a;
+      --b;
+      ++deg[a];
+      ++deg[b];
+    }
+    int o = 0;
+    int e = 0;
+    for (int i = 0; i < n; ++i) {
+      o += (deg[i] % 2 == 1);
+      e += (deg[i] % 2 == 0);
+    }
+    lli z = 0;
+    for (int i = 0; i <= k; ++i) {
+      if ((o - i) % 2 == 0 && 0 <= i) {
+        (z += math::mod_comb(o, i) * math::mod_comb(e, k - i) % mod) %= mod;
+      }
+    }
+    cout << z << endl;
+  }
+
+  return 0;
+}
