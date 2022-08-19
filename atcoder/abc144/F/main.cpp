ce530a3fa6663aeea4973b7324f33b18e065b64f
Johniel <johniel.s.m@gmail.com>
2022-08-19 18:33:30 +0900

    F

---
+++ atcoder/abc144/F/main.cpp
@@ -0,0 +1,91 @@
+// github.com/Johniel/contests
+// atcoder/abc144/F/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+// #define endl "\n"
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+// constexpr lli mod = 998244353;
+
+
+const int N = 600 + 4;
+vec<int> g[N];
+double memo[N];
+pair<int, int> taboo;
+double rec(int curr)
+{
+  double& ret = memo[curr];
+  unless (ret < 0) return ret;
+  double s = 0;
+  int cnt = 0;
+  each (next, g[curr]) {
+    if (make_pair(curr, next) == taboo) continue;
+    ++cnt;
+    s += rec(next);
+  }
+  return ret = s / cnt + 1.0;
+}
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.setf(ios_base::fixed);
+  cout.precision(15);
+
+  int n, m;
+  while (cin >> n >> m) {
+    fill(g, g + N, vec<int>());
+    for (int i = 0; i < m; ++i) {
+      int a, b;
+      cin >> a >> b;
+      --a;
+      --b;
+      g[a].push_back(b);
+    }
+    fill(memo, memo + N, -1); memo[n - 1] = 0;
+    taboo = {-1, -1};
+    double z = rec(0);
+    vec<pair<int, int>> v;
+    for (int i = 0; i < n; ++i) {
+      unless (1 < g[i].size()) continue;
+      int k = g[i].front();
+      each (j, g[i]) {
+        if (memo[k] < memo[j]) k = j;
+      }
+      v.push_back({i, k});
+    }
+    each (i, v) {
+      fill(memo, memo + N, -1); memo[n - 1] = 0;
+      taboo = i;
+      setmin(z, rec(0));
+    }
+    cout << z << endl;
+  }
+
+  return 0;
+}
