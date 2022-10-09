371f1ab8854a474331ec0cfbac70da2b0c323871
Johniel <johniel.s.m@gmail.com>
2022-10-09 19:04:40 +0900

    2093

---
+++ yukicoder/2093/main.cpp
@@ -0,0 +1,61 @@
+// github.com/Johniel/contests
+// yukicoder/2093/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+// #define endl "\n"
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+// constexpr lli mod = 998244353;
+
+int main(int argc, char *argv[])
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.setf(ios_base::fixed);
+  cout.precision(15);
+
+  int n, l;
+  while (cin >> n >> l) {
+    const int N = 1000 + 3;
+    int dp[N][N];
+    fill(&dp[0][0], &dp[N - 1][N - 1] + 1, -(1 << 29));
+    dp[0][0] = 0;
+
+    vec<pair<int, int>> v(n);
+    cin >> v;
+
+    for (int i = 0; i < v.size(); ++i) {
+      for (int j = 0; j < N; ++j) {
+        if (j + v[i].first <= l) setmax(dp[i + 1][j + v[i].first], dp[i][j] + v[i].second);
+        setmax(dp[i + 1][j], dp[i][j]);
+      }
+    }
+    cout << *max_element(&dp[0][0], &dp[N - 1][N - 1] + 1) << endl;
+  }
+
+  return 0;
+}
