d378d05f1a5221f9d84f2129533d51bbe17f0924
Johniel <johniel.s.m@gmail.com>
2022-10-13 19:21:17 +0900

    D

---
+++ codeforces/826div3/D/main.cpp
@@ -0,0 +1,148 @@
+// github.com/Johniel/contests
+// codeforces/826div3/D/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+// #define endl "\n"
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void ___initio(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+// constexpr lli mod = 998244353;
+
+template<typename T>
+struct SegTree {
+  int n;
+  int origin_size;
+  vector<T> v;
+  using F = function<T(T, T)>;
+  F fn;
+  T e;
+  SegTree(int n_, T e_, F fn_) {
+    origin_size = n_;
+    e = e_;
+    fn = fn_;
+    n = 1;
+    while (n < n_) n *= 2;
+    v.resize(2 * n - 1, e);
+  }
+
+  void update(size_t k, T a) {
+    k += n - 1;
+    v[k] = a;
+    while (k > 0) {
+      k = (k - 1) / 2;
+      v[k] = fn(v[k * 2 + 1], v[k * 2 + 2]);
+    }
+    return ;
+  }
+
+  T operator [] (size_t idx) const {
+    return v.at(idx + n - 1);
+  }
+
+  inline T operator () () const {
+    return query(0, origin_size, 0, 0, n);
+  }
+
+  inline T operator () (size_t a) const {
+    return query(a, a+1, 0, 0, n);
+  }
+
+  inline T operator () (size_t a, size_t b) const {
+    return query(a, b, 0, 0, n);
+  }
+
+  inline T query(size_t a, size_t b) const {
+    assert(a < b);
+    assert(b <= origin_size);
+    return query(a, b, 0, 0, n);
+  }
+
+  T query(size_t a, size_t b, size_t k, size_t l, size_t r) const {
+    if (r <= a || b <= l) return e;
+    if (a <= l && r <= b) return v.at(k);
+
+    T vl = query(a, b, k * 2 + 1, l, (l + r) / 2);
+    T vr = query(a, b, k * 2 + 2, (l + r) / 2, r);
+
+    return fn(vl, vr);
+  }
+};
+
+int main(int argc, char *argv[])
+{
+
+  int _;
+  cin >> _;
+
+  int n;
+  while (cin >> n) {
+    const int inf = 1 << 29;
+    vec<int> a(n);
+    cin >> a;
+
+    SegTree<int> mx(a.size(), -inf, [] (auto x, auto y) { return max(x, y); });
+    SegTree<int> mn(a.size(), +inf, [] (auto x, auto y) { return min(x, y); });
+    for (int i = 0; i < n; ++i) {
+      mx.update(i, a[i]);
+      mn.update(i, a[i]);
+    }
+
+    function<int(int, int)> rec = [&] (int begin, int end) {
+      int z = 0;
+
+      if (end - begin == 1) return 0;
+
+      int mid = (end - begin) / 2 + begin;
+
+      pair<int, int> x, y;
+      x.first = mn(begin, mid);
+      x.second = mx(begin, mid);
+      y.first = mn(mid, end);
+      y.second = mx(mid, end);
+
+      unless (x < y) {
+        ++z;
+        swap(x, y);
+      }
+
+      unless (x.second - x.first + 1 == y.second - y.first + 1) return inf;
+      unless (x.second + 1 == y.first) return inf;
+
+      z += rec(begin, mid);
+      z += rec(mid, end);
+      return z;
+    };
+
+
+    int x = rec(0, a.size());
+    if (inf <= x) cout << -1 << endl;
+    else cout << x << endl;
+  }
+
+  return 0;
+}
