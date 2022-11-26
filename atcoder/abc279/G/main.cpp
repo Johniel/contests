d49f7f17fa28cdad54d7285ee1eb8396aebb97d8
Johniel <johniel.s.m@gmail.com>
2022-11-27 03:32:30 +0900

    G

---
+++ atcoder/abc279/G/main.cpp
@@ -0,0 +1,177 @@
+// github.com/Johniel/contests
+// atcoder/abc279/G/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+// #define endl "\n"
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void ___initio(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+template<typename T> using heap = priority_queue<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 998244353;
+
+template<typename T>
+struct SegTree {
+  int n;
+  int origin_size;
+  vector<T> v;
+  using F = function<T(T, T)>;
+  F fn;
+  T e;
+  SegTree(int n_, T e_, F fn_) {
+    origin_size = n_;
+    e = e_;
+    fn = fn_;
+    n = 1;
+    while (n < n_) n *= 2;
+    v.resize(2 * n - 1, e);
+  }
+
+  void update(size_t k, T a) {
+    k += n - 1;
+    v[k] = a;
+    while (k > 0) {
+      k = (k - 1) / 2;
+      v[k] = fn(v[k * 2 + 1], v[k * 2 + 2]);
+    }
+    return ;
+  }
+
+  T operator [] (size_t idx) const {
+    return v.at(idx + n - 1);
+  }
+
+  inline T operator () () const {
+    return query(0, origin_size, 0, 0, n);
+  }
+
+  inline T operator () (size_t a) const {
+    return query(a, a+1, 0, 0, n);
+  }
+
+  inline T operator () (size_t a, size_t b) const {
+    return query(a, b, 0, 0, n);
+  }
+
+  inline T query(size_t a, size_t b) const {
+    assert(a < b);
+    assert(b <= origin_size);
+    return query(a, b, 0, 0, n);
+  }
+
+  T query(size_t a, size_t b, size_t k, size_t l, size_t r) const {
+    if (r <= a || b <= l) return e;
+    if (a <= l && r <= b) return v.at(k);
+
+    T vl = query(a, b, k * 2 + 1, l, (l + r) / 2);
+    T vr = query(a, b, k * 2 + 2, (l + r) / 2, r);
+
+    return fn(vl, vr);
+  }
+};
+
+// from: https://qiita.com/drken/items/3b4fdf0a78e7a138cd9a
+template<int MOD> struct Fp {
+  long long val;
+  constexpr Fp(long long v = 0) noexcept : val(v % MOD) {
+    if (val < 0) val += MOD;
+  }
+  constexpr int getmod() { return MOD; }
+  constexpr Fp operator - () const noexcept {
+    return val ? MOD - val : 0;
+  }
+  constexpr Fp operator + (const Fp& r) const noexcept { return Fp(*this) += r; }
+  constexpr Fp operator - (const Fp& r) const noexcept { return Fp(*this) -= r; }
+  constexpr Fp operator * (const Fp& r) const noexcept { return Fp(*this) *= r; }
+  constexpr Fp operator / (const Fp& r) const noexcept { return Fp(*this) /= r; }
+  constexpr Fp& operator += (const Fp& r) noexcept {
+    val += r.val;
+    if (val >= MOD) val -= MOD;
+    return *this;
+  }
+  constexpr Fp& operator -= (const Fp& r) noexcept {
+    val -= r.val;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr Fp& operator *= (const Fp& r) noexcept {
+    val = val * r.val % MOD;
+    return *this;
+  }
+  constexpr Fp& operator /= (const Fp& r) noexcept {
+    long long a = r.val, b = MOD, u = 1, v = 0;
+    while (b) {
+      long long t = a / b;
+      a -= t * b; swap(a, b);
+      u -= t * v; swap(u, v);
+    }
+    val = val * u % MOD;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr bool operator == (const Fp& r) const noexcept {
+    return this->val == r.val;
+  }
+  constexpr bool operator != (const Fp& r) const noexcept {
+    return this->val != r.val;
+  }
+  friend constexpr ostream& operator << (ostream &os, const Fp<MOD>& x) noexcept {
+    return os << x.val;
+  }
+  friend constexpr Fp<MOD> modpow(const Fp<MOD> &a, long long n) noexcept {
+    if (n == 0) return 1;
+    auto t = modpow(a, n / 2);
+    t = t * t;
+    if (n & 1) t = t * a;
+    return t;
+  }
+};
+// constexpr lli mod = 1e9 + 7;
+using mint = Fp<mod>;
+
+int main(int argc, char *argv[])
+{
+  int n, k, _c;
+  while (cin >> n >> k >> _c) {
+    const mint c(_c);
+    const int N = 1e6 + 3;
+    SegTree<mint> seg1(N, mint(0), [] (auto a, auto b) { return a + b; });
+    SegTree<mint> seg2(N, mint(0), [] (auto a, auto b) { return a + b; });
+    for (int i = 0; i <= n; ++i) {
+      // 単色 + 別の色に切り替え + 片方の色を残す
+      int a = max(0, i - k + 2);
+      mint x = c + seg1(0, a) * (c - 1) + seg2(0, a);
+      // 別の色を加えて2色 + これまでの2色を継続
+      int b = max(0, i - (k - 1) + 1);
+      int d = max(0, i);
+      mint y = seg1(b, d) * (c - 1) + seg2(b, d);
+      seg1.update(i, x);
+      seg2.update(i, y);
+    }
+    cout << seg1[n-1] + seg2[n-1] << endl;
+  }
+  return 0;
+}
