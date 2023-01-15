b889dceb2ee27bffa9be53926a87a4b51c9e5300
Johniel <johniel.s.m@gmail.com>
2023-01-16 00:33:07 +0900

    F

---
+++ atcoder/abc285/F/main.cpp
@@ -0,0 +1,173 @@
+// github.com/Johniel/contests
+// atcoder/abc285/F/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+// #define endl "\n"
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void ___initio(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+template<typename T> using heap = priority_queue<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+// constexpr lli mod = 998244353;
+
+template<typename T>
+struct SegTree {
+  int n;
+  int origin_size;
+  vector<T> v;
+  using F = function<T(T, T)>;
+  F fn;
+  T e;
+  SegTree(int n_, T e_, F fn_) {
+    origin_size = n_;
+    e = e_;
+    fn = fn_;
+    n = 1;
+    while (n < n_) n *= 2;
+    v.resize(2 * n - 1, e);
+  }
+
+  void update(size_t k, T a) {
+    k += n - 1;
+    v[k] = a;
+    while (k > 0) {
+      k = (k - 1) / 2;
+      v[k] = fn(v[k * 2 + 1], v[k * 2 + 2]);
+    }
+    return ;
+  }
+
+  T operator [] (size_t idx) const {
+    return v.at(idx + n - 1);
+  }
+
+  inline T operator () () const {
+    return query(0, origin_size, 0, 0, n);
+  }
+
+  inline T operator () (size_t a) const {
+    return query(a, a+1, 0, 0, n);
+  }
+
+  inline T operator () (size_t a, size_t b) const {
+    return query(a, b, 0, 0, n);
+  }
+
+  inline T query(size_t a, size_t b) const {
+    assert(a <= b);
+    assert(b <= origin_size);
+    return query(a, b, 0, 0, n);
+  }
+
+  T query(size_t a, size_t b, size_t k, size_t l, size_t r) const {
+    if (r <= a || b <= l) return e;
+    if (a <= l && r <= b) return v.at(k);
+
+    T vl = query(a, b, k * 2 + 1, l, (l + r) / 2);
+    T vr = query(a, b, k * 2 + 2, (l + r) / 2, r);
+
+    return fn(vl, vr);
+  }
+};
+
+int main(int argc, char *argv[])
+{
+  int n;
+  str s;
+  int q;
+  while (cin >> n >> s >> q) {
+    SegTree<bool> seg(n, true, [&] (auto a, auto b) { return a && b; });
+
+    using A = array<int, 30>;
+    A E;
+    E.fill(0);
+
+    SegTree<A> occ(n, E, [] (A a, A b) {
+      A x;
+      for (int i = 0; i < x.size(); ++i) x[i] = a[i] + b[i];
+      return x;
+    });
+
+    for (int i = 0; i < s.size(); ++i) {
+      if (i + 1 < s.size()) seg.update(i, s[i] <= s[i + 1]);
+      A a;
+      a.fill(0);
+      ++a[s[i] - 'a'];
+      occ.update(i, a);
+    }
+
+    A m;
+    m.fill(0);
+    each (c, s) ++m[c - 'a'];
+
+    while (q--) {
+      int op;
+      cin >> op;
+
+      if (op == 1) {
+        int i;
+        char c;
+        cin >> i >> c;
+        --i;
+
+        --m[s[i] - 'a'];
+        s[i] = c;
+        ++m[s[i] - 'a'];
+
+        A a;
+        a.fill(0);
+        ++a[s[i] - 'a'];
+        occ.update(i, a);
+
+        if (i + 1 < s.size()) seg.update(i, s[i] <= s[i + 1]);
+        if (i) seg.update(i - 1, s[i - 1] <= s[i]);
+      }
+
+      if (op == 2) {
+        int begin, end;
+        cin >> begin >> end;
+        --begin;
+
+        bool f = seg.query(begin, end - 1);
+
+        A x = occ.query(begin, end);
+        int a, b;
+        a = b = -1;
+        for (int i = 0; i < x.size(); ++i) {
+          if (x[i] && a == -1) a = i;
+          if (x[i]) b = i;
+        }
+
+        for (int i = a + 1; i < b; ++i) {
+          f = f && (x[i] == m[i]);
+        }
+        cout << (f ? "Yes" : "No") << endl;
+      }
+    }
+  }
+  return 0;
+}
