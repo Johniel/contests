1c853ed0a9a60a9a2e394febedefd7776455ecaa
Johniel <johniel.s.m@gmail.com>
2023-03-11 23:05:01 +0900

    ABCD

---
+++ atcoder/abc293/C/main.cpp
@@ -0,0 +1,76 @@
+// github.com/Johniel/contests
+// atcoder/abc293/C/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+// #define endl "\n"
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void ___initio(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+template<typename T> using heap = priority_queue<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+// constexpr lli mod = 998244353;
+
+const int H = 11;
+const int W = 11;
+int g[H][W];
+int h, w;
+set<int> vis;
+lli rec(int i, int j)
+{
+  vis.insert(g[i][j]);
+  if (i == h - 1 && j == w - 1) {
+    vis.erase(g[i][j]);
+    return 1;
+  }
+  int di[] = {+1, 0};
+  int dj[] = {0, +1};
+  lli z = 0;
+  for (int d = 0; d < 2; ++d) {
+    int ni = i + di[d];
+    int nj = j + dj[d];
+    unless (0 <= ni && ni < h) continue;
+    unless (0 <= nj && nj < w) continue;
+    if (vis.count(g[ni][nj])) continue;
+    z += rec(ni, nj);
+  }
+  vis.erase(g[i][j]);
+  return z;
+}
+
+int main(int argc, char *argv[])
+{
+  while (cin >> h >> w) {
+    for (int i = 0; i < h; ++i) {
+      for (int j = 0; j < w; ++j) {
+        cin >> g[i][j];
+      }
+    }
+    vis.clear();
+    cout << rec(0, 0) << endl;
+  }
+  return 0;
+}
---
+++ atcoder/abc293/D/main.cpp
@@ -0,0 +1,106 @@
+// github.com/Johniel/contests
+// atcoder/abc293/D/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+// #define endl "\n"
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void ___initio(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+template<typename T> using heap = priority_queue<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+// constexpr lli mod = 998244353;
+
+struct UnionFind {
+  vector<int> r, p;
+  UnionFind(int n) {
+    r.resize(n, 0);
+    p.resize(n, -1);
+  }
+  void unite(int a, int b) {
+    a = find(a);
+    b = find(b);
+    if (a == b) return ;
+    if (r[a] > r[b]) {
+      p[a] += p[b];
+      p[b] = a;
+    } else {
+      p[b] += p[a];
+      p[a] = b;
+      if (r[a] == r[b]) ++r[b];
+    }
+    return ;
+  }
+  int find(int a) {
+    return (p[a] < 0) ? a : p[a] = find(p[a]);
+  }
+  bool same(int a, int b) {
+    return find(a) == find(b);
+  }
+  size_t size(int n) {
+    return -p.at(find(n));
+  }
+};
+
+int main(int argc, char *argv[])
+{
+  int n, m;
+  while (cin >> n >> m) {
+    int x[m];
+    int y[m];
+    for (int i = 0; i < m; ++i) {
+      char cx, cy;
+      cin >> x[i] >> cx >> y[i] >> cy;
+      --x[i];
+      --y[i];
+    }
+    UnionFind uf(n);
+    for (int i = 0; i < m; ++i) {
+      uf.unite(x[i], y[i]);
+    }
+    for (int i = 0; i < n; ++i) {
+      uf.find(i);
+    }
+    vec<int> v;
+    for (int i = 0; i < n; ++i) {
+      v.push_back(uf.find(i));
+    }
+    sort(v.begin(), v.end());
+    v.erase(unique(v.begin(), v.end()), v.end());
+    map<int, int> cnt;
+    for (int i = 0; i < m; ++i) {
+      ++cnt[uf.find(x[i])];
+    }
+    int a = 0;
+    int b = 0;
+    each (i, v) {
+      if (uf.size(i) <= cnt[i]) ++a;
+      else ++b;
+    }
+    cout << a << ' ' << b << endl;
+  }
+  return 0;
+}
