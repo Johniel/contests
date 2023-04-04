fdf54f7d265216e11ed07378be34d355d36e87db
Johniel <johniel.s.m@gmail.com>
2023-04-05 02:00:37 +0900

    ABC

---
+++ codeforces/863div3/C/main.cpp
@@ -0,0 +1,89 @@
+// github.com/Johniel/contests
+// codeforces/863div3/C/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+// #define endl "\n"
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void ___initio(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+// constexpr lli mod = 998244353;
+
+int main(int argc, char *argv[])
+{
+  int _;
+  cin >> _;
+
+  int n;
+  while (cin >> n) {
+    vec<int> b(n - 1);
+    cin >> b;
+
+    vec<pair<int, int>> v;
+    each (i, b) {
+      if (v.empty()) v.push_back(make_pair(i, 1));
+      else {
+        if (v.back().first == i) ++v.back().second;
+        else v.push_back(make_pair(i, 1));
+      }
+    }
+
+    if (v.size() == 1) {
+      for (int i = 0; i < n; ++i) {
+        cout << b[0] << ' ';
+      }
+      cout << endl;
+      continue;
+    }
+
+    for (int i = 0; i + 1 < v.size(); ++i) {
+      int j = i + 1;
+      if (v[i].first < v[j].first) {
+        while (j < v.size() && v[j - 1].first < v[j].first) ++j;
+        unless (i) ++v[i].second;
+        i = j - 2;
+      } else  {
+        while (j < v.size() && v[j - 1].first > v[j].first) ++j;
+        if (i) --v[i].second;
+        i = j - 2;
+        ++v[j - 1].second;
+      }
+    }
+    vec<int> a;
+    each (i, v) {
+      for (int j = 0; j < i.second; ++j) {
+        a.push_back(i.first);
+      }
+    }
+    each (i, a) cout << i << ' '; cout << endl;
+    assert(a.size() == n);
+    for (int i = 0; i < b.size(); ++i) {
+      assert(b[i] == max(a[i], a[i + 1]));
+    }
+  }
+return 0;
+}
