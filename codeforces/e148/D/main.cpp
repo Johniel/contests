c8894800047ab18fcc59ef0a29f18e17038a3ad6
Johniel <johniel.s.m@gmail.com>
2023-05-13 01:30:55 +0900

    ABCD

---
+++ codeforces/e148/D/main.cpp
@@ -0,0 +1,161 @@
+// github.com/Johniel/contests
+// codeforces/e148/D/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+// #define endl "\n"
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void ___initio(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+// constexpr lli mod = 998244353;
+
+template<typename T = long long int>
+struct BIT {
+  vector<T> v;
+  int n;
+
+  BIT(int n_ = 0) {
+    n = n_;
+    v.resize(n + 1, 0);
+  }
+
+  T operator () (int i) const {
+    T s = 0;
+    while (i > 0) {
+      s += v.at(i);
+      i -= i & -i;
+    }
+    return s;
+  }
+
+  T operator () (int begin, int end) const {
+    assert(begin <= end);
+    return (*this)(end) - (*this)(begin);
+  }
+
+  void add(int i, T x) {
+    ++i;
+    while (i <= n) {
+      v.at(i) += x;
+      i += i & -i;
+    }
+    return ;
+  }
+
+  static bool verify(void) {
+    vector<int> v;
+    v.push_back(1 << 0);
+    v.push_back(1 << 2);
+    v.push_back(1 << 1);
+    v.push_back(1 << 4);
+    v.push_back(1 << 3);
+    v.push_back(1 << 6);
+    v.push_back(1 << 5);
+
+    BIT<int> bit(v.size());
+    for (int i = 0; i < v.size(); ++i) {
+      bit.add(i, v[i]);
+    }
+
+    if (bit(1, 3) != v[1] + v[2]) return false;
+
+    int sum = 0;
+    for (int i = 0; i < v.size(); ++i) {
+      if (sum != bit(i)) return false;
+      sum += v[i];
+    }
+    if (sum != bit(v.size())) return false;
+
+    for (int i = 0; i < v.size(); ++i) {
+      int sum = 0;
+      for (int j = i; j < v.size(); ++j) {
+        if (sum != bit(i, j)) return false;
+        sum += v[j];
+      }
+      if (sum != bit(i, v.size())) return false;
+    }
+
+    return true;
+  }
+};
+
+int main(int argc, char *argv[])
+{
+  int n, q;
+  while (cin >> n >> q) {
+    vec<lli> a(n);
+    vec<lli> qs(q);
+    cin >> a >> qs;
+    sort(a.begin(), a.end());
+    vec<lli> w;
+    each (_k, qs) {
+      lli k = _k;
+      // cout << "? " << _k << endl;
+      const int N = 1000 + 3;
+      BIT<lli> bit(N);
+      vec<lli> b = a;
+      for (int i = 0; i < b.size() && k;) {
+        if (k % 2) {
+          b[i] += k;
+          --k;
+          ++i;
+        } else if (i + 1 < b.size()) {
+          b[i] += k;
+          --k;
+          ++i;
+          b[i] += k;
+          --k;
+          ++i;
+        } else break;
+      }
+      sort(b.begin(), b.end());
+      // cout << b << k << endl;
+      for (int i = 1; i < b.size(); ++i) {
+        lli mn = min(k, (b[i] - b[0]) * 2);
+        k -= mn;
+        b[i] -= mn / 2;
+      }
+      for (int i = 0; i < b.size(); ++i) {
+        b[i] -= k / (n * 2);
+      }
+      k %= (n * 2);
+      // cout << "k:=" << k << endl;
+      for (int i = 0; i < b.size() && k; ++i) {
+        --b[i];
+        k -= 2;
+      }
+      assert(k == 0);
+      vec<lli> v = b;
+      const lli z = *min_element(v.begin(), v.end());
+      // cout << v << z << endl;
+      w.push_back(z);
+    }
+    each (i, w) cout << i << ' '; cout << endl;
+    // break;
+  }
+  return 0;
+}
