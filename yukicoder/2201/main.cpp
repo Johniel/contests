c4020ea1ae00b80ab004630ff8188e2e656dbc8e
Johniel <johniel.s.m@gmail.com>
2023-06-16 15:14:36 +0900

    2201

---
+++ yukicoder/2201/main.cpp
@@ -0,0 +1,56 @@
+// github.com/Johniel/contests
+// yukicoder/2201/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+// #define endl "\n"
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void ___initio(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+// constexpr lli mod = 998244353;
+
+int main(int argc, char *argv[])
+{
+  str s;
+  while (cin >> s) {
+    int a = count(s.begin(), s.end(), 'l') + count(s.begin(), s.end(), 'o');
+    int b = count(s.begin(), s.end(), 'a') + count(s.begin(), s.end(), 's');
+    int d = 0;
+    for (char c = 'a'; c <= 'z'; ++c) {
+      if (c == 'a' || c == 's' || c == 'l' || c == 'o') continue;
+      d += count(s.begin(), s.end(), c);
+    }
+    if (a && b) {
+      lli z = ((1LL << a) - 1) * ((1LL << b) - 1);
+      if (a + b == s.size()) --z;
+      cout << z << endl;
+    } else {
+      cout << 0 << endl;
+    }
+  }
+  return 0;
+}
