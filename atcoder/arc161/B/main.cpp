9d6cab862e47ba03dbb5c3cee136f7e1d4717698
Johniel <johniel.s.m@gmail.com>
2023-07-30 19:58:24 +0900

    B

---
+++ atcoder/arc161/B/main.cpp
@@ -0,0 +1,68 @@
+// github.com/Johniel/contests
+// atcoder/arc161/B/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+// #define endl "\n"
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void ___initio(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+// constexpr lli mod = 998244353;
+
+int main(int argc, char *argv[])
+{
+  int _;
+  cin >> _;
+
+  lli n;
+  while (cin >> n) {
+    const int N = 62;
+    const int M = 5;
+    static lli dp[N][M][2];
+    const lli inf = 1LL << 62;
+    fill(&dp[0][0][0], &dp[N - 1][M - 1][2 - 1] + 1, -inf);
+    dp[N - 1][0][false] = 0;
+    for (int i = N - 1; 0 < i; --i) {
+      for (int j = 0; j < 3; ++j) {
+        const lli b = (1LL << (i - 1));
+        if (n & b) {
+          setmax(dp[i - 1][j + 1][false], dp[i][j][false] + b);
+          setmax(dp[i - 1][j + 1][true], dp[i][j][true] + b);
+          setmax(dp[i - 1][j][true], dp[i][j][false]);
+        } else {
+          setmax(dp[i - 1][j][false], dp[i][j][false]);
+          setmax(dp[i - 1][j][true], dp[i][j][true]);
+          setmax(dp[i - 1][j + 1][true], dp[i][j][true] + b);
+        }
+      }
+      setmax(dp[i - 1][3][false], dp[i][3][false]);
+      setmax(dp[i - 1][3][true], dp[i][3][true]);
+    }
+    cout << max<lli>({-1, dp[0][3][false],  dp[0][3][true]}) << endl;
+  }
+  return 0;
+}
