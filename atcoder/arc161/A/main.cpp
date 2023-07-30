e2b45f35840824d73fb127a0c8b1780cdffd9c2d
Johniel <johniel.s.m@gmail.com>
2023-07-30 19:27:01 +0900

    A

---
+++ atcoder/arc161/A/main.cpp
@@ -0,0 +1,66 @@
+// github.com/Johniel/contests
+// atcoder/arc161/A/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+// #define endl "\n"
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void ___initio(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+// constexpr lli mod = 998244353;
+
+int main(int argc, char *argv[])
+{
+  int n;
+  while (cin >> n) {
+    vec<int> a(n);
+    cin >> a;
+    sort(a.begin(), a.end());
+    deque<int> q;
+    each (i, a) q.push_back(i);
+    vec<int> b(n);
+    for (int i = 0; i < n; ++i) {
+      if (i % 2) {
+        b[i] = q.back();
+        q.pop_back();
+      }
+    }
+    for (int i = n - 1; 0 <= i; --i) {
+      unless (i % 2) {
+        b[i] = q.front();
+        q.pop_front();
+      }
+    }
+    bool f = true;
+    for (int i = 1; i < n; i += 2) {
+      f = f && (b[i - 1] < b[i]);
+      f = f && (b[i + 1] < b[i]);
+    }
+    cout << (f ? "Yes" : "No") << endl;
+  }
+  return 0;
+}
