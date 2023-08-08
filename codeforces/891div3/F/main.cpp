493240c8f10f82032ca32997c93953403232aa32
Johniel <johniel.s.m@gmail.com>
2023-08-08 18:05:25 +0900

    F

---
+++ codeforces/891div3/F/main.cpp
@@ -0,0 +1,89 @@
+// github.com/Johniel/contests
+// codeforces/891div3/F/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+#define endl "\n"
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void ___initio(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+// constexpr lli mod = 998244353;
+
+namespace math {
+  lli isqrt(lli x)
+  {
+    if (x == 0) return 0;
+    const bool neg = (x < 0);
+    x = abs(x);
+    lli sqrtx = sqrt(x) - 1;
+    while (sqrtx + 1 <= x / (sqrtx + 1)) {
+      sqrtx++;
+    }
+    return neg ? -sqrtx : +sqrtx;
+  }
+};
+
+int main(int argc, char *argv[])
+{
+  int _;
+  cin >> _;
+
+  int n;
+  while (cin >> n) {
+    vec<lli> a(n);
+    cin >> a;
+    map<lli, lli> m;
+    each (i, a) ++m[i];
+    int q;
+    cin >> q;
+
+    vec<lli> v;
+    while (q--) {
+      lli x, y;
+      cin >> x >> y;
+
+      lli b24ac = x * x - 4 * y;
+      lli p = math::isqrt(b24ac);
+      if (p * p != b24ac) {
+        v.push_back(0);
+        continue;
+      }
+
+      if ((x + p) % 2 == 0 && (x - p) % 2 == 0) {
+        lli s = (x + p) / 2;
+        lli t = (x - p) / 2;
+        if (m.count(s) == 0 || m.count(t) == 0) v.push_back(0);
+        else if (s != t) v.push_back(m[s] * m[t]);
+        else v.push_back(m[s] * (m[t] - 1) / 2);
+      } else {
+        v.push_back(0);
+      }
+    }
+    each (i, v) cout << i << ' '; cout << endl;
+  }
+  return 0;
+}
