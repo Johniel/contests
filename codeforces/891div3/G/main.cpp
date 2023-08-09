4bb8ab73a0cfa13f01f8143c9bc5c4f646e8af71
Johniel <johniel.s.m@gmail.com>
2023-08-09 19:40:18 +0900

    G

---
+++ codeforces/891div3/G/main.cpp
@@ -0,0 +1,128 @@
+// github.com/Johniel/contests
+// codeforces/891div3/G/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+// #define endl "\n"
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void ___initio(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 998244353;
+
+namespace math {
+  lli extgcd(lli a, lli b, lli& x, lli& y)
+  {
+    lli g = a;
+    x = 1;
+    y = 0;
+    if (b != 0) {
+      g = extgcd(b, a % b, y, x);
+      y -= (a / b) * x;
+    }
+    return g;
+  }
+
+  lli mod_inverse(lli a, lli m)
+  {
+    lli x, y;
+    extgcd(a, m, x, y);
+    return (m + x % m) % m;
+  }
+
+  lli mod_pow(lli n, lli p, lli M = mod)
+  {
+    if (p == 0) return 1;
+    if (p == 1) return n % M;
+    lli m = mod_pow(n, p / 2, M);
+    m *= m;
+    m %= M;
+    if (p % 2) m = (m * n) % M;
+    return m;
+  }
+};
+
+struct UnionFind {
+  vector<int> r, p;
+  UnionFind(int n) {
+    r.resize(n, 0);
+    p.resize(n, -1);
+  }
+  void unite(int a, int b) {
+    a = find(a);
+    b = find(b);
+    if (a == b) return ;
+    if (r[a] > r[b]) {
+      p[a] += p[b];
+      p[b] = a;
+    } else {
+      p[b] += p[a];
+      p[a] = b;
+      if (r[a] == r[b]) ++r[b];
+    }
+    return ;
+  }
+  int find(int a) {
+    return (p[a] < 0) ? a : p[a] = find(p[a]);
+  }
+  bool same(int a, int b) {
+    return find(a) == find(b);
+  }
+  size_t size(int n) {
+    return -p.at(find(n));
+  }
+};
+
+int main(int argc, char *argv[])
+{
+  int _;
+  cin >> _;
+
+  int n, S;
+  while (cin >> n >> S) {
+    vec<pair<int, pair<int, int>>> es;
+    for (int i = 0; i < n - 1; ++i) {
+      int a, b, c;
+      cin >> a >> b >> c;
+      --a;
+      --b;
+      es.push_back(make_pair(c, make_pair(a, b)));
+    }
+    sort(es.begin(), es.end());
+    UnionFind uf(n);
+    lli z = 1;
+    each (we, es) {
+      const pair<int, int> e = we.second;
+      const lli w = we.first;
+      const lli a = uf.size(e.first);
+      const lli b = uf.size(e.second);
+      const lli c = math::mod_pow(S - w + 1, a * b - 1, mod);
+      (z *= c) %= mod;
+      uf.unite(e.first, e.second);
+    }
+    cout << z << endl;
+  }
+  return 0;
+}
