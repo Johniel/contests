0cd7ce197d11ac8793ccee71e1498a4eec2f6c8c
Johniel <johniel.s.m@gmail.com>
2023-10-22 13:51:25 +0900

    G

---
+++ atcoder/abc325/G/main.cpp
@@ -0,0 +1,71 @@
+// github.com/Johniel/contests
+// atcoder/abc325/G/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define each_with_index(i, e, c) for (const auto i: views::iota(0, (int)c.size())) if (auto& e = c.at(i); true)
+#define each_pair(p, c) for (const auto _i: views::iota(0, (int)c.size())) if (std::pair<const int, decltype((c.at(_i)))> p = {_i, c[_i]}; true)
+#define unless(cond) if (!(cond))
+// #define endl "\n"
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void ___initio(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+// constexpr lli mod = 1e9 + 7;
+constexpr lli mod = 998244353;
+
+str s;
+int rm;
+const int inf = (1 << 27);
+
+const int N = 300 + 3;
+int memo[N][N];
+int rec(const int begin, const int end)
+{
+  int& ret = memo[begin][end];
+  if (0 <= ret) return ret;
+  if (begin == end) return ret = 0;
+
+  int mn = end - begin;
+  for (int i = begin; i < end; ++i) {
+    for (int j = i + 1; j < end; ++j) {
+      if (s[i] == 'o' && s[j] == 'f') {
+        int a = rec(begin, i);
+        int b = rec(i + 1, j);
+        int c = rec(j + 1, end);
+        if (b == 0) setmin(mn, a + max(0, c - rm));
+      }
+    }
+  }
+  return ret = mn;
+}
+
+int main(int argc, char *argv[])
+{
+  while (cin >> s >> rm) {
+    fill(&memo[0][0], &memo[N - 1][N - 1] + 1, -inf);
+    cout << rec(0, s.size()) << endl;
+  }
+  return 0;
+}
