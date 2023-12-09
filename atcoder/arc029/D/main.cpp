807e1d5dd730489734807c5695bad252eeba54f5
Johniel <johniel.s.m@gmail.com>
2023-12-10 02:48:40 +0900

    D

---
+++ atcoder/arc029/D/main.cpp
@@ -0,0 +1,114 @@
+// github.com/Johniel/contests
+// atcoder/arc029/D/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define each_with_index(i, e, c) for (const auto i: views::iota(0, (int)c.size())) if (auto& e = c.at(i); true)
+#define each_pair(p, c) for (const auto _i: views::iota(0, (int)c.size())) if (std::pair<const int, decltype((c.at(_i)))> p = {_i, c[_i]}; true)
+#define unless(cond) if (!(cond))
+// #define endl "\n"
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename P, typename Q, typename R> ostream& operator << (ostream& os, tuple<P, Q, R> t) { os << "(" << get<0>(t) << "," << get<1>(t) << "," << get<2>(t) << ")"; return os; }
+template<typename P, typename Q, typename R> istream& operator >> (istream& is, tuple<P, Q, R>& t) { is >> get<0>(t) >> get<1>(t) >> get<2>(t); return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, stack<T> s) { os << "stack{"; while (s.size()) { os << s.top() << ","; s.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, queue<T> q) { os << "queue{"; while (q.size()) { os << q.front() << ","; q.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, deque<T> q) { os << "deque{"; for (int i = 0; i < q.size(); ++i) os << q[i] << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, priority_queue<T> q) { os << "heap{"; while (q.size()) { os << q.top() << ","; q.pop(); } os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void ___initio(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+// constexpr lli mod = 1e9 + 7;
+constexpr lli mod = 998244353;
+
+const int N = 5000 + 3;
+vec<int> g[N];
+
+int treesize[N];
+int fn(int curr)
+{
+  treesize[curr] = 1;
+  each (next, g[curr]) treesize[curr] += fn(next);
+  return treesize[curr];
+}
+
+vec<int> s;
+lli memo[N][N];
+lli rec(int curr, int removed)
+{
+  lli& ret = memo[curr][removed];
+  if (ret != -1) return ret;
+
+  const lli inf = 1LL << 60;
+  vec<lli> v(treesize[curr] + 1, inf);
+  v[0] = 0;
+  v[1] = s[curr];
+  each (next, g[curr]) {
+    vec<lli> u = v;
+    for (int i = 1; i <= treesize[curr]; ++i) {
+      for (int j = 0; j <= treesize[next]; ++j) {
+        if (i + j < v.size()) {
+          setmin(u[i + j], v[i] + rec(next, j));
+        }
+      }
+    }
+    v = u;
+  }
+  for (int i = 0; i < v.size(); ++i) {
+    memo[curr][i] = v[i];
+  }
+  return ret = v.at(removed);
+}
+
+int main(int argc, char *argv[])
+{
+  int n;
+  while (cin >> n) {
+    fill(&memo[0][0], &memo[N - 1][N - 1] + 1, -1);
+    s.resize(n);
+    cin >> s;
+    for (int i = 0; i < n - 1; ++i) {
+      int a, b;
+      cin >> a >> b;
+      --a;
+      --b;
+      g[a].push_back(b);
+    }
+    fn(0);
+    int m;
+    cin >> m;
+    vec<int> t(m);
+    cin >> t;
+    sort(t.begin(), t.end());
+    reverse(t.begin(), t.end());
+    lli x = accumulate(s.begin(), s.end(), 0LL);
+    lli y = 0;
+    lli z = x;
+    for (int i = 0; i < s.size(); ++i) {
+      y += t[i];
+      setmax(z, x - rec(0, i + 1) + y);
+    }
+    cout << z << endl;
+  }
+  return 0;
+}
