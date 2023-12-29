bd08df815278984ef91a71a296460eb4100da18d
Johniel <johniel.s.m@gmail.com>
2023-12-29 10:53:58 +0900

    E

---
+++ atcoder/arc059/E/main.cpp
@@ -0,0 +1,204 @@
+// github.com/Johniel/contests
+// atcoder/arc059/E/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define each_with_index(i, e, c) for (const auto i: views::iota(0, (int)c.size())) if (auto& e = c.at(i); true)
+#define each_pair(p, c) for (const auto _i: views::iota(0, (int)c.size())) if (std::pair<const int, decltype((c.at(_i)))> p = {_i, c[_i]}; true)
+#define unless(cond) if (!(cond))
+// #define endl "\n"
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p);
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p);
+template<typename P, typename Q, typename R> ostream& operator << (ostream& os, tuple<P, Q, R> t) { os << "(" << get<0>(t) << "," << get<1>(t) << "," << get<2>(t) << ")"; return os; }
+template<typename P, typename Q, typename R> istream& operator >> (istream& is, tuple<P, Q, R>& t) { is >> get<0>(t) >> get<1>(t) >> get<2>(t); return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, stack<T> s) { os << "stack{"; while (s.size()) { os << s.top() << ","; s.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, queue<T> q) { os << "queue{"; while (q.size()) { os << q.front() << ","; q.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, deque<T> q) { os << "deque{"; for (int i = 0; i < q.size(); ++i) os << q[i] << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, priority_queue<T> q) { os << "heap{"; while (q.size()) { os << q.top() << ","; q.pop(); } os << "}"; return os; }
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void ___initio(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 1e9 + 7;
+
+namespace math {
+  lli extgcd(lli a, lli b, lli& x, lli& y)
+  {
+    lli g = a;
+    x = 1;
+    y = 0;
+    if (b != 0) {
+      g = extgcd(b, a % b, y, x);
+      y -= (a / b) * x;
+    }
+    return g;
+  }
+
+  lli mod_inverse(lli a, lli m)
+  {
+    lli x, y;
+    extgcd(a, m, x, y);
+    return (m + x % m) % m;
+  }
+
+  lli mod_pow(lli n, lli p, lli M = mod)
+  {
+    if (p == 0) return 1;
+    if (p == 1) return n % M;
+    lli m = mod_pow(n, p / 2, M);
+    m *= m;
+    m %= M;
+    if (p % 2) m = (m * n) % M;
+    return m;
+  }
+};
+
+template<typename T>
+struct PrefixSum2D {
+  vector<vector<T>> sum;
+  PrefixSum2D() {}
+  PrefixSum2D(vector<vector<T>> v) {
+    const int H = v.size() + 1;
+    const int W = v[0].size() + 1;
+    sum.resize(H);
+    fill(sum.begin(), sum.end(), vector<T>(W, 0));
+    for (int i = 0; i < v.size(); ++i) {
+      for (int j = 0; j < v[i].size(); ++j) {
+        sum[i + 1][j + 1] = v[i][j];
+      }
+    }
+    for (int i = 1; i < H; ++i) {
+      for (int j = 0; j < W; ++j) {
+        sum[i][j] += sum[i - 1][j];
+      }
+    }
+    for (int i = 0; i < H; ++i) {
+      for (int j = 1; j < W; ++j) {
+        sum[i][j] += sum[i][j - 1];
+      }
+    }
+  }
+  T operator () (int ai, int aj, int bi, int bj) const { return this->query(ai, aj, bi, bj); }
+  T query(int ai, int aj, int bi, int bj) const {
+    assert(0 <= ai && ai < sum.size());
+    assert(0 <= bi && bi < sum.size());
+    assert(0 <= aj && aj < sum[0].size());
+    assert(0 <= bj && bj < sum[0].size());
+    assert(ai <= bi && aj <= bj);
+    return sum[bi][bj] + sum[ai][aj] - sum[ai][bj] - sum[bi][aj];
+  }
+};
+
+// from: https://qiita.com/drken/items/3b4fdf0a78e7a138cd9a
+template<int MOD> struct Fp {
+  long long val;
+  constexpr Fp(long long v = 0) noexcept : val(v % MOD) {
+    if (val < 0) val += MOD;
+  }
+  constexpr int getmod() { return MOD; }
+  constexpr Fp operator - () const noexcept {
+    return val ? MOD - val : 0;
+  }
+  constexpr Fp operator + (const Fp& r) const noexcept { return Fp(*this) += r; }
+  constexpr Fp operator - (const Fp& r) const noexcept { return Fp(*this) -= r; }
+  constexpr Fp operator * (const Fp& r) const noexcept { return Fp(*this) *= r; }
+  constexpr Fp operator / (const Fp& r) const noexcept { return Fp(*this) /= r; }
+  constexpr Fp& operator += (const Fp& r) noexcept {
+    val += r.val;
+    if (val >= MOD) val -= MOD;
+    return *this;
+  }
+  constexpr Fp& operator -= (const Fp& r) noexcept {
+    val -= r.val;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr Fp& operator *= (const Fp& r) noexcept {
+    val = val * r.val % MOD;
+    return *this;
+  }
+  constexpr Fp& operator /= (const Fp& r) noexcept {
+    long long a = r.val, b = MOD, u = 1, v = 0;
+    while (b) {
+      long long t = a / b;
+      a -= t * b; swap(a, b);
+      u -= t * v; swap(u, v);
+    }
+    val = val * u % MOD;
+    if (val < 0) val += MOD;
+    return *this;
+  }
+  constexpr bool operator == (const Fp& r) const noexcept {
+    return this->val == r.val;
+  }
+  constexpr bool operator != (const Fp& r) const noexcept {
+    return this->val != r.val;
+  }
+  friend constexpr ostream& operator << (ostream &os, const Fp<MOD>& x) noexcept {
+    return os << x.val;
+  }
+  friend constexpr Fp<MOD> modpow(const Fp<MOD> &a, long long n) noexcept {
+    if (n == 0) return 1;
+    auto t = modpow(a, n / 2);
+    t = t * t;
+    if (n & 1) t = t * a;
+    return t;
+  }
+};
+template<int MOD> istream& operator >> (istream& is, Fp<MOD>& m) { is >> m.val; m.val %= MOD; return is; }
+template<int MOD> ostream& operator << (ostream& os, const Fp<MOD>& m) { os << m.val; return os; }
+using mint = Fp<mod>;
+
+int main(int argc, char *argv[])
+{
+  const int N = 400 + 3;
+  int n, c;
+  while (cin >> n >> c) {
+    vec<lli> a(n), b(n);
+    cin >> a >> b;
+
+    vec<vec<mint>> g(N, vec<mint>(N, mint(1)));
+    for (int x = 0; x < N; ++x) {
+      for (int p = 0; p < N; ++p) {
+        g[x][p] = mint(math::mod_pow(x, p));
+      }
+    }
+    PrefixSum2D<mint> sum(g);
+
+    static lli dp[N][N];
+    fill(&dp[0][0], &dp[N - 1][N - 1] + 1, 0);
+    dp[0][0] = 1;
+    for (int i = 0; i < a.size(); ++i) {
+      for (int j = 0; j <= c; ++j) {
+        unless (dp[i][j]) continue;
+        for (int y = 0; j + y <= c; ++y) {
+          lli w = sum(a[i], y, b[i] + 1, y + 1).val;
+          (dp[i + 1][j + y] += dp[i][j] * w % mod) %= mod;
+        }
+      }
+    }
+    cout << dp[n][c] << endl;
+  }
+  return 0;
+}
