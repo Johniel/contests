b3f3919f47d573eae83c50baf5e031296bb08920
Johniel <johniel.s.m@gmail.com>
2024-01-02 16:04:17 +0900

    C

---
+++ atcoder/arc047/C/main.cpp
@@ -0,0 +1,147 @@
+// github.com/Johniel/contests
+// atcoder/arc047/C/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define each_with_index(i, e, c) for (const auto i: views::iota(0, (int)c.size())) if (auto& e = c.at(i); true)
+#define each_pair(p, c) for (const auto _i: views::iota(0, (int)c.size())) if (std::pair<const int, decltype((c.at(_i)))> p = {_i, c[_i]}; true)
+#define unless(cond) if (!(cond))
+// #define endl "\n"
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p);
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p);
+template<typename P, typename Q, typename R> ostream& operator << (ostream& os, tuple<P, Q, R> t) { os << "(" << get<0>(t) << "," << get<1>(t) << "," << get<2>(t) << ")"; return os; }
+template<typename P, typename Q, typename R> istream& operator >> (istream& is, tuple<P, Q, R>& t) { is >> get<0>(t) >> get<1>(t) >> get<2>(t); return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, stack<T> s) { os << "stack{"; while (s.size()) { os << s.top() << ","; s.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, queue<T> q) { os << "queue{"; while (q.size()) { os << q.front() << ","; q.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, deque<T> q) { os << "deque{"; for (int i = 0; i < q.size(); ++i) os << q[i] << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, priority_queue<T> q) { os << "heap{"; while (q.size()) { os << q.top() << ","; q.pop(); } os << "}"; return os; }
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void ___initio(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 998244353;
+
+namespace math {
+  lli extgcd(lli a, lli b, lli& x, lli& y)
+  {
+    lli g = a;
+    x = 1;
+    y = 0;
+    if (b != 0) {
+      g = extgcd(b, a % b, y, x);
+      y -= (a / b) * x;
+    }
+    return g;
+  }
+
+  lli mod_inverse(lli a, lli m)
+  {
+    lli x, y;
+    extgcd(a, m, x, y);
+    return (m + x % m) % m;
+  }
+
+  lli mod_pow(lli n, lli p, lli M = mod)
+  {
+    if (p == 0) return 1;
+    if (p == 1) return n % M;
+    lli m = mod_pow(n, p / 2, M);
+    m *= m;
+    m %= M;
+    if (p % 2) m = (m * n) % M;
+    return m;
+  }
+  template<std::integral T>
+  T ceil(T x, T div) {
+    // ABC334B
+    assert(div != T(0));
+    T r = !!(x % div);
+    return x / div + ((x < 0) == (div < 0) ? r : 0);
+  }
+}
+
+template<typename T = long long int>
+struct BIT {
+  vector<T> v;
+  const int n;
+
+  BIT(int n_ = 0) : n(n_), v(n_ + 1, 0) {}
+
+  T operator () (int i) const {
+    T s = 0;
+    while (i > 0) {
+      s += v.at(i);
+      i -= i & -i;
+    }
+    return s;
+  }
+
+  T operator () (int begin, int end) const {
+    assert(begin <= end);
+    return (*this)(end) - (*this)(begin);
+  }
+
+  void add(int i, T x) {
+    ++i;
+    while (i <= n) {
+      v.at(i) += x;
+      i += i & -i;
+    }
+    return ;
+  }
+
+  size_t lower_bound(const T query) const {
+    int small = 0;
+    int large = v.size();
+    while (small + 1 < large) {
+      auto mid = (small + large) / 2;
+      if ((*this)(mid) <= query) small = mid;
+      else large = mid;
+    }
+    if (query <= (*this)(small)) return small;
+    if (query <= (*this)(large)) return large;
+    return size();
+  }
+
+  size_t size(void) const { return n; }
+};
+
+int main(int argc, char *argv[])
+{
+  lli n, k;
+  while (cin >> n >> k) {
+    BIT<lli> bit(n + 1);
+    for (int i = 0; i <= n; ++i) bit.add(i, 1);
+    lli b = n;
+    vec<int> v;
+    for (int i = 0; i < n; ++i) {
+      int a = bit.lower_bound(math::ceil(b, k));
+      b = (b % k ? b % k : k) * (n - i - 1);
+      bit.add(a, -1);
+      v.push_back(a);
+    }
+    each (i, v) cout << i << endl;
+  }
+  return 0;
+}
