48b119880207679de0b57af05ff654a48d468967
Johniel <johniel.s.m@gmail.com>
2024-01-09 15:09:51 +0900

    G

---
+++ codeforces/918div4/G/main.cpp
@@ -0,0 +1,97 @@
+// github.com/Johniel/contests
+// codeforces/918div4/G/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define each_with_index(i, e, c) for (const auto i: views::iota(0, (int)c.size())) if (auto& e = c.at(i); true)
+#define each_pair(p, c) for (const auto _i: views::iota(0, (int)c.size())) if (std::pair<const int, decltype((c.at(_i)))> p = {_i, c[_i]}; true)
+#define unless(cond) if (!(cond))
+// #define endl "\n"
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p);
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p);
+template<typename P, typename Q, typename R> ostream& operator << (ostream& os, tuple<P, Q, R> t) { os << "(" << get<0>(t) << "," << get<1>(t) << "," << get<2>(t) << ")"; return os; }
+template<typename P, typename Q, typename R> istream& operator >> (istream& is, tuple<P, Q, R>& t) { is >> get<0>(t) >> get<1>(t) >> get<2>(t); return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "set{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "map{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, stack<T> s) { os << "stack{"; while (s.size()) { os << s.top() << ","; s.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, queue<T> q) { os << "queue{"; while (q.size()) { os << q.front() << ","; q.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, deque<T> q) { os << "deque{"; for (int i = 0; i < q.size(); ++i) os << q[i] << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, priority_queue<T> q) { os << "heap{"; while (q.size()) { os << q.top() << ","; q.pop(); } os << "}"; return os; }
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void ___initio(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+// constexpr lli mod = 1e9 + 7;
+constexpr lli mod = 998244353;
+
+int main(int argc, char *argv[])
+{
+  { int _; cin >> _; }
+  int n, m;
+  while (cin >> n >> m) {
+    const int N = 1000 + 3;
+    const int S = 1000 + 3;
+    vec<pair<int, lli>> g[N];
+    for (int i = 0; i < m; ++i) {
+      int a, b, c;
+      cin >> a >> b >> c;
+      --a;
+      --b;
+      g[a].push_back(make_pair(b, c));
+      g[b].push_back(make_pair(a, c));
+    }
+    vec<int> s(n);
+    cin >> s;
+    const int mxs = *max_element(s.begin(), s.end());
+
+    priority_queue<tuple<lli, int, int>> q;
+    static lli cost[N][N];
+    const lli inf = 1LL << 60;
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j <= mxs; ++j) {
+        cost[i][j] = inf;
+      }
+    }
+    q.push(make_tuple(0LL, 0, s[0]));
+    cost[0][s[0]] = 0;
+    while (q.size()) {
+      auto [c, curr, slowness] = q.top();
+      c = abs(c);
+      q.pop();
+      if (cost[curr][slowness] != c) continue;
+      each (e, g[curr]) {
+        auto [next, w] = e;
+        int ns = min(slowness, s[next]);
+        if (cost[next][ns] > c + slowness * w) {
+          cost[next][ns] = c + slowness * w;
+          q.push(make_tuple(-cost[next][ns], next, ns));
+        }
+      }
+    }
+    lli z = inf;
+    for (int i = 0; i <= mxs; ++i) {
+      setmin(z, cost[n - 1][i]);
+    }
+    cout << z << endl;
+  }
+  return 0;
+}
