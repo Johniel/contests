b08658ceb3a0ef3ddcf3834fa421f0e56a6ca60f
Johniel <johniel.s.m@gmail.com>
2024-01-16 22:02:11 +0900

    F

---
+++ atcoder/abc333/F/main.cpp
@@ -0,0 +1,120 @@
+// github.com/Johniel/contests
+// atcoder/abc333/F/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define each_with_index(i, e, c) for (const auto i: views::iota(0, (int)c.size())) if (auto& e = c.at(i); true)
+#define each_pair(p, c) for (const auto _i: views::iota(0, (int)c.size())) if (std::pair<const int, decltype((c.at(_i)))> p = {_i, c[_i]}; true)
+#define unless(cond) if (!(cond))
+// #define endl "\n"
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p);
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p);
+template<typename P, typename Q, typename R> ostream& operator << (ostream& os, tuple<P, Q, R> t) { os << "(" << get<0>(t) << "," << get<1>(t) << "," << get<2>(t) << ")"; return os; }
+template<typename P, typename Q, typename R> istream& operator >> (istream& is, tuple<P, Q, R>& t) { is >> get<0>(t) >> get<1>(t) >> get<2>(t); return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "set{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "map{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, stack<T> s) { os << "stack{"; while (s.size()) { os << s.top() << ","; s.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, queue<T> q) { os << "queue{"; while (q.size()) { os << q.front() << ","; q.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, deque<T> q) { os << "deque{"; for (int i = 0; i < q.size(); ++i) os << q[i] << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, priority_queue<T> q) { os << "heap{"; while (q.size()) { os << q.top() << ","; q.pop(); } os << "}"; return os; }
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void ___initio(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr lli mod = 998244353;
+
+namespace math {
+  lli extgcd(lli a, lli b, lli& x, lli& y)
+  {
+    lli g = a;
+    x = 1;
+    y = 0;
+    if (b != 0) {
+      g = extgcd(b, a % b, y, x);
+      y -= (a / b) * x;
+    }
+    return g;
+  }
+
+  lli mod_inverse(lli a, lli m)
+  {
+    lli x, y;
+    extgcd(a, m, x, y);
+    return (m + x % m) % m;
+  }
+
+  lli mod_pow(lli n, lli p, lli M = mod)
+  {
+    if (p == 0) return 1;
+    if (p == 1) return n % M;
+    lli m = mod_pow(n, p / 2, M);
+    m *= m;
+    m %= M;
+    if (p % 2) m = (m * n) % M;
+    return m;
+  }
+};
+
+const int N = 3000 + 3;
+
+int main(int argc, char *argv[])
+{
+  vec<lli> p2;
+  p2.push_back(1);
+  for (int i = 0; i < N; ++i) {
+    p2.push_back(p2.back() * 2 % mod);
+  }
+
+  static lli dp[N][N];
+  fill(&dp[0][0], &dp[N - 1][N - 1] + 1, 0);
+  dp[1][0] = 1;
+  for (int remained = 2; remained < N; ++remained) {
+    lli x = 0;
+    vec<lli> v;
+    lli w = math::mod_inverse(4, mod);
+    for (int position = remained - 2; 0 <= position; --position) {
+      (x += dp[remained - 1][position] * w % mod) %= mod;
+      (w *= math::mod_inverse(2, mod)) %= mod;
+    }
+
+    const lli y = p2[remained] * math::mod_inverse((p2[remained] - 1 + mod) % mod, mod) % mod;
+
+    dp[remained][0] = x * y % mod;
+    for (int position = 1; position < remained; ++position) {
+      (x *= math::mod_inverse(2, mod)) %= mod;
+
+      x -= dp[remained - 1][position - 1] * math::mod_inverse(p2[remained + 1], mod) % mod;
+      (x += mod) %= mod;
+
+      x += dp[remained - 1][position - 1] * math::mod_inverse(2, mod) % mod;
+      x %= mod;
+
+      dp[remained][position] = x * y % mod;
+    }
+  }
+
+  int n;
+  while (cin >> n) {
+    for (int i = 0; i < n; ++i) cout << dp[n][i] << ' ';
+    cout << endl;
+  }
+
+  return 0;
+}
