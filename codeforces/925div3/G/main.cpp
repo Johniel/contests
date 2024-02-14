066b2c4d0d6864c60de5d5d07a193a44258d0b76
Johniel <johniel.s.m@gmail.com>
2024-02-14 12:02:53 +0900

    G

---
+++ codeforces/925div3/G/main.cpp
@@ -0,0 +1,155 @@
+// github.com/Johniel/contests
+// codeforces/925div3/G/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+#define makepair(a, b) make_pair(a, b)
+// #define endl "\n"
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p);
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p);
+template<typename P, typename Q, typename R> ostream& operator << (ostream& os, tuple<P, Q, R> t) { os << "(" << get<0>(t) << "," << get<1>(t) << "," << get<2>(t) << ")"; return os; }
+template<typename P, typename Q, typename R> istream& operator >> (istream& is, tuple<P, Q, R>& t) { is >> get<0>(t) >> get<1>(t) >> get<2>(t); return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "set{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "map{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, stack<T> s) { os << "stack{"; while (s.size()) { os << s.top() << ","; s.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, queue<T> q) { os << "queue{"; while (q.size()) { os << q.front() << ","; q.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, deque<T> q) { os << "deque{"; for (int i = 0; i < q.size(); ++i) os << q[i] << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, priority_queue<T> q) { os << "heap{"; while (q.size()) { os << q.top() << ","; q.pop(); } os << "}"; return os; }
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void ___initio(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+constexpr lli mod = 998244353;
+
+const int N = 8 * 1e6;
+
+namespace math {
+  lli fact[N], finv[N], inv[N];
+
+  void init(void)
+  {
+    fact[0] = fact[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < N; i++){
+      fact[i] = fact[i - 1] * i % mod;
+      inv[i] = mod - inv[mod % i] * (mod / i) % mod;
+      finv[i] = finv[i - 1] * inv[i] % mod;
+    }
+    return ;
+  }
+
+  lli mod_comb(int n, int k)
+  {
+    assert(n < N && k < N);
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fact[n] * (finv[k] * finv[n - k] % mod) % mod;
+  }
+
+  inline long long int nCk(int n, int k) { return mod_comb(n, k); }
+
+  // n個の箱にm個の何かを分配する場合の数
+  inline long long int nHm(int n, int m) { return nCk(n + m - 1, m); }
+
+  // ∑{k=0...n} pCk * qC(n-k) = (p+q)Cn ※ p<k && pCk=0
+  // https://manabitimes.jp/math/622
+  inline long long int vandermonde_convolution(int p, int q, int n) { return nCk(p + q, n); }
+};
+
+namespace math {
+  // gcd(a,b)を返す。xとyにはax+by=±gcd(a,b)を満たす数を設定する。
+  lli extgcd(lli a, lli b, lli& x, lli& y)
+  {
+    lli g = a;
+    x = 1;
+    y = 0;
+    if (b != 0) {
+      g = extgcd(b, a % b, y, x);
+      y -= (a / b) * x;
+    }
+    return g;
+  }
+
+  lli mod_inverse(lli a, lli m)
+  {
+    lli x, y;
+    extgcd(a, m, x, y);
+    return (m + x % m) % m;
+  }
+
+  lli mod_pow(lli n, lli p, lli M = mod)
+  {
+    if (p == 0) return 1;
+    if (p == 1) return n % M;
+    lli m = mod_pow(n, p / 2, M);
+    m *= m;
+    m %= M;
+    if (p % 2) m = (m * n) % M;
+    return m;
+  }
+};
+
+int main(int argc, char *argv[])
+{
+  math::init();
+  { int _; cin >> _; }
+  vec<lli> c(4);
+  while (cin >> c) {
+    if (1 < abs(c[0] - c[1])) {
+      cout << 0 << endl;
+      continue;
+    }
+    // 0-2-1-3-0
+    // 1-3-0-2-1
+    lli z = 1;
+    if (c[0] == 0 && c[1] == 0) {
+      if (c[3] == 0 || c[2] == 0) z = 1;
+      else z = 0;
+    } else if (c[0] == c[1]) {
+      // 0-1-0-1
+      lli x = 1;
+      x *= math::nHm(c[0]+1, c[3]);
+      x *= math::nHm(c[1], c[2]);
+      x %= mod;
+      // 1-0-1-0
+      lli y = 1;
+      y *= math::nHm(c[0], c[3]);
+      y *= math::nHm(c[1]+1, c[2]);
+      y %= mod;
+      z = (x + y) % mod;
+    } else if (c[0] + 1 == c[1]) {
+      // 1-0-1-0-1
+      z *= math::nHm(c[0]+1, c[3]);
+      z *= math::nHm(c[1], c[2]);
+      z %= mod;
+    } else if (c[1] + 1 == c[0]) {
+      // 0-1-0-1-0
+      z *= math::nHm(c[0], c[3]);
+      z *= math::nHm(c[1]+1, c[2]);
+      z %= mod;
+    }
+    cout << z << endl;
+  }
+  return 0;
+}
