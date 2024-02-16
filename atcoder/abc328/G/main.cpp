dcde6a8de1e35a5f4daab9a52d61a79871131fc3
Johniel <johniel.s.m@gmail.com>
2024-02-16 20:24:01 +0900

    G

---
+++ atcoder/abc328/G/main.cpp
@@ -0,0 +1,89 @@
+// github.com/Johniel/contests
+// atcoder/abc328/G/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define each_with_index(i, e, c) for (const auto i: views::iota(0, (int)c.size())) if (auto& e = c.at(i); true)
+#define each_pair(p, c) for (const auto _i: views::iota(0, (int)c.size())) if (std::pair<const int, decltype((c.at(_i)))> p = {_i, c[_i]}; true)
+#define unless(cond) if (!(cond))
+// #define endl "\n"
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+template<typename P, typename Q, typename R> ostream& operator << (ostream& os, tuple<P, Q, R> t) { os << "(" << get<0>(t) << "," << get<1>(t) << "," << get<2>(t) << ")"; return os; }
+template<typename P, typename Q, typename R> istream& operator >> (istream& is, tuple<P, Q, R>& t) { is >> get<0>(t) >> get<1>(t) >> get<2>(t); return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "#{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, stack<T> s) { os << "stack{"; while (s.size()) { os << s.top() << ","; s.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, queue<T> q) { os << "queue{"; while (q.size()) { os << q.front() << ","; q.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, deque<T> q) { os << "deque{"; for (int i = 0; i < q.size(); ++i) os << q[i] << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, priority_queue<T> q) { os << "heap{"; while (q.size()) { os << q.top() << ","; q.pop(); } os << "}"; return os; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void ___initio(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+// constexpr lli mod = 1e9 + 7;
+constexpr lli mod = 998244353;
+
+int main(int argc, char *argv[])
+{
+  int n;
+  lli c;
+  while (cin >> n >> c) {
+    vec<lli> a(n);
+    vec<lli> b(n);
+    cin >> a >> b;
+
+    const int N = 22 + 1;
+    const lli inf = 1LL << 60;
+    static lli cost[N][N][N];
+    fill(&cost[0][0][0], &cost[N - 1][N - 1][N - 1] + 1, inf);
+    for (int i = 0; i < a.size(); ++i) {
+      for (int j = 0; j < b.size(); ++j) {
+        lli s = 0;
+        for (int k = 0; i + k < a.size() && j + k < b.size(); ++k) {
+          s += abs(a[i + k] - b[j + k]);
+          cost[i][j][k + 1] = s;
+        }
+      }
+    }
+
+    const int BIT = 1 << N;
+    // const int BIT = 1 << 22;
+    // static lli dp[N][BIT]; // [bをi番目まで見た][aで使ったもの]
+    static lli dp[BIT]; // [bをi番目まで見た][aで使ったもの]
+    fill(&dp[0], &dp[BIT - 1] + 1, inf);
+    dp[0] = 0;
+    for (int bit = 0; bit < (1 << n); ++bit) {
+      const int i = __builtin_popcount(bit);
+      if (dp[bit] == inf) continue;
+      for (int j = i + 1; j <= n; ++j) {
+        const int len = j - i;
+        for (int k = 0; k < n; ++k) {
+          if (cost[i][k][len] == inf) continue;
+          const int mask = ((1 << len) - 1);
+          if (bit & (mask * (1 << k))) continue;
+          setmin(dp[bit | (mask << k)], dp[bit] + cost[i][k][len] + c);
+        }
+      }
+    }
+    cout << dp[(1 << n) - 1] - c << endl;
+  }
+  return 0;
+}
