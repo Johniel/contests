9d8f581468c39dce59f3b638bbef2302a92ca9c8
Johniel <johniel.s.m@gmail.com>
2024-02-24 16:59:07 +0900

    D

---
+++ codeforces/e612/D/main.cpp
@@ -0,0 +1,123 @@
+// github.com/Johniel/contests
+// codeforces/e612/D/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+#define makepair(a, b) make_pair(a, b)
+// #define endl "\n"
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p);
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p);
+template<typename P, typename Q, typename R> ostream& operator << (ostream& os, tuple<P, Q, R> t) { os << "(" << get<0>(t) << "," << get<1>(t) << "," << get<2>(t) << ")"; return os; }
+template<typename P, typename Q, typename R> istream& operator >> (istream& is, tuple<P, Q, R>& t) { is >> get<0>(t) >> get<1>(t) >> get<2>(t); return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "set{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "map{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, stack<T> s) { os << "stack{"; while (s.size()) { os << s.top() << ","; s.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, queue<T> q) { os << "queue{"; while (q.size()) { os << q.front() << ","; q.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, deque<T> q) { os << "deque{"; for (int i = 0; i < q.size(); ++i) os << q[i] << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, priority_queue<T> q) { os << "heap{"; while (q.size()) { os << q.top() << ","; q.pop(); } os << "}"; return os; }
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void ___initio(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+// constexpr lli mod = 1e9 + 7;
+constexpr lli mod = 998244353;
+
+template<typename T>
+struct PrefixSum {
+  vector<T> sum;
+  PrefixSum(vector<T> v) {
+    sum.push_back(0);
+    for (int i = 0; i < v.size(); ++i) {
+      sum.push_back(sum.back() + v[i]);
+    }
+  }
+  T operator () (size_t begin, size_t end) const {
+    assert(begin <= end);
+    return sum[end] - sum[begin];
+  }
+  T operator () (size_t end) const {
+    return (*this)(0, end);
+  }
+  int lower_bound(T x) const {
+    return std::lower_bound(sum.begin(), sum.end(), x) - sum.begin();
+  }
+  int upper_bound(T x) const {
+    return std::upper_bound(sum.begin(), sum.end(), x) - sum.begin();
+  }
+  size_t size(void) const { return (int)sum.size() - 1; }
+};
+
+const int inf = (1 << 29);
+
+vec<int> fn(const vec<lli>& a)
+{
+  PrefixSum<lli> sum(a);
+
+  vec<int> same;
+  for (int i = 0; i + 1 < a.size(); ++i) {
+    same.push_back(a[i] == a[i + 1]);
+  }
+  PrefixSum<int> s(same);
+
+  auto f = [&] (int i, int mid) {
+    return a[i] < sum(i + 1, mid) && s(i + 1, mid - 1) != (mid - 1) - (i + 1);
+  };
+
+  vec<int> v(a.size(), inf);
+  for (int i = 0; i + 1 < a.size(); ++i) {
+    if (a[i] < a[i + 1]) {
+      v[i] = 1;
+      continue;
+    }
+    int small = i;
+    int large = a.size();
+    while (small + 1 < large) {
+      int mid = (small + large) / 2;
+      if (f(i, mid)) large = mid;
+      else small = mid;
+    }
+    if (f(i, large)) setmin(v[i], large - i - 1);
+  }
+
+  return v;
+}
+
+int main(int argc, char *argv[])
+{
+  { int _; cin >> _; }
+  int n;
+  while (cin >> n) {
+    vec<lli> a(n);
+    cin >> a;
+
+    vec<int> v = fn(a);
+    reverse(a.begin(), a.end());
+    vec<int> u = fn(a);
+    reverse(a.begin(), a.end());
+    reverse(u.begin(), u.end());
+    for (int i = 0; i < v.size(); ++i) {
+      int mn = min(u[i], v[i]);
+      cout << (mn == inf ? -1 : mn) << ' ';
+    }
+    cout << '\n';
+  }
+  return 0;
+}
