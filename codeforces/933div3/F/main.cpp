477254b1ca5c170643b72f6e0a17ac58a8509de8
Johniel <johniel.s.m@gmail.com>
2024-03-12 15:03:54 +0900

    F

---
+++ codeforces/933div3/F/main.cpp
@@ -0,0 +1,98 @@
+// github.com/Johniel/contests
+// codeforces/933div3/F/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+#define makepair(a, b) make_pair(a, b)
+// #define endl "\n"
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p);
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p);
+template<typename P, typename Q, typename R> ostream& operator << (ostream& os, tuple<P, Q, R> t) { os << "(" << get<0>(t) << "," << get<1>(t) << "," << get<2>(t) << ")"; return os; }
+template<typename P, typename Q, typename R> istream& operator >> (istream& is, tuple<P, Q, R>& t) { is >> get<0>(t) >> get<1>(t) >> get<2>(t); return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "set{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "map{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, stack<T> s) { os << "stack{"; while (s.size()) { os << s.top() << ","; s.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, queue<T> q) { os << "queue{"; while (q.size()) { os << q.front() << ","; q.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, deque<T> q) { os << "deque{"; for (int i = 0; i < q.size(); ++i) os << q[i] << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, priority_queue<T> q) { os << "heap{"; while (q.size()) { os << q.top() << ","; q.pop(); } os << "}"; return os; }
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void ___initio(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+// constexpr lli mod = 1e9 + 7;
+constexpr lli mod = 998244353;
+
+int main(int argc, char *argv[])
+{
+  { int _; cin >> _; }
+  int n, m, k;
+  while (cin >> n >> m >> k) {
+    vec<int> a(n);
+    vec<int> d(m);
+    vec<int> f(k);
+    cin >> a >> d >> f;
+    sort(d.begin(), d.end());
+    sort(f.begin(), f.end());
+
+    vec<int> diff;
+    for (int i = 0; i + 1 < a.size(); ++i) {
+      diff.push_back(a[i + 1] - a[i]);
+    }
+
+    int mx = *max_element(diff.begin(), diff.end());
+    if (1 < count(diff.begin(), diff.end(), mx)) {
+      cout << mx << endl;
+      continue;
+    }
+
+    sort(diff.begin(), diff.end());
+    diff.pop_back();
+    for (int i = 0; i + 1 < a.size(); ++i) {
+      unless (a[i + 1] - a[i] == mx) continue;
+
+      auto fn = [&] (lli r) {
+        lli x = a[i + 1] - r;
+        lli y = a[i] + r;
+        unless (x <= y) return false;
+        for (int j = 0; j < d.size(); ++j) {
+          lli mn = x - d[j];
+          lli mx = y - d[j];
+          auto itr = lower_bound(f.begin(), f.end(), mn);
+          if (itr != f.end() && *itr <= mx) return true;
+        }
+        return false;
+      };
+
+      lli small = 0;
+      lli large = mx;
+      while (small + 1 < large) {
+        auto mid = (small + large) / 2;
+        if (fn(mid)) large = mid;
+        else small = mid;
+      }
+      diff.push_back(large);
+      break;
+    }
+    sort(diff.begin(), diff.end());
+    cout << diff.back() << endl;
+  }
+  return 0;
+}
