81ed4b620afa426c0140edf7fdf935f5aa7d7651
Johniel <johniel.s.m@gmail.com>
2024-03-31 03:36:30 +0900

    E

--- atcoder/abc347/E/main.cpp
+++ atcoder/abc347/E/main.cpp
@@ -40,35 +40,186 @@ template<typename T> using vec = vector<T>;
 // constexpr lli mod = 1e9 + 7;
 constexpr lli mod = 998244353;
 
-#include "atcoder/lazysegtree"
-#include "atcoder/modint"
+template<typename S, typename F>
+class LazySegTree {
+public:
+  using op_fn = function<S(S, S)>;
+  using mapping_fn = function<S(F, S)>;
+  using composition_fn = function<F(F, F)>; // composition(f(x), g(x)):=f(g(x))
 
-using namespace std;
-using namespace atcoder;
+  LazySegTree(size_t n_, op_fn op_, mapping_fn mapping_, composition_fn composition_, S e_, F id_)
+    : size(n_), op(op_), mapping(mapping_), composition(composition_), e(e_), id(id_), n(bit_ceil(n_)) {
+    value.resize(n * 2 - 1, e);
+    lazy.resize(n * 2 - 1, id);
+  }
 
-using mint = modint998244353;
-template<int m> ostream& operator << (ostream& os, static_modint<m> x) { os << x.val(); return os; }
-template<int m> ostream& operator << (ostream& os, dynamic_modint<m> x) { os << x.val(); return os; }
+  void set(size_t i, S x) {
+    assert(0 <= i && i < size);
+    get(i);
+    i += n - 1;
+    value[i] = x;
+    while (0 < i) {
+      i = (i - 1) / 2;
+      value[i] = op(value.at(i * 2 + 1), value.at(i * 2 + 2));
+    }
+    return ;
+  }
 
-struct S {
-  bool vis;
-  lli sum;
+  inline S get(size_t i) { return query(i, i + 1); }
+  inline S query(void) { return query(0, size); };
+  S query(size_t begin, size_t end) {
+    assert(begin <= end);
+    assert(end <= size);
+    return query(begin, end, 0, 0, n);
+  }
+  S prod(size_t begin, size_t end) { return query(begin, end); }
+  S all_prod(void) { return query(); }
+
+  inline S apply(size_t i, F f) { return apply(i, i + 1, f); }
+  S apply(size_t begin, size_t end, F f) {
+    assert(begin <= end);
+    assert(end <= size);
+    return apply(begin, end, f, 0, 0, n);
+  }
+
+  void show(ostream& os, int idx = 0, int depth = 0) const {
+    if (idx < value.size()) {
+      os << string(depth, ' ') << make_pair(value[idx], lazy[idx]) << endl;
+      show(os, idx * 2 + 1, depth + 2);
+      show(os, idx * 2 + 2, depth + 2);
+    }
+    return ;
+  }
+
+  int max_right(int i, function<bool(S)> pred) {
+    assert(pred(e));
+    int small = i;
+    int large = size;
+    while (small + 1 < large) {
+      int mid = (small + large) / 2;
+      if (pred(query(i, mid))) small = mid;
+      else large = mid;
+    }
+    // unless (pred(query(i, small))) return i;
+    return small;
+  }
+
+  int min_left() {
+    assert("not implemented");
+    return 0;
+  }
+
+private:
+  vector<S> value;
+  vector<F> lazy;
+  const int n;
+  const size_t size;
+
+  op_fn op;
+  composition_fn composition;
+  mapping_fn mapping;
+  const S e;
+  const F id;
+
+  inline void push(int k) {
+    if(2 * k + 2 < lazy.size()) {
+      lazy[2 * k + 1] = composition(lazy[k], lazy[2 * k + 1]);
+      lazy[2 * k + 2] = composition(lazy[k], lazy[2 * k + 2]);
+    }
+    value[k] = mapping(lazy[k], value[k]);
+    lazy[k] = id;
+    return ;
+  }
+
+  S apply(size_t begin, size_t end, F f, size_t k, size_t l, size_t r) {
+    push(k);
+    if (r <= begin || end <= l) return value[k];
+
+    if (begin <= l && r <= end) {
+      lazy[k] = composition(f, lazy[k]);
+      push(k);
+      return value[k];
+    } else {
+      S vl = apply(begin, end, f, k * 2 + 1, l, (l + r) / 2);
+      S vr = apply(begin, end, f, k * 2 + 2, (l + r) / 2, r);
+      return value[k] = op(vl, vr);
+    }
+  }
+
+  S query(size_t begin, size_t end, size_t k, size_t l, size_t r) {
+    if (r <= begin || end <= l) return e;
+    push(k);
+
+    if (begin <= l && r <= end) {
+      return value[k];
+    } else {
+      S vl = query(begin, end, k * 2 + 1, l, (l + r) / 2);
+      S vr = query(begin, end, k * 2 + 2, (l + r) / 2, r);
+      return op(vl, vr);
+    }
+  }
 };
-struct F {
-  lli m;
-};
-S op(S l, S r) { return l; }
-S e() { return S{false, 0}; }
-S mapping(F l, S r) {
-  if (r.vis) r.sum += l.m;
-  return r;
-} // 区間更新をしないならlを捨てることも考える。
-F composition(F l, F r) { return F{r.m + l.m}; } // lが後。
-F id() { return F{0}; }
-//    vec<S> ini(n, S{0, 1});
-//    lazy_segtree<S, op, e, F, mapping, composition, id> seg(ini);
-// https://github.com/atcoder/ac-library/blob/master/document_ja/lazysegtree.md
+template<typename S, typename F> ostream& operator << (ostream& os, LazySegTree<S, F> seg) { seg.show(os); return os; }
 
+
+template<typename F>
+struct DualSegTree {
+  // ABC017D
+  using composition_fn = function<F(F, F)>; // composition(f(x), g(x)):=f(g(x))
+  DualSegTree(size_t n_, composition_fn composition_, F e_)
+    : n(n_), composition(composition_), e(e_), bit_ceiled_n(bit_ceil(n_)) {
+    lazy.resize(2 * bit_ceiled_n, e);
+  }
+  void apply(size_t begin, size_t end, F f) {
+    assert(0 <= begin && begin < n);
+    assert(0 <= end && end <= n);
+    assert(begin <= end);
+    size_t a = begin + bit_ceiled_n;
+    size_t b = end + bit_ceiled_n;
+    push((a >> __builtin_ctz(a)));
+    push((b >> __builtin_ctz(b)) - 1);
+    for (; a < b; a /= 2, b /= 2 ) {
+      if (a & 1) {
+        lazy[a] = composition(f, lazy[a]);
+        ++a;
+      }
+      if (b & 1) {
+        --b;
+        lazy[b] = composition(f, lazy[b]);
+      }
+    }
+    return ;
+  }
+  void set(size_t idx, F f) {
+    assert(0 <= idx && idx < n);
+    push(idx + bit_ceiled_n);
+    lazy[idx + bit_ceiled_n] = f;
+    return ;
+  }
+  F get(size_t idx) {
+    assert(0 <= idx && idx < n);
+    push(idx + bit_ceiled_n);
+    return lazy[idx + bit_ceiled_n];
+  }
+  size_t size(void) const { return n; } ;
+  vector<F> lazy;
+  const composition_fn composition;
+  const F e;
+  const size_t n;
+  const size_t bit_ceiled_n;
+  void push(size_t idx) {
+    int depth = bit_width(idx);
+    for (int d = depth - 1; 0 < d; --d) {
+      size_t i = idx >> d;
+      if (lazy[i] != e) {
+        lazy[i * 2 + 0] = composition(lazy[i], lazy.at(i * 2 + 0));
+        lazy[i * 2 + 1] = composition(lazy[i], lazy.at(i * 2 + 1));
+        lazy[i] = e;
+      }
+    }
+    return ;
+  }
+};
 
 int main(int argc, char *argv[])
 {
@@ -77,30 +228,45 @@ int main(int argc, char *argv[])
     vec<int> v(q);
     cin >> v;
 
-    const int N = 2 * 1e5 + 3;
+    const int enable = 0;
+    const int disable = 1;
+    const int nil = 2;
 
-    vec<S> ini(N, S{false, 0});
-    lazy_segtree<S, op, e, F, mapping, composition, id> seg(ini);
+    using S = pair<int, lli>; // 有無,sum
+    using F = lli;
+    using lazy_segtree = LazySegTree<S, F>;
+    lazy_segtree::op_fn op = [&] (S a, S b) {
+      if (a.first != nil) return a;
+      if (b.first != nil) return b;
+      return make_pair(nil, 0LL);
+    };
+    lazy_segtree::mapping_fn mapping = [] (F a, S b) {
+      if (b.first == enable) b.second += a;
+      return b;
+    };
+    lazy_segtree::composition_fn composition = [] (F a, F b) { return a + b; }; // composition(f(x), g(x)):=f(g(x))
+    const int N = 2 * 1e5 + 3;
+    lazy_segtree seg(n+1, op, mapping, composition, make_pair(nil, 0LL), 0);
+    for (int i = 1; i <= n; ++i) {
+      seg.set(i, make_pair(disable, 0LL));
+    }
 
     set<int> vis;
     each (i, v) {
       if (vis.count(i)) {
-        auto  p = seg.get(i);
-        p.vis = false;
+        pair<bool, lli> p = seg.get(i);
+        p.first = disable;
         seg.set(i, p);
         vis.erase(i);
       } else {
-        auto  p = seg.get(i);
-        p.vis = true;
+        pair<bool, lli> p = seg.get(i);
+        p.first = enable;
         seg.set(i, p);
         vis.insert(i);
       }
-      seg.apply(0, N, F{(lli)vis.size()});
+      seg.apply(0, n+1, vis.size());
     }
-    for (int i = 0; i < n; ++i) {
-      cout << seg.get(i + 1).sum << ' ';
-    }
-    cout << endl;
+    for (int i = 1; i <= n; ++i) cout << seg.get(i).second << ' '; cout << endl;
   }
   return 0;
 }
