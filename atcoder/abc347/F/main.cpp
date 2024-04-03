9c8d745be308c8671e8b72b1f7da2b48ecece53e
Johniel <johniel.s.m@gmail.com>
2024-04-03 15:13:19 +0900

    F

---
+++ atcoder/abc347/F/main.cpp
@@ -0,0 +1,202 @@
+// github.com/Johniel/contests
+// atcoder/abc347/F/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+#define makepair(a, b) make_pair(a, b)
+// #define endl "\n"
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p);
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p);
+template<typename P, typename Q, typename R> ostream& operator << (ostream& os, tuple<P, Q, R> t) { os << "(" << get<0>(t) << "," << get<1>(t) << "," << get<2>(t) << ")"; return os; }
+template<typename P, typename Q, typename R> istream& operator >> (istream& is, tuple<P, Q, R>& t) { is >> get<0>(t) >> get<1>(t) >> get<2>(t); return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "set{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "map{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, stack<T> s) { os << "stack{"; while (s.size()) { os << s.top() << ","; s.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, queue<T> q) { os << "queue{"; while (q.size()) { os << q.front() << ","; q.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, deque<T> q) { os << "deque{"; for (int i = 0; i < q.size(); ++i) os << q[i] << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, priority_queue<T> q) { os << "heap{"; while (q.size()) { os << q.top() << ","; q.pop(); } os << "}"; return os; }
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void _____(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+template<typename T>
+struct PrefixSum2D {
+  vector<vector<T>> sum;
+  PrefixSum2D() {}
+  PrefixSum2D(vector<vector<T>> v) {
+    const int H = v.size() + 1;
+    const int W = v[0].size() + 1;
+    sum.resize(H);
+    fill(sum.begin(), sum.end(), vector<T>(W, 0));
+    for (int i = 0; i < v.size(); ++i) {
+      for (int j = 0; j < v[i].size(); ++j) {
+        sum[i + 1][j + 1] = v[i][j];
+      }
+    }
+    for (int i = 1; i < H; ++i) {
+      for (int j = 0; j < W; ++j) {
+        sum[i][j] += sum[i - 1][j];
+      }
+    }
+    for (int i = 0; i < H; ++i) {
+      for (int j = 1; j < W; ++j) {
+        sum[i][j] += sum[i][j - 1];
+      }
+    }
+  }
+  T operator () (int ai, int aj, int bi, int bj) const { return this->query(ai, aj, bi, bj); }
+  T query(int ai, int aj, int bi, int bj) const {
+    assert(0 <= ai && ai < sum.size());
+    assert(0 <= bi && bi < sum.size());
+    assert(0 <= aj && aj < sum[0].size());
+    assert(0 <= bj && bj < sum[0].size());
+    assert(ai <= bi && aj <= bj);
+    return sum[bi][bj] + sum[ai][aj] - sum[ai][bj] - sum[bi][aj];
+  }
+  pair<size_t, size_t> size(void) const { return make_pair(sum.size() - 1, sum[0].size() - 1); }
+};
+
+template <typename T, typename F>
+struct SegmentTree2D {
+private:
+  int id(int h, int w) const { return h * 2 * W + w; }
+
+public:
+  int H, W;
+  vector<T> seg;
+  const F f;
+  const T I;
+
+  SegmentTree2D(int h, int w, F _f, const T& i) : f(_f), I(i) { init(h, w); }
+
+  void init(int h, int w) {
+    H = W = 1;
+    while (H < h) H <<= 1;
+    while (W < w) W <<= 1;
+    seg.assign(4 * H * W, I);
+  }
+
+  // build にのみ呼ぶ
+  void set(int h, int w, const T& x) { seg[id(h + H, w + W)] = x; }
+
+  void build() {
+    // w in [W, 2W)
+    for (int w = W; w < 2 * W; w++) {
+      for (int h = H - 1; h; h--) {
+        seg[id(h, w)] = f(seg[id(2 * h + 0, w)], seg[id(2 * h + 1, w)]);
+      }
+    }
+    // h in [0, 2H)
+    for (int h = 0; h < 2 * H; h++) {
+      for (int w = W - 1; w; w--) {
+        seg[id(h, w)] = f(seg[id(h, 2 * w + 0)], seg[id(h, 2 * w + 1)]);
+      }
+    }
+  }
+
+  T get(int h, int w) const { return seg[id(h + H, w + W)]; }
+  T operator()(int h, int w) const { return seg[id(h + H, w + W)]; }
+
+  void update(int h, int w, const T& x) {
+    h += H, w += W;
+    seg[id(h, w)] = x;
+    for (int i = h >> 1; i; i >>= 1) {
+      seg[id(i, w)] = f(seg[id(2 * i + 0, w)], seg[id(2 * i + 1, w)]);
+    }
+    for (; h; h >>= 1) {
+      for (int j = w >> 1; j; j >>= 1) {
+        seg[id(h, j)] = f(seg[id(h, 2 * j + 0)], seg[id(h, 2 * j + 1)]);
+      }
+    }
+  }
+
+  T _inner_query(int h, int w1, int w2) {
+    T res = I;
+    for (; w1 < w2; w1 >>= 1, w2 >>= 1) {
+      if (w1 & 1) res = f(res, seg[id(h, w1)]), w1++;
+      if (w2 & 1) --w2, res = f(res, seg[id(h, w2)]);
+    }
+    return res;
+  }
+
+  // [ (h1,w1), (h2,w2) ) 半開
+  T query(int h1, int w1, int h2, int w2) {
+    if (h1 >= h2 || w1 >= w2) return I;
+    T res = I;
+    h1 += H, h2 += H, w1 += W, w2 += W;
+    for (; h1 < h2; h1 >>= 1, h2 >>= 1) {
+      if (h1 & 1) res = f(res, _inner_query(h1, w1, w2)), h1++;
+      if (h2 & 1) --h2, res = f(res, _inner_query(h2, w1, w2));
+    }
+    return res;
+  }
+};
+// https://nyaannyaan.github.io/library/data-structure-2d/2d-segment-tree.hpp.html
+
+
+int main(int argc, char *argv[])
+{
+  int n, m;
+  while (cin >> n >> m) {
+    vec<vec<lli>> g(n, vec<lli>(n));
+    cin >> g;
+
+    lli z = 0;
+    for (int _ = 0; _ < 4; ++_) {
+      PrefixSum2D<lli> sum(g);
+      SegmentTree2D seg(n, n, [] (lli a, lli b) {return max(a, b); }, -(1LL << 60));
+      for (int i = 0; i + m <= n; ++i) {
+        for (int j = 0; j + m <= n; ++j) {
+          lli x = sum(i, j, i + m, j + m);
+          seg.update(i, j, x);
+        }
+      }
+      for (int i = 0; i + m <= n; ++i) {
+        for (int j = 0; j + m <= n; ++j) {
+          lli a = seg.query(0, 0, i - m + 1, j - m + 1);
+          lli b = seg.query(0, j, i - m + 1, n - m + 1);
+          lli c = seg.query(i, 0, n - m + 1, n - m + 1);
+          setmax(z, a + b + c);
+        }
+        {
+          lli a = seg.query(0, 0, i - m + 1, n - m + 1);
+          lli b = seg.query(i, 0, n - m + 1, n - m + 1);
+          setmax(z, a + b);
+        }
+        {
+          int j = i + m;
+          lli a = seg.query(0, 0, i - m + 1, n - m + 1);
+          lli b = seg.query(i, 0, j - m + 1, n - m + 1);
+          lli c = seg.query(j, 0, n - m + 1, n - m + 1);
+          setmax(z, a + b + c);
+        }
+      }
+      vec<vec<lli>> tmp = g;
+      for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+          g[i][j] = tmp[n - j - 1][i];
+        }
+      }
+    }
+    cout << z << endl;
+  }
+  return 0;
+}
