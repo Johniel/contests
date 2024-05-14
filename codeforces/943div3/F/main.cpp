b0b7f9324d0f017d6c351fe46263d3689424642c
Johniel <johniel.s.m@gmail.com>
2024-05-14 19:20:11 +0900

    F

---
+++ codeforces/943div3/F/main.cpp
@@ -0,0 +1,82 @@
+// github.com/Johniel/contests
+// codeforces/943div3/F/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p);
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p);
+template<typename P, typename Q, typename R> ostream& operator << (ostream& os, tuple<P, Q, R> t) { os << "(" << get<0>(t) << "," << get<1>(t) << "," << get<2>(t) << ")"; return os; }
+template<typename P, typename Q, typename R> istream& operator >> (istream& is, tuple<P, Q, R>& t) { is >> get<0>(t) >> get<1>(t) >> get<2>(t); return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "set{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "map{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, stack<T> s) { os << "stack{"; while (s.size()) { os << s.top() << ","; s.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, queue<T> q) { os << "queue{"; while (q.size()) { os << q.front() << ","; q.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, deque<T> q) { os << "deque{"; for (int i = 0; i < q.size(); ++i) os << q[i] << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, priority_queue<T> q) { os << "heap{"; while (q.size()) { os << q.top() << ","; q.pop(); } os << "}"; return os; }
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void _____(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+int main(int argc, char *argv[])
+{
+  { int _; cin >> _; }
+  int n, q;
+  while (cin >> n >> q) {
+    vec<int> a(n);
+    cin >> a;
+
+    vec<int> b({0});
+    map<int, vec<int>> m;
+    m[0].push_back(0);
+    each (i, a) {
+      b.push_back(b.back() ^ i);
+      m[b.back()].push_back(b.size() - 1);
+    }
+
+    while (q--) {
+      int begin, end;
+      cin >> begin >> end;
+      --begin;
+      { // 2
+        if (b[begin] == b[end]) {
+          cout << "yes" << endl;
+          continue;
+        }
+      }
+      { // 3
+        int A = b[end];
+        auto itrA = lower_bound(m[A].begin(), m[A].end(), begin + 1);
+        int B = b[begin];
+        auto itr0 = lower_bound(m[B].begin(), m[B].end(), end);
+        if (itr0 != m[B].begin()) {
+          --itr0;
+          if (itrA != m[A].end() && *itrA < *itr0) {
+            cout << "YES" << endl;
+            continue;
+          }
+        }
+      }
+      cout << "NO" << endl;
+    }
+    cout << endl;
+  }
+  return 0;
+}
