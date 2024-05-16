75535c27b5f23b3d36e0758e3f1dd170ab9ee241
Johniel <johniel.s.m@gmail.com>
2024-05-16 15:14:40 +0900

    G

--- codeforces/943div3/G/main.cpp
+++ codeforces/943div3/G/main.cpp
@@ -35,52 +35,6 @@ using ull = unsigned long long;
 using str = string;
 template<typename T> using vec = vector<T>;
 
-template<typename T>
-struct SegTree {
-  const int n;
-  const int origin_size;
-  vector<T> v;
-  using F = function<T(T, T)>;
-  const F fn;
-  const T e;
-  SegTree(size_t n_, T e_, F fn_) : e(e_), origin_size(n_), fn(fn_), n(bit_ceil(n_)) {
-    assert(fn(e, e) == e);
-    v.resize(2 * n - 1, e);
-  }
-  SegTree(const vector<T>& v, T e_, F fn_) : SegTree(v.size(), e_, fn_) {
-    for (int i = 0; i < v.size(); ++i) set(i, v[i]);
-  }
-  void set(size_t k, T a) {
-    k += n - 1;
-    v[k] = a;
-    while (k > 0) {
-      k = (k - 1) / 2;
-      v[k] = fn(v[k * 2 + 1], v[k * 2 + 2]);
-    }
-    return ;
-  }
-  inline T get(size_t idx) const { return v.at(idx + n - 1); }
-  inline T operator () (void) const { return query(0, origin_size, 0, 0, n); }
-  inline T operator () (size_t begin, size_t end) const { return query(begin, end, 0, 0, n); }
-  inline T all_prod(void) const { return query(0, origin_size, 0, 0, n); }
-  inline T prod(size_t begin, size_t end) const { return query(begin, end, 0, 0, n); }
-  inline T query(size_t begin, size_t end) const {
-    assert(begin <= end);
-    assert(end <= origin_size);
-    return query(begin, end, 0, 0, n);
-  }
-  T query(size_t a, size_t b, size_t k, size_t l, size_t r) const {
-    if (r <= a || b <= l) return e;
-    if (a <= l && r <= b) return v.at(k);
-    T vl = query(a, b, k * 2 + 1, l, (l + r) / 2);
-    T vr = query(a, b, k * 2 + 2, (l + r) / 2, r);
-    return fn(vl, vr);
-  }
-  size_t size(void) const { return origin_size; }
-};
-template<typename T> istream& operator >> (istream& is, SegTree<T>& seg) { for (int i = 0; i < seg.origin_size; ++i) { T t; is >> t; seg.set(i, t); } return is; }
-template<typename T> ostream& operator << (ostream& os, SegTree<T>& seg) { vector<T> v; for (int i = 0; i < seg.n; ++i) v.push_back(seg[i]); os << v; return os; }
-
 // s自身とすべてのsuffixとの共通接頭辞の文字数を返す。
 // つまり、{|LCP(s[0], s.substring(0))|, |LCP(s[0], s.substring(1))|, |LCP(s[0], s.substring(2))|, ... }
 // verified at ABC257G
@@ -108,29 +62,42 @@ vector<int> z_algorithm(const string& s)
   return v;
 }
 
-namespace cfseg{
-// https://codeforces.com/blog/entry/18051
-  const int N = 3 * 1e5;  // limit for array size
-  int n;  // array size
-  int t[2 * N];
-
-  void build() {  // build the tree
-    for (int i = n - 1; i > 0; --i) t[i] = max(t[i<<1], t[i<<1|1]);
+template<typename T>
+struct SegTree {
+  // https://codeforces.com/blog/entry/18051
+  using F = function<T(T, T)>;
+  const F op;
+  const T e;
+  const int n;
+  vector<T> v;
+  SegTree(size_t n_, T e_, F op_) : e(e_), n(n_), op(op_) {
+    assert(op(e, e) == e);
+    v.resize(2 * n, e);
   }
-
-  void modify(int p, int value) {  // set value at position p
-    for (t[p += n] = value; p > 1; p >>= 1) t[p>>1] = max(t[p], t[p^1]);
+  SegTree(const vector<T>& v, T e_, F op_) : SegTree(v.size(), e_, op_) {
+    for (int i = 0; i < v.size(); ++i) set(i, v[i]);
   }
-
-  int query(int l, int r) {  // sum on interval [l, r)
-    int res = 0;
+  void set(size_t k, T a) {
+    for (v[k += n] = a; k > 1; k >>= 1) v[k >> 1] = op(v[k], v[k ^ 1]);
+    return ;
+  }
+  inline T get(size_t k) const { return v.at(k + n); }
+  inline T operator () (void) const { return query(0, n, 0, 0, n); }
+  inline T operator () (size_t begin, size_t end) const { return query(begin, end); }
+  inline T all_prod(void) const { return query(0, n); }
+  inline T prod(size_t begin, size_t end) const { return query(begin, end); }
+  T query(size_t l, size_t r) {
+    T res = 0;
     for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
-      if (l&1) setmax(res, t[l++]); // res += t[l++];
-      if (r&1) setmax(res, t[--r]); // res += t[--r];
+      if (l & 1) res = op(v[l++], res);
+      if (r & 1) res = op(res, v[--r]);
     }
     return res;
   }
+  size_t size(void) const { return n; }
 };
+template<typename T> istream& operator >> (istream& is, SegTree<T>& seg) { for (int i = 0; i < seg.size(); ++i) { T t; is >> t; seg.set(i, t); } return is; }
+template<typename T> ostream& operator << (ostream& os, SegTree<T>& seg) { vector<T> v; for (int i = 0; i < seg.size(); ++i) v.push_back(seg[i]); os << v; return os; }
 
 int main(int argc, char *argv[])
 {
@@ -139,12 +106,7 @@ int main(int argc, char *argv[])
   str s;
   while (cin >> n >> l >> r >> s) {
     vec<int> za = z_algorithm(s);
-    cfseg::n = za.size();
-    for (int i = 0; i < za.size(); ++i) {
-      cfseg::t[i + n] = za[i];
-    }
-    cfseg::build();
-    // SegTree<int> seg(za, 0, [] (auto a, auto b) { return max(a, b); });
+    SegTree<int> seg(za, 0, [] (auto a, auto b) { return max(a, b); });
     vec<int> v(n+1, -1);
     for (int len = 2; len < v.size(); ++len) {
       int prev = 0;
@@ -154,10 +116,10 @@ int main(int argc, char *argv[])
         int large = za.size();
         while (small + 1 < large) {
           int mid = (small + large) / 2;
-          if (len <= cfseg::query(prev, mid)) large = mid;
+          if (len <= seg.query(prev, mid)) large = mid;
           else small = mid;
         }
-        auto p = cfseg::query(prev, large);
+        auto p = seg.query(prev, large);
         if (p < len) break;
         ++k;
         prev = large - 1 + len;
@@ -171,7 +133,6 @@ int main(int argc, char *argv[])
         setmax(w[j], i);
       }
     }
-    // cout << v << endl; cout << w << endl;
     w[1] = n;
     for (int i = l; i <= r; ++i) cout << w.at(i) << ' '; cout << "\n";
   }
