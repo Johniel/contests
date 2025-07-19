b97413d27f13eddbdeef1ed623beec653028daf7
Johniel <johniel.s.m@gmail.com>
2025-07-19 14:05:24 +0900

    E

---
+++ atcoder/abc409/E/main.cpp
@@ -0,0 +1,84 @@
+// github.com/Johniel/contests
+// atcoder/abc409/E/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+#define __builtin_popcount(x) __builtin_popcountll(x)
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p);
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p);
+template<typename... Ts> ostream& operator << (ostream& os, const tuple<Ts...>& t) { os << "("; if constexpr (sizeof...(Ts) > 0) { apply([&](const Ts&... args) { ((os << args << ','), ...); }, t); } os << ")"; return os; }
+template<typename... Ts> istream& operator >> (istream& is, tuple<Ts...>& t) { apply([&](Ts&... args) { ((is >> args), ...); }, t); return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "set{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "map{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, stack<T> s) { os << "stack{"; while (s.size()) { os << s.top() << ","; s.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, queue<T> q) { os << "queue{"; while (q.size()) { os << q.front() << ","; q.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, deque<T> q) { os << "deque{"; for (int i = 0; i < q.size(); ++i) os << q[i] << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, priority_queue<T> q) { os << "heap{"; while (q.size()) { os << q.top() << ","; q.pop(); } os << "}"; return os; }
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void _____(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr lli mod = 998244353; // 1e9 + 7;
+
+const int N = 2 * 1e5 + 3;
+vec<pair<int, int>> g[N];
+vec<lli> xs;
+lli z;
+pair<lli, lli> rec(int curr, int prev)
+{
+  pair<lli, lli> p = make_pair(0, 0);
+  if (xs[curr] < 0) p.second += abs(xs[curr]);
+  else p.first += xs[curr];
+  each (e, g[curr]) {
+    auto [next, cost] = e;
+    if (next == prev) continue;
+    auto [a, b]= rec(next, curr);
+    p.first += a;
+    p.second += b;
+    z += cost * (a + b);
+  }
+  int mn = min(p.first, p.second);
+  p.first -= mn;
+  p.second -= mn;
+  return p;
+}
+
+int main(int argc, char *argv[])
+{
+  int n;
+  while (cin >> n) {
+    fill(g, g + n, vec<pair<int, int>>());
+    xs.resize(n);
+    cin >> xs;
+    for (int i = 0; i < n - 1; ++i) {
+      int a, b, c;
+      cin >> a >> b >> c;
+      --a;
+      --b;
+      g[a].push_back(make_pair(b, c));
+      g[b].push_back(make_pair(a, c));
+    }
+    z = 0;
+    rec(0, 0);
+    cout << z << endl;
+  }
+  return 0;
+}
