50fba71c93d64572bcd06331fe9fdfac49a165de
Johniel <johniel.s.m@gmail.com>
2025-08-03 11:14:43 +0900

    ABCDE

---
+++ atcoder/abc417/E/main.cpp
@@ -0,0 +1,95 @@
+// github.com/Johniel/contests
+// atcoder/abc417/E/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+#define __builtin_popcount(x) __builtin_popcountll(x)
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p);
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p);
+template<typename... Ts> ostream& operator << (ostream& os, const tuple<Ts...>& t) { os << "("; if constexpr (sizeof...(Ts) > 0) { apply([&](const Ts&... args) { ((os << args << ','), ...); }, t); } os << ")"; return os; }
+template<typename... Ts> istream& operator >> (istream& is, tuple<Ts...>& t) { apply([&](Ts&... args) { ((is >> args), ...); }, t); return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "set{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "map{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, stack<T> s) { os << "stack{"; while (s.size()) { os << s.top() << ","; s.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, queue<T> q) { os << "queue{"; while (q.size()) { os << q.front() << ","; q.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, deque<T> q) { os << "deque{"; for (int i = 0; i < q.size(); ++i) os << q[i] << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, priority_queue<T> q) { os << "heap{"; while (q.size()) { os << q.top() << ","; q.pop(); } os << "}"; return os; }
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void _____(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr lli mod = 998244353; // 1e9 + 7;
+
+const int N = 1000 + 3;
+vec<int> g[N];
+
+vec<int> path;
+vec<int> vis;
+vec<int> taboo;
+int n, m, x, y;
+void rec(int curr) {
+  vis[curr] = 1;
+  path.push_back(curr);
+  if (y == curr) {
+    each (i, path) cout << i + 1 << ' ';
+    cout << endl;
+    throw "";
+  }
+  each (next, g[curr]) {
+    if (vis[next] || taboo[next]) continue;
+    rec(next);
+  }
+  taboo[curr] = 1;
+  path.pop_back();
+  vis[curr] = 0;
+  return ;
+}
+
+int main(int argc, char *argv[])
+{
+  { int _; cin >> _; }
+  while (cin >> n >> m >> x >> y) {
+    fill(g, g + n, vec<int>());
+    --x;
+    --y;
+    for (int i = 0; i < m; ++i) {
+      int a, b;
+      cin >> a >> b;
+      --a;
+      --b;
+      g[a].push_back(b);
+      g[b].push_back(a);
+    }
+    for (int i = 0; i < n; ++i) {
+      sort(g[i].begin(), g[i].end());
+    }
+    vis.resize(n, 0);
+    taboo.resize(n, 0);
+    path.clear();
+    try {
+      fill(vis.begin(), vis.end(), 0);
+      fill(taboo.begin(), taboo.end(), 0);
+      rec(x);
+    } catch (const char* e) {
+    }
+  }
+  return 0;
+}
