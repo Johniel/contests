d18fcc93651e823268137e1cab90d7735fe2832d
Johniel <johniel.s.m@gmail.com>
2025-08-24 22:43:54 +0900

    ABCDE

---
+++ atcoder/abc420/E/main.cpp
@@ -0,0 +1,107 @@
+// github.com/Johniel/contests
+// atcoder/abc420/E/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+#define __builtin_popcount(x) __builtin_popcountll(x)
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p);
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p);
+template<typename... Ts> ostream& operator << (ostream& os, const tuple<Ts...>& t) { os << "("; if constexpr (sizeof...(Ts) > 0) { apply([&](const Ts&... args) { ((os << args << ','), ...); }, t); } os << ")"; return os; }
+template<typename... Ts> istream& operator >> (istream& is, tuple<Ts...>& t) { apply([&](Ts&... args) { ((is >> args), ...); }, t); return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "set{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "map{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, stack<T> s) { os << "stack{"; while (s.size()) { os << s.top() << ","; s.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, queue<T> q) { os << "queue{"; while (q.size()) { os << q.front() << ","; q.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, deque<T> q) { os << "deque{"; for (int i = 0; i < q.size(); ++i) os << q[i] << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, priority_queue<T> q) { os << "heap{"; while (q.size()) { os << q.top() << ","; q.pop(); } os << "}"; return os; }
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void _____(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr lli mod = 998244353; // 1e9 + 7;
+
+struct UnionFind {
+  vector<int> r, p;
+  UnionFind(int n): r(n, 0), p(n, -1) {}
+  bool unite(int a, int b) {
+    a = find(a);
+    b = find(b);
+    if (a == b) return false;
+    if (r[a] > r[b]) swap(a, b);
+    p[b] += p[a];
+    p[a] = b;
+    if (r[a] == r[b]) ++r[b];
+    return true;
+  }
+  int find(int a) {
+    return (p[a] < 0) ? a : p[a] = find(p[a]);
+  }
+  bool same(int a, int b) {
+    return find(a) == find(b);
+  }
+  size_t size(int n) {
+    return -p.at(find(n));
+  }
+};
+
+int main(int argc, char *argv[])
+{
+  int n, q;
+  while (cin >> n >> q) {
+    UnionFind uf(n);
+    vec<int> black(n, 0);
+    vec<int> cnt(n, 0);
+    while (q--) {
+      int op;
+      cin >> op;
+      if (op == 1) {
+        int u, v;
+        cin >> u >> v;
+        --u;
+        --v;
+        u = uf.find(u);
+        v = uf.find(v);
+        if (uf.same(u, v)) continue;
+        int tmp = cnt[v] + cnt[u];
+        cnt[v] = cnt[u] = 0;
+        uf.unite(u, v);
+        int rep = uf.find(u);
+        cnt[rep] = tmp;
+      }
+      if (op == 2) {
+        int v;
+        cin >> v;
+        --v;
+        int rep = uf.find(v);
+        if (black[v]) --cnt[rep];
+        black[v] ^= 1;
+        if (black[v]) ++cnt[rep];
+      }
+      if (op == 3) {
+        int v;
+        cin >> v;
+        --v;
+        cout << (cnt[uf.find(v)] ? "Yes" : "No") << endl;
+      }
+    }
+  }
+  return 0;
+}
