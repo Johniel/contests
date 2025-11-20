d1de7583bfb1dd44c778284c3631e5c5cb36adb4
Johniel <johniel.s.m@gmail.com>
2025-11-21 02:03:06 +0900

    ABCDF

---
+++ codeforces/1065div3/F/main.cpp
@@ -0,0 +1,173 @@
+// github.com/Johniel/contests
+// codeforces/1065div3/F/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+#define __builtin_popcount(x) __builtin_popcountll(x)
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p);
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p);
+template<typename... Ts> ostream& operator << (ostream& os, const tuple<Ts...>& t) { os << "("; if constexpr (sizeof...(Ts) > 0) { apply([&](const Ts&... args) { ((os << args << ','), ...); }, t); } os << ")"; return os; }
+template<typename... Ts> istream& operator >> (istream& is, tuple<Ts...>& t) { apply([&](Ts&... args) { ((is >> args), ...); }, t); return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "set{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "map{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, stack<T> s) { os << "stack{"; while (s.size()) { os << s.top() << ","; s.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, queue<T> q) { os << "queue{"; while (q.size()) { os << q.front() << ","; q.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, deque<T> q) { os << "deque{"; for (int i = 0; i < q.size(); ++i) os << q[i] << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, priority_queue<T> q) { os << "heap{"; while (q.size()) { os << q.top() << ","; q.pop(); } os << "}"; return os; }
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void _____(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr lli mod = 998244353; // 1e9 + 7;
+
+template<typename T>
+struct SegTree {
+  // https://codeforces.com/blog/entry/18051
+  using F = function<T(T, T)>;
+  const F op;
+  const T e;
+  const int n;
+  const int bitceiln;
+  vector<T> v;
+  SegTree(size_t n_, T e_, F op_) : e(e_), n(n_), op(op_), bitceiln(__bit_ceil(n)) {
+    assert(op(e, e) == e);
+    v.resize(2 * bitceiln, e);
+  }
+  SegTree(const vector<T>& v, T e_, F op_) : SegTree(v.size(), e_, op_) {
+    for (size_t i = 0; i < v.size(); ++i) set(i, v[i]);
+  }
+  void set(size_t k, T a) {
+    assert(k < n);
+    for (v[k += bitceiln] = a; k >>= 1; v[k] = op(v[k * 2], v[k * 2 + 1])) ;
+  }
+  T get(size_t k) const { assert(k < n); return v[k + bitceiln]; }
+  T operator () (void) const { return v.at(1); }
+  T operator () (size_t begin, size_t end) { return query(begin, end); }
+  T all_prod(void) const { return v.at(1); }
+  T query(void) const { return v.at(1); }
+  T prod(size_t begin, size_t end) const { return query(begin, end); }
+  T query(size_t l, size_t r) {
+    assert(0 <= l && l <= r && r <= n);
+    T L = e, R = e;
+    for (l += bitceiln, r += bitceiln; l < r; l >>= 1, r >>= 1) {
+      if (l & 1) L = op(L, v[l++]);
+      if (r & 1) R = op(v[--r], R);
+    }
+    return op(L, R);
+  }
+  size_t size(void) const { return n; }
+};
+template<typename T> istream& operator >> (istream& is, SegTree<T>& seg) { for (size_t i = 0; i < seg.size(); ++i) { T t; is >> t; seg.set(i, t); } return is; }
+template<typename T> ostream& operator << (ostream& os, SegTree<T>& seg) { os << seg.v; return os; }
+
+struct UnionFind {
+  vector<int> r, p;
+  UnionFind(int n): r(n, 0), p(n, -1) {}
+  bool unite(int a, int b) {
+    a = find(a);
+    b = find(b);
+    if (a == b) return false;
+    if (r[a] > r[b]) swap(a, b);
+    p[b] += p[a];
+    p[a] = b;
+    if (r[a] == r[b]) ++r[b];
+    return true;
+  }
+  int find(int a) {
+    return (p[a] < 0) ? a : p[a] = find(p[a]);
+  }
+  bool same(int a, int b) {
+    return find(a) == find(b);
+  }
+  size_t size(int n) {
+    return -p.at(find(n));
+  }
+};
+
+int main(int argc, char *argv[])
+{
+  { int _; cin >> _; }
+  int n;
+  while (cin >> n) {
+    vec<int> p(n);
+    cin >> p;
+
+    bool f = false;
+    set<int> vis;
+    for (int i = 0; i + 1 < p.size(); ++i) {
+      vis.insert(p[i]);
+      if (*vis.rbegin() == n && *vis.begin() == n - i) {
+        f = true;
+        break;
+      }
+    }
+    if (f) {
+      cout << "No" << endl;
+      continue;
+    }
+
+    map<int, int> idx;
+    for (int i = 0; i < p.size(); ++i) {
+      idx[p[i]] = i;
+    }
+    vec<pair<int, int>> es;
+    UnionFind uf(n+1);
+
+    const int inf = (1 << 29);
+
+    SegTree<int> mn(n+1, +inf, [] (auto a, auto b) { return min(a, b); });
+    SegTree<int> mx(n+1, -inf, [] (auto a, auto b) { return max(a, b); });
+    for (int i = 0; i < p.size(); ++i) mx.set(p[i], i);
+    for (int i = 0; i < p.size(); ++i) mn.set(p[i], i);
+
+    for (int v = 1; v <= n; ++v) {
+      while (true) {
+        int i = mn.query(0, v);
+        if (i == inf) break;
+        unless (i < idx[v]) break;
+        if (!uf.same(v, p[i])) {
+          es.push_back(make_pair(v, p[i]));
+          uf.unite(v, p[i]);
+        }
+        mn.set(p[i], inf);
+      }
+     }
+
+    for (int v = 1; v <= n; ++v) {
+       while (true) {
+        int i = mx.query(v, n + 1);
+        if (i == -inf) break;
+        unless (idx[v] < i) break;
+        if (!uf.same(p[i], v)) {
+          es.push_back(make_pair(p[i], v));
+          uf.unite(p[i], v);
+        }
+        mx.set(p[i], -inf);
+      }
+     }
+
+    cerr << p << endl;
+    cout << "Yes" << endl;
+    each (e, es) cout << e.first << ' ' << e.second << endl;
+    assert(es.size() == n - 1);
+  }
+  return 0;
+}
