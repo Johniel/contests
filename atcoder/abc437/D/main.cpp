daea383b53b8994015dd9d6131b863acc13c2e7b
Johniel <johniel.s.m@gmail.com>
2025-12-20 22:49:14 +0900

    ABCDEF

---
+++ atcoder/abc437/D/main.cpp
@@ -0,0 +1,110 @@
+// github.com/Johniel/contests
+// atcoder/abc437/D/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+#define __builtin_popcount(x) __builtin_popcountll(x)
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p);
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p);
+template<typename... Ts> ostream& operator << (ostream& os, const tuple<Ts...>& t) { os << "("; if constexpr (sizeof...(Ts) > 0) { apply([&](const Ts&... args) { ((os << args << ','), ...); }, t); } os << ")"; return os; }
+template<typename... Ts> istream& operator >> (istream& is, tuple<Ts...>& t) { apply([&](Ts&... args) { ((is >> args), ...); }, t); return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "set{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "map{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, stack<T> s) { os << "stack{"; while (s.size()) { os << s.top() << ","; s.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, queue<T> q) { os << "queue{"; while (q.size()) { os << q.front() << ","; q.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, deque<T> q) { os << "deque{"; for (int i = 0; i < q.size(); ++i) os << q[i] << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, priority_queue<T> q) { os << "heap{"; while (q.size()) { os << q.top() << ","; q.pop(); } os << "}"; return os; }
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void _____(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr lli mod = 998244353; // 1e9 + 7;
+
+template<typename T = long long int>
+struct BIT {
+  vector<T> v;
+  const int n;
+
+  BIT(int n_ = 0) : n(n_), v(n_ + 1, 0) {}
+
+  T operator () (int i) const {
+    T s = 0;
+    while (i > 0) {
+      (s += v.at(i)) %= mod;
+      i -= i & -i;
+    }
+    return s;
+  }
+
+  T query(int begin, int end) const {
+    assert(begin <= end);
+    return (*this)(end) - (*this)(begin);
+  }
+
+  T operator () (int begin, int end) const {
+    return query(begin, end);
+  }
+
+  void add(int i, T x) {
+    ++i;
+    while (i <= n) {
+      v.at(i) += x;
+      i += i & -i;
+    }
+    return ;
+  }
+
+  size_t size(void) const { return n; }
+};
+
+int main(int argc, char *argv[])
+{
+  int n, m;
+  while (cin >> n >> m) {
+    vec<lli> a(n), b(m);
+    cin >> a >> b;
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    BIT<lli> bit(a.size());
+    for (int i = 0; i < a.size(); ++i) {
+      bit.add(i, a[i]);
+    }
+
+    // cout << a << endl;
+    // cout << b << endl;
+
+    lli z = 0;
+    for (int i = 0; i < b.size(); ++i) {
+      auto itr = lower_bound(a.begin(), a.end(), b[i] + 1);
+      const int idx = itr - a.begin();
+
+      // cout << b[i] << ' ' << idx << "," << -bit(0, idx) << ", +" << idx * b[i] << endl;
+      // cout << (a.size() - idx) <<"*" <<b[i] << endl;
+      z += (idx * b[i] % mod) - bit(0, idx) + mod;
+      z %= mod;
+      z += bit(idx, bit.size()) - ((a.size() - idx) * b[i] % mod) + mod;
+      z %= mod;
+    }
+
+    cout << z << endl;
+  }
+  return 0;
+}
