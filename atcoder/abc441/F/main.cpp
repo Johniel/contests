da84b401e020e9b4cc23e6bb112f1da6a22db611
Johniel <johniel.s.m@gmail.com>
2026-01-17 23:02:31 +0900

    F

--- atcoder/abc441/F/main.cpp
+++ atcoder/abc441/F/main.cpp
@@ -81,60 +81,11 @@ int main(int argc, char *argv[])
       }
     }
 
-
-    // cout << mx << endl;
-    // for (int i = 0; i <= v.size(); ++i) {
-    //   for (int j = 0; j <= m; ++j) {
-    //     if (dp1[i][j] < 0) cout << "_ ";
-    //     else cout << dp1[i][j] << ' ';
-    //   }
-    //   cout << endl;
-    // }
-    // cout << endl;
-    // for (int i = 0; i <= v.size(); ++i) {
-    //   for (int j = 0; j <= m; ++j) {
-    //     if (dp2[i][j] < 0) cout << "_ ";
-    //     else cout << dp2[i][j] << ' ';
-    //   }
-    //   cout << endl;
-    // }
-    // cout << endl;
-
     for (int i = 0; i <= v.size(); ++i) {
       for (int j = 0; j <= m; ++j) {
         setmax(dp2[i][j + 1], dp2[i][j]);
       }
     }
-
-    // for (int i = 0; i <= v.size(); ++i) {
-    //   for (int j = 0; j <= m; ++j) {
-    //     if (dp2[i][j] < 0) cout << "_ ";
-    //     else cout << dp2[i][j] << ' ';
-    //   }
-    //   cout << endl;
-    // }
-    // cout << endl;
-
-    // {
-    //   int i = v.size() - 1;
-    //   bool A = false;
-    //   bool B = false;
-    //   for (int pref = 0; pref <= m; ++pref) {
-    //     {
-    //       int suff = m - (pref + v[i].first);
-
-    //       if (0 <= suff && suff <= m) {
-    //         cout << make_pair(i, pref) << make_pair(pref, suff) << dp1[i][pref] << "+" << dp2[i + 1][suff] << "=="<< mx << endl;
-    //       }
-
-    //       if (0 <= suff && suff <= m && dp1[i][pref] + dp2[i + 1][suff] + v[i].second == mx) A = true;
-    //     }
-    //     {
-    //       int suff = m - pref;
-    //       if (0 <= suff && suff <= m && dp1[i][pref] + dp2[i + 1][suff] == mx) B = true;
-    //     }
-    //   }
-    // }
 
     str s;
     for (int i = 0; i < v.size(); ++i) {
