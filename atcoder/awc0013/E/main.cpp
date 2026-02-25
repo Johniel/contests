96dc3a8e3650ebd0aa7fff7c8241b315e68478a7
Johniel <johniel.s.m@gmail.com>
2026-02-25 20:38:59 +0900

    ABCDE

---
+++ atcoder/awc0013/E/main.cpp
@@ -0,0 +1,104 @@
+// github.com/Johniel/contests
+// atcoder/awc0013/E/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+#define __builtin_popcount(x) __builtin_popcountll(x)
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p);
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p);
+template<typename... Ts> ostream& operator << (ostream& os, const tuple<Ts...>& t) { os << "("; if constexpr (sizeof...(Ts) > 0) { apply([&](const Ts&... args) { ((os << args << ','), ...); }, t); } os << ")"; return os; }
+template<typename... Ts> istream& operator >> (istream& is, tuple<Ts...>& t) { apply([&](Ts&... args) { ((is >> args), ...); }, t); return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "set{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "map{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, stack<T> s) { os << "stack{"; while (s.size()) { os << s.top() << ","; s.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, queue<T> q) { os << "queue{"; while (q.size()) { os << q.front() << ","; q.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, deque<T> q) { os << "deque{"; for (int i = 0; i < q.size(); ++i) os << q[i] << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, priority_queue<T> q) { os << "heap{"; while (q.size()) { os << q.top() << ","; q.pop(); } os << "}"; return os; }
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void _____(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr lli mod = 998244353; // 1e9 + 7;
+
+template<int N>
+struct BiparticleMatching {
+  array<vector<int>, N> g;
+  array<int, N> match;
+  array<bool, N> vis;
+
+  void init(void) {
+    fill(g.begin(), g.end(), vector<int>());
+    return ;
+  }
+
+  void add_edge(int i, int j) {
+    g[i].push_back(j);
+    g[j].push_back(i);
+    return ;
+  }
+
+  bool rec(int curr) {
+    vis[curr] = true;
+    each (next, g[curr]) {
+      int w = match[next];
+      if (w < 0 || (!vis[w] && rec(w))) {
+        match[curr] = next;
+        match[next] = curr;
+        return true;
+      }
+    }
+    return false;
+  }
+
+  int run(void) {
+    int sum = 0;
+    fill(match.begin(), match.end(), -1);
+    for (int i = 0; i < N; ++i) {
+      if (match[i] == -1) {
+        fill(vis.begin(), vis.end(), false);
+        sum += rec(i);
+      }
+    }
+    return sum;
+  }
+};
+
+int main(int argc, char *argv[])
+{
+  int n, m;
+  while (cin >> n >> m) {
+    constexpr int N = 205;
+    BiparticleMatching<N> matching;
+
+    for (int i = 0; i < n; ++i) {
+      int k;
+      cin >> k;
+      vec<int> c(k);
+      cin >> c;
+      each (j, c) --j;
+      each (j, c) {
+        matching.add_edge(i, j + N / 2);
+      }
+    }
+    cout << matching.run() << endl;
+  }
+  return 0;
+}
