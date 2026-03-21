d516c6d92750c2a6a85edf1ca241c1e156d19e61
Johniel <johniel.s.m@gmail.com>
2026-03-21 19:31:26 +0900

    ABCDE

---
+++ atcoder/awc0030/D/main.cpp
@@ -0,0 +1,155 @@
+// github.com/Johniel/contests
+// atcoder/awc0030/D/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+#define __builtin_popcount(x) __builtin_popcountll(x)
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p);
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p);
+template<typename... Ts> ostream& operator << (ostream& os, const tuple<Ts...>& t) { os << "("; if constexpr (sizeof...(Ts) > 0) { apply([&](const Ts&... args) { ((os << args << ','), ...); }, t); } os << ")"; return os; }
+template<typename... Ts> istream& operator >> (istream& is, tuple<Ts...>& t) { apply([&](Ts&... args) { ((is >> args), ...); }, t); return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "set{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "map{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, stack<T> s) { os << "stack{"; while (s.size()) { os << s.top() << ","; s.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, queue<T> q) { os << "queue{"; while (q.size()) { os << q.front() << ","; q.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, deque<T> q) { os << "deque{"; for (int i = 0; i < q.size(); ++i) os << q[i] << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, priority_queue<T> q) { os << "heap{"; while (q.size()) { os << q.top() << ","; q.pop(); } os << "}"; return os; }
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void _____(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr lli mod = 998244353; // 1e9 + 7;
+
+namespace {
+  // https://atcoder.jp/contests/typical90/tasks/typical90_u
+
+  const int N = 2 * 1e5 + 3;
+  bool vis[N];
+
+  void rec(vec<int> graph[N], int curr, int ord[], int &cnt)
+  {
+    vis[curr] = true;
+    for (auto& next : graph[curr]) {
+      unless (vis[next]) rec(graph, next, ord, cnt);
+    }
+    ord[--cnt] = curr;
+    return ;
+  }
+
+  vector<int> g[N];
+  vector<int> h[N];
+  void init(const int size)
+  {
+    fill(g, g + size, vector<int>());
+  }
+
+  int ord[N];
+  int scc[N];
+  vector<vector<int>> detect_scc(const int size)
+  {
+    int cnt, prev = size - 1;
+    vector<vector<int>> ret;
+    fill(ord, ord + size, -1);
+    fill(scc, scc + size, -1);
+    fill(h, h + size, vector<int>());
+    for (int curr = 0; curr < size; ++curr) {
+      for (const auto& next : g[curr]) {
+        h[next].push_back(curr);
+      }
+    }
+    cnt = size;
+    fill(vis, vis + size, false);
+    for (int i = 0; i < size; ++i) {
+      unless (vis[i]) rec(g, i, ord, cnt);
+    }
+    cnt = size;
+    fill(vis, vis + size, false);
+    for (int i = 0; i < size; ++i) {
+      unless (vis[ord[i]]) {
+        rec(h, ord[i], scc, cnt);
+        int j = prev;
+        vector<int> T;
+        for (j = prev; 0 <= j && scc[j] != -1; --j) T.push_back(scc[j]);
+        prev = j;
+        ret.push_back(T);
+      }
+    }
+    return ret;
+  }
+};
+
+struct UnionFind {
+  vector<int> r, p;
+  UnionFind(int n): r(n, 0), p(n, -1) {}
+  bool unite(int a, int b) {
+    a = find(a);
+    b = find(b);
+    if (a == b) return false;
+    if (r[a] > r[b]) swap(a, b);
+    p[b] += p[a];
+    p[a] = b;
+    if (r[a] == r[b]) ++r[b];
+    return true;
+  }
+  int find(int a) {
+    return (p[a] < 0) ? a : p[a] = find(p[a]);
+  }
+  bool same(int a, int b) {
+    return find(a) == find(b);
+  }
+  size_t size(int n) {
+    return -p.at(find(n));
+  }
+};
+
+int main(int argc, char *argv[])
+{
+  int n;
+  while (cin >> n) {
+    fill(g, g + N, vec<int>());
+    for (int i = 0; i < n; ++i) {
+      int j;
+      cin >> j;
+      --j;
+      g[i].push_back(j);
+    }
+    vec<vec<int>> scc = detect_scc(n);
+    UnionFind uf(n);
+    for (int i = 0; i < scc.size(); ++i) {
+      for (int j = 0; j < scc[i].size(); ++j) {
+        uf.unite(scc[i][0], scc[i][j]);
+      }
+    }
+    vec<int> cost(n, -1);
+    function<int(int)> rec = [&] (int curr) {
+      if (0 <= cost[curr]) return cost[curr];
+      if (uf.size(curr) != 1) return int(uf.size(curr));
+      if (cost[curr] == -2) return 0;
+      cost[curr] = -2;
+      return cost[curr] = rec(g[curr][0]) + (uf.size(curr) != 1);
+    };
+    for (int i = 0; i < n; ++i) {
+      cout << rec(i) << ' ';
+    }
+    cout << endl;
+  }
+  return 0;
+}
