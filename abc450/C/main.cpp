40f9f4e6319e4f5463e6465c988d8069dc8f78aa
Johniel <johniel.s.m@gmail.com>
2026-03-21 22:57:58 +0900

    ABCD

---
+++ abc450/C/main.cpp
@@ -0,0 +1,88 @@
+// github.com/Johniel/contests
+// abc450/C/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+#define __builtin_popcount(x) __builtin_popcountll(x)
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p);
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p);
+template<typename... Ts> ostream& operator << (ostream& os, const tuple<Ts...>& t) { os << "("; if constexpr (sizeof...(Ts) > 0) { apply([&](const Ts&... args) { ((os << args << ','), ...); }, t); } os << ")"; return os; }
+template<typename... Ts> istream& operator >> (istream& is, tuple<Ts...>& t) { apply([&](Ts&... args) { ((is >> args), ...); }, t); return is; }
+template<typename T> ostream& operator << (ostream& os, vector<T> v) { os << "("; for (auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, set<T> s) { os << "set{"; for (auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, map<K, V> m) { os << "map{"; for (auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (auto& i: a) os << i << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, stack<T> s) { os << "stack{"; while (s.size()) { os << s.top() << ","; s.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, queue<T> q) { os << "queue{"; while (q.size()) { os << q.front() << ","; q.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, deque<T> q) { os << "deque{"; for (int i = 0; i < q.size(); ++i) os << q[i] << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, priority_queue<T> q) { os << "heap{"; while (q.size()) { os << q.top() << ","; q.pop(); } os << "}"; return os; }
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void _____(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr lli mod = 998244353; // 1e9 + 7;
+
+int main(int argc, char *argv[])
+{
+  int h, w;
+  while (cin >> h >> w) {
+    char g[h][w];
+    for (int i = 0; i < h; ++i) {
+      for (int j = 0; j < w; ++j) {
+        cin >> g[i][j];
+      }
+    }
+
+
+
+    int z = 0;
+    for (int a = 0; a < h; ++a) {
+      for (int b = 0; b < w; ++b) {
+        unless (g[a][b] == '.') continue;
+        g[a][b] = '%';
+        queue<pair<int, int>> q;
+        bool f = true;
+        for (q.push(make_pair(a, b)); q.size(); q.pop()) {
+          auto [i, j] = q.front();
+          for (int d = 0; d < 4; ++d) {
+            constexpr array<int, 8> di({0, 1, -1, 0, 1, -1, 1, -1});
+            constexpr array<int, 8> dj({1, 0, 0, -1, 1, -1, -1, 1});
+            const int ni = i + di[d];
+            const int nj = j + dj[d];
+            unless (0 <= ni && ni < h){
+              f = false;
+              continue;
+            }
+            unless (0 <= nj && nj < w) {
+              f = false;
+              continue;
+            }
+            if (g[ni][nj] == '.') {
+              g[ni][nj] = '%';
+              q.push(make_pair(ni, nj));
+            }
+          }
+        }
+        z += f;
+      }
+    }
+    cout << z << endl;
+  }
+  return 0;
+}
