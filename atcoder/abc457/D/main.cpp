716d7c9fc164ef79c8d2cc10cfbd2508573c57e3
Johniel <johniel.s.m@gmail.com>
2026-05-09 22:48:28 +0900

    ABCDE

---
+++ atcoder/abc457/D/main.cpp
@@ -0,0 +1,116 @@
+// github.com/Johniel/contests
+// atcoder/abc457/D/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+#define __builtin_popcount(x) __builtin_popcountll(x)
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p);
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p);
+template<typename... Ts> ostream& operator << (ostream& os, const tuple<Ts...>& t) { os << "("; if constexpr (sizeof...(Ts) > 0) { apply([&](const Ts&... args) { ((os << args << ','), ...); }, t); } os << ")"; return os; }
+template<typename... Ts> istream& operator >> (istream& is, tuple<Ts...>& t) { apply([&](Ts&... args) { ((is >> args), ...); }, t); return is; }
+template<typename T> ostream& operator << (ostream& os, const vector<T>& v) { os << "("; for (const auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, const set<T>& s) { os << "set{"; for (const auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, const map<K, V>& m) { os << "map{"; for (const auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (const auto& i: a) os << i << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, stack<T> s) { os << "stack{"; while (s.size()) { os << s.top() << ","; s.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, queue<T> q) { os << "queue{"; while (q.size()) { os << q.front() << ","; q.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, const deque<T>& q) { os << "deque{"; for (int i = 0; i < q.size(); ++i) os << q[i] << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, priority_queue<T> q) { os << "heap{"; while (q.size()) { os << q.top() << ","; q.pop(); } os << "}"; return os; }
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void _____(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr lli mod = 998244353; // 1e9 + 7;
+
+using int128 = __int128;
+
+int128 sto128(string &s)
+{
+  __int128 val = 0;
+  for (int i = 0; i < s.size() && isdigit(s[i]); ++i) {
+    val = (10 * val) + (s[i] - '0');
+  }
+  return val;
+}
+
+// https://kenkoooo.hatenablog.com/entry/2016/11/30/163533
+ostream& operator << (ostream& os, __int128_t value)
+{
+  ostream::sentry s(os);
+  if (s) {
+    __uint128_t tmp = value < 0 ? -value : value;
+    char buffer[128];
+    char *d = end(buffer);
+    do {
+      --d;
+      *d = "0123456789"[tmp % 10];
+      tmp /= 10;
+    } while (tmp != 0);
+    if (value < 0) {
+      --d;
+      *d = '-';
+    }
+    int len = end(buffer) - d;
+    if (os.rdbuf()->sputn(d, len) != len) {
+      os.setstate(ios_base::badbit);
+    }
+  }
+  return os;
+}
+
+istream& operator >> (istream& is, int128& val)
+{
+  string s;
+  is >> s;
+  val = sto128(s);
+  return is;
+}
+
+int main(int argc, char *argv[])
+{
+  int n;
+  int128 k;
+  while (cin >> n >> k) {
+    vec<int128> a(n);
+    for (int i = 0; i < n; ++i) {
+      int128 x;
+      cin >> x;
+      a[i] = x;
+    }
+
+    auto fn = [&] (const int128 x) {
+      int128 y = k;
+      for (int i = 0; i < a.size(); ++i) {
+        if (x - a[i] <= 0) continue;
+        y -= (x - a[i] + i) / (i + 1);
+      }
+      return 0 <= y;
+    };
+
+    int128 small = 0;
+    int128 large = (int128)(1LL << 40) * (int128)(1LL << 40);
+    while (small + 1 < large) {
+      auto mid = (small + large) / 2;
+      if (fn(mid)) small = mid;
+      else large = mid;
+    }
+    cout << small << endl;
+  }
+  return 0;
+}
---
+++ atcoder/abc457/E/main.cpp
@@ -0,0 +1,144 @@
+// github.com/Johniel/contests
+// atcoder/abc457/E/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+#define __builtin_popcount(x) __builtin_popcountll(x)
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p);
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p);
+template<typename... Ts> ostream& operator << (ostream& os, const tuple<Ts...>& t) { os << "("; if constexpr (sizeof...(Ts) > 0) { apply([&](const Ts&... args) { ((os << args << ','), ...); }, t); } os << ")"; return os; }
+template<typename... Ts> istream& operator >> (istream& is, tuple<Ts...>& t) { apply([&](Ts&... args) { ((is >> args), ...); }, t); return is; }
+template<typename T> ostream& operator << (ostream& os, const vector<T>& v) { os << "("; for (const auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, const set<T>& s) { os << "set{"; for (const auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, const map<K, V>& m) { os << "map{"; for (const auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (const auto& i: a) os << i << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, stack<T> s) { os << "stack{"; while (s.size()) { os << s.top() << ","; s.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, queue<T> q) { os << "queue{"; while (q.size()) { os << q.front() << ","; q.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, const deque<T>& q) { os << "deque{"; for (int i = 0; i < q.size(); ++i) os << q[i] << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, priority_queue<T> q) { os << "heap{"; while (q.size()) { os << q.top() << ","; q.pop(); } os << "}"; return os; }
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void _____(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr lli mod = 998244353; // 1e9 + 7;
+
+template<typename T>
+struct SegTree {
+  // https://codeforces.com/blog/entry/18051
+  using F = function<T(T, T)>;
+  const F op;
+  const T e;
+  const int n;
+  const int bitceiln;
+  vector<T> v;
+  SegTree(size_t n_, T e_, F op_) : e(e_), n(n_), op(op_), bitceiln(__bit_ceil((unsigned int)n)) {
+    assert(op(e, e) == e);
+    v.resize(2 * bitceiln, e);
+  }
+  SegTree(const vector<T>& v, T e_, F op_) : SegTree(v.size(), e_, op_) {
+    for (size_t i = 0; i < v.size(); ++i) set(i, v[i]);
+  }
+  void set(size_t k, T a) {
+    assert(k < n);
+    for (v[k += bitceiln] = a; k >>= 1; v[k] = op(v[k * 2], v[k * 2 + 1])) ;
+  }
+  T get(size_t k) const { assert(k < n); return v[k + bitceiln]; }
+  T operator () (void) const { return v.at(1); }
+  T operator () (size_t begin, size_t end) { return query(begin, end); }
+  T all_prod(void) const { return v.at(1); }
+  T query(void) const { return v.at(1); }
+  T prod(size_t begin, size_t end) const { return query(begin, end); }
+  T query(size_t l, size_t r) {
+    assert(0 <= l && l <= r && r <= n);
+    T L = e, R = e;
+    for (l += bitceiln, r += bitceiln; l < r; l >>= 1, r >>= 1) {
+      if (l & 1) L = op(L, v[l++]);
+      if (r & 1) R = op(v[--r], R);
+    }
+    return op(L, R);
+  }
+  size_t size(void) const { return n; }
+};
+template<typename T> istream& operator >> (istream& is, SegTree<T>& seg) { for (size_t i = 0; i < seg.size(); ++i) { T t; is >> t; seg.set(i, t); } return is; }
+template<typename T> ostream& operator << (ostream& os, SegTree<T>& seg) { os << seg.v; return os; }
+
+int main(int argc, char *argv[])
+{
+  int n, m;
+  while (cin >> n >> m) {
+    vec<pair<int, int>> v(m);
+    cin >> v;
+    map<lli, vec<lli>> head, tail;
+    each (k, v) {
+      head[k.first].push_back(k.second);
+      tail[k.second].push_back(k.first);
+    }
+    each (k, head) sort(k.second.begin(), k.second.end());
+    each (k, tail) sort(k.second.begin(), k.second.end());
+
+    map<pair<int, int>, int> vis;
+    each (i, v) ++vis[i];
+
+    const int N = 2 * 1e5 + 3;
+    const int inf = (1LL << 29);
+    SegTree<int> seg(N, inf, [] (auto a, auto b) { return min(a, b); });
+    each (k, v) {
+      int x = seg.get(k.first);
+      seg.set(k.first, min(x, k.second));
+    }
+
+    int q;
+    cin >> q;
+    while (q--) {
+      int s, t;
+      cin >> s >> t;
+      bool f = false;
+      { // ２枚使う
+        pair<int, int> x, y;
+        x = y = make_pair(0, 0);
+        if (head.count(s)) {
+          auto itr = lower_bound(head[s].begin(), head[s].end(), t + 1);
+          if (itr != head[s].begin()) {
+            --itr;
+            if (*itr <= t) x = make_pair(s, *itr);
+          }
+        }
+        if (tail.count(t)) {
+          auto itr = lower_bound(tail[t].begin(), tail[t].end(), s + 1);
+          if (itr != tail[t].end() && s <= *itr) {
+            y = make_pair(*itr, t);
+          }
+        }
+        if (x.first == s && y.second == t && x.second <= t && s <= y.first && y.first <= x.second + 1) {
+          f = true;
+        }
+      }
+      if (!f) { // S側１枚+覆われてる小さな１枚
+        auto x = make_pair(s, t);
+        if (vis.count(x) && 2 <= vis[x]) f = true;
+        if (vis.count(x) && vis[x] == 1) {
+          if (seg.query(s + 1, t + 1) <= t) f = true;
+          if (seg.query(s, t) < t) f = true;
+        }
+      }
+      cout << (f ? "Yes" : "No") << endl;
+    }
+  }
+  return 0;
+}
