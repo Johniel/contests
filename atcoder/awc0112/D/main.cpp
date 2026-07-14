32edd4933bd919019a89052e8667857755c2167c
Johniel <johniel.s.m@gmail.com>
2026-07-14 20:23:42 +0900

    ABCDE

---
+++ atcoder/awc0112/D/main.cpp
@@ -0,0 +1,70 @@
+// github.com/Johniel/contests
+// atcoder/awc0112/D/main.cpp
+
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+#define __builtin_popcount(x) __builtin_popcountll(x)
+
+using namespace std;
+
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p);
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p);
+template<typename... Ts> ostream& operator << (ostream& os, const tuple<Ts...>& t) { os << "("; if constexpr (sizeof...(Ts) > 0) { apply([&](const Ts&... args) { ((os << args << ','), ...); }, t); } os << ")"; return os; }
+template<typename... Ts> istream& operator >> (istream& is, tuple<Ts...>& t) { apply([&](Ts&... args) { ((is >> args), ...); }, t); return is; }
+template<typename T> ostream& operator << (ostream& os, const vector<T>& v) { os << "("; for (const auto& i: v) os << i << ","; os << ")"; return os; }
+template<typename T> istream& operator >> (istream& is, vector<T>& v) { for (auto& i: v) is >> i; return is; }
+template<typename T> ostream& operator << (ostream& os, const set<T>& s) { os << "set{"; for (const auto& i: s) os << i << ","; os << "}"; return os; }
+template<typename K, typename V> ostream& operator << (ostream& os, const map<K, V>& m) { os << "map{"; for (const auto& i: m) os << i << ","; os << "}"; return os; }
+template<typename E, size_t N> istream& operator >> (istream& is, array<E, N>& a) { for (auto& i: a) is >> i; return is; }
+template<typename E, size_t N> ostream& operator << (ostream& os, array<E, N>& a) { os << "[" << N << "]{"; for (const auto& i: a) os << i << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, stack<T> s) { os << "stack{"; while (s.size()) { os << s.top() << ","; s.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, queue<T> q) { os << "queue{"; while (q.size()) { os << q.front() << ","; q.pop(); } os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, const deque<T>& q) { os << "deque{"; for (int i = 0; i < q.size(); ++i) os << q[i] << ","; os << "}"; return os; }
+template<typename T> ostream& operator << (ostream& os, priority_queue<T> q) { os << "heap{"; while (q.size()) { os << q.top() << ","; q.pop(); } os << "}"; return os; }
+template<typename P, typename Q> ostream& operator << (ostream& os, pair<P, Q> p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template<typename P, typename Q> istream& operator >> (istream& is, pair<P, Q>& p) { is >> p.first >> p.second; return is; }
+
+template<typename T> inline T setmax(T& a, T b) { return a = std::max(a, b); }
+template<typename T> inline T setmin(T& a, T b) { return a = std::min(a, b); }
+
+__attribute__((constructor)) static void _____(void) { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.setf(ios_base::fixed); cout.precision(15); return ; }
+
+using lli = long long int;
+using ull = unsigned long long;
+using str = string;
+template<typename T> using vec = vector<T>;
+
+constexpr lli mod = 998244353; // 1e9 + 7;
+
+int main(int argc, char *argv[])
+{
+  int n, k;
+  while (cin >> n >> k) {
+    vec<pair<lli, int>> v(n);
+    cin >> v;
+    const int N = 200 + 3;
+    const int K = 1e4 + 3;
+    static lli dp[N][K][2];
+    const lli inf = 1LL << 60;
+    fill(&dp[0][0][0], &dp[N - 1][K - 1][2 - 1] + 1, -inf);
+    dp[0][0][0] = 0;
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j <= k; ++j) {
+        if (j + v[i].second <= k) {
+          setmax(dp[i + 1][j + v[i].second][1], dp[i][j][0] + v[i].first);
+        }
+        setmax(dp[i + 1][j][0], dp[i][j][0]);
+        setmax(dp[i + 1][j][0], dp[i][j][1]);
+      }
+    }
+    lli z = 0;
+    for (int i = 0; i <= k; ++i) {
+      setmax(z, dp[n][i][0]);
+      setmax(z, dp[n][i][1]);
+    }
+    cout << z << endl;
+  }
+  return 0;
+}
